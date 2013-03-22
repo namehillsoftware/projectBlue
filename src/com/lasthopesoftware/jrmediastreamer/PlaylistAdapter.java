c5f674e1ff2184ff5b390a3363b093e23739e50a
heisthemachine <dvedvick@devedcomputers.com>
2013-03-22 09:24:18 -0400

    changed file menu so it looks nicer

--- src/com/lasthopesoftware/jrmediastreamer/PlaylistAdapter.java
+++ src/com/lasthopesoftware/jrmediastreamer/PlaylistAdapter.java
@@ -10,6 +10,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -18,6 +19,7 @@ import android.widget.AbsListView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
 
@@ -51,11 +53,11 @@ public class PlaylistAdapter extends BaseAdapter {
 		AbsListView.LayoutParams lp = new AbsListView.LayoutParams(
 	            ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 		
-		ViewFlipper parentView = new ViewFlipper(mContext);
+		ViewFlipper parentView = new ViewFlipper(parent.getContext());
 		parentView.setLayoutParams(lp);
 		
-        TextView textView = new TextView(mContext);
-        textView.setTextAppearance(mContext, android.R.style.TextAppearance_Large);
+        TextView textView = new TextView(parentView.getContext());
+        textView.setTextAppearance(parentView.getContext(), android.R.style.TextAppearance_Large);
         textView.setLayoutParams(lp);
         // Center the text vertically
         textView.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);
@@ -66,113 +68,69 @@ public class PlaylistAdapter extends BaseAdapter {
         
         parentView.addView(textView);
         
-        GridView gridView = new GridView(mContext);
-		gridView.setNumColumns(3);
-		gridView.setStretchMode(GridView.STRETCH_COLUMN_WIDTH);
-		gridView.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
-		gridView.setAdapter(new ItemLongClickGridAdapter(mContext, playlist));
+        LayoutInflater inflater = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE );
+        LinearLayout fileMenu = (LinearLayout)inflater.inflate(R.layout.active_jr_item_menu, null);
+        
+        Button shuffleButton = (Button)fileMenu.findViewById(R.id.btnShuffle);
+        shuffleButton.setOnClickListener(new ShuffleClickHandler(playlist));
+        
+        Button playButton = (Button)fileMenu.findViewById(R.id.btnPlayAll);
+        playButton.setOnClickListener(new PlayClickHandler(playlist));
+        
+        Button viewButton = (Button)fileMenu.findViewById(R.id.btnViewFiles);
+        viewButton.setOnClickListener(new ViewFilesClickHandler(playlist));
 		
-		parentView.addView(gridView);
+		parentView.addView(fileMenu);
 		
 		return parentView;
 	}
 	
-	private static class ItemLongClickGridAdapter extends BaseAdapter {
-		private Context mContext;
+	private static class PlayClickHandler implements OnClickListener {
 		private JrPlaylist mPlaylist;
 		
-		private String[] mButtons = {
-				"Play", "Shuffle", "View"
-		};
-		
-		public ItemLongClickGridAdapter(Context context, JrPlaylist playlist) {
-			mContext = context;
+		public PlayClickHandler(JrPlaylist playlist) {
 			mPlaylist = playlist;
 		}
 		
 		@Override
-		public int getCount() {
-	        return mButtons.length;
-	    }
-
-		@Override
-	    public Object getItem(int position) {
-	        return null;
-	    }
-
-		@Override
-	    public long getItemId(int position) {
-	        return 0;
-	    }
+		public void onClick(View v) {
+			JrSession.playlist = mPlaylist.getFiles();
+			JrFile file = mPlaylist.getFiles().get(0);
+			Intent svcIntent = new Intent(StreamingMusicService.ACTION_PLAY, Uri.parse(file.getUrl()), v.getContext(), StreamingMusicService.class);
+			v.getContext().startService(svcIntent);
+		}
+	}
+	
+	private static class ShuffleClickHandler implements OnClickListener {
+		private JrPlaylist mPlaylist;
 		
-		public View getView(int position, View convertView, ViewGroup parent) {
-	        Button button;
-	        if (convertView != null) return (Button)convertView; 
-	        
-	        button = new Button(mContext);
-	        button.setText(mButtons[position]);
-	        button.setGravity(Gravity.CENTER_HORIZONTAL);
-	        switch (position) {
-	        case 0:
-	        	button.setOnClickListener(new PlayClickHandler(mPlaylist));
-	        	break;
-	        case 1:
-	        	button.setOnClickListener(new ShuffleClickHandler(mPlaylist));
-	        	break;
-	        default:
-	        	button.setOnClickListener(new ViewFilesClickHandler(mPlaylist));
-	        	break;
-	        }
-	        return button;
-	    }
-		
-		private static class PlayClickHandler implements OnClickListener {
-			private JrPlaylist mPlaylist;
-			
-			public PlayClickHandler(JrPlaylist playlist) {
-				mPlaylist = playlist;
-			}
-			
-			@Override
-			public void onClick(View v) {
-				JrSession.playlist = mPlaylist.getFiles();
-				JrFile file = mPlaylist.getFiles().get(0);
-				Intent svcIntent = new Intent(StreamingMusicService.ACTION_PLAY, Uri.parse(file.getUrl()), v.getContext(), StreamingMusicService.class);
-				v.getContext().startService(svcIntent);
-			}
+		public ShuffleClickHandler(JrPlaylist playlist) {
+			mPlaylist = playlist;
 		}
 		
-		private static class ShuffleClickHandler implements OnClickListener {
-			private JrPlaylist mPlaylist;
-			
-			public ShuffleClickHandler(JrPlaylist playlist) {
-				mPlaylist = playlist;
-			}
-			
-			@Override
-			public void onClick(View v) {
-				JrSession.playlist = mPlaylist.getFiles(JrPlaylist.GET_SHUFFLED);
-				JrFile file = mPlaylist.getFiles().get(0);
-				Intent svcIntent = new Intent(StreamingMusicService.ACTION_PLAY, Uri.parse(file.getUrl()), v.getContext(), StreamingMusicService.class);
-				v.getContext().startService(svcIntent);
-			}
+		@Override
+		public void onClick(View v) {
+			JrSession.playlist = mPlaylist.getFiles(JrPlaylist.GET_SHUFFLED);
+			JrFile file = mPlaylist.getFiles().get(0);
+			Intent svcIntent = new Intent(StreamingMusicService.ACTION_PLAY, Uri.parse(file.getUrl()), v.getContext(), StreamingMusicService.class);
+			v.getContext().startService(svcIntent);
+		}
+	}
+	
+	private static class ViewFilesClickHandler implements OnClickListener {
+		private JrPlaylist mPlaylist;
+		
+		public ViewFilesClickHandler(JrPlaylist playlist) {
+			mPlaylist = playlist;
 		}
 		
-		private static class ViewFilesClickHandler implements OnClickListener {
-			private JrPlaylist mPlaylist;
-			
-			public ViewFilesClickHandler(JrPlaylist playlist) {
-				mPlaylist = playlist;
-			}
-			
-			@Override
-			public void onClick(View v) {
-	    		Intent intent = new Intent(v.getContext(), ViewFiles.class);
-	    		intent.setAction(ViewFiles.VIEW_ITEM_FILES);
-	    		intent.putExtra(ViewFiles.KEY, mPlaylist.getKey());
-	    		JrSession.selectedItem = mPlaylist;
-	    		v.getContext().startActivity(intent);
-			}
+		@Override
+		public void onClick(View v) {
+    		Intent intent = new Intent(v.getContext(), ViewFiles.class);
+    		intent.setAction(ViewFiles.VIEW_ITEM_FILES);
+    		intent.putExtra(ViewFiles.KEY, mPlaylist.getKey());
+    		JrSession.selectedItem = mPlaylist;
+    		v.getContext().startActivity(intent);
 		}
 	}
 }
