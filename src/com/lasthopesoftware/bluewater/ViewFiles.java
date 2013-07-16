63dcb090d5f5c1341216338d7743dd43600daf9c
heisthemachine <dvedvick@devedcomputers.com>
2013-07-15 21:13:21 -0400

    Added async fetch for file and playlist views.

--- src/com/lasthopesoftware/bluewater/ViewFiles.java
+++ src/com/lasthopesoftware/bluewater/ViewFiles.java
@@ -1,15 +1,24 @@
 package com.lasthopesoftware.bluewater;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jrFileSystem.IJrDataTask.OnCompleteListener;
 import jrFileSystem.IJrFilesContainer;
 import jrFileSystem.IJrItemFiles;
 import jrFileSystem.IJrItem;
+import jrFileSystem.JrFile;
+import jrFileSystem.JrFiles;
 import jrFileSystem.JrItem;
 import jrFileSystem.JrPlaylist;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 
 public class ViewFiles extends FragmentActivity {
 
@@ -19,23 +28,44 @@ public class ViewFiles extends FragmentActivity {
 	public static final String VIEW_PLAYLIST_FILES = "view_playlist_files";
 	
 	private IJrItem<?> mItem;
+	private Context mContext;
 	
-	
+	private ProgressBar pbLoading;
+	private ListView fileListView;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        mContext = this;
         getActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_view_files);
+        fileListView = (ListView)findViewById(R.id.lvFilelist);
+        pbLoading = (ProgressBar)findViewById(R.id.pbLoadingFileList);
+        
+        fileListView.setVisibility(View.INVISIBLE);
+        pbLoading.setVisibility(View.VISIBLE);
+        
         mItem = this.getIntent().getAction().equals(VIEW_PLAYLIST_FILES) ? new JrPlaylist(this.getIntent().getIntExtra(KEY, 1)) :
         				new JrItem(this.getIntent().getIntExtra(KEY, 1));
         this.setTitle(this.getIntent().getStringExtra(VALUE));
-        FileListAdapter fileListAdapter = new FileListAdapter(this, ((IJrFilesContainer)mItem).getJrFiles().getFiles());
-    	
-    	ListView fileListView = (ListView)findViewById(R.id.lvFilelist);
-    	fileListView.setOnItemClickListener(new ClickFileListener(this, ((IJrFilesContainer)mItem).getJrFiles()));
-    	fileListView.setAdapter(fileListAdapter);
+        JrFiles filesContainer = (JrFiles)((IJrFilesContainer)mItem).getJrFiles();
+        
+        filesContainer.setOnFilesCompleteListener(new OnCompleteListener<List<JrFile>>() {
+			
+			@Override
+			public void onComplete(List<JrFile> result) {
+				ArrayList<JrFile> innerResult =  (ArrayList<JrFile>) result;
+				FileListAdapter fileListAdapter = new FileListAdapter(mContext, innerResult);
+		    			    	
+		    	fileListView.setOnItemClickListener(new ClickFileListener(mContext, ((IJrFilesContainer)mItem).getJrFiles()));
+		    	fileListView.setAdapter(fileListAdapter);
+		    	
+		    	fileListView.setVisibility(View.VISIBLE);
+		        pbLoading.setVisibility(View.INVISIBLE);
+			}
+		});
+        
+        filesContainer.getFilesAsync();
 	}
 	
 	@Override
--- src/com/lasthopesoftware/bluewater/ViewPlaylists.java
+++ src/com/lasthopesoftware/bluewater/ViewPlaylists.java
@@ -1,36 +1,64 @@
 package com.lasthopesoftware.bluewater;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jrAccess.JrSession;
+import jrFileSystem.IJrDataTask.OnCompleteListener;
+import jrFileSystem.JrFile;
+import jrFileSystem.JrFiles;
 import jrFileSystem.JrPlaylist;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 
 public class ViewPlaylists extends FragmentActivity {
 
 	public static final String KEY = "key";   
+	private JrPlaylist mPlaylist;
+	
+	private ProgressBar pbLoading;
+	private ListView playlistView;
+	
+	private Context mContext;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_playlists);      
-        JrPlaylist playlist = (JrPlaylist) JrSession.selectedItem;
-        ListAdapter adapter;
-        ListView playlistView = (ListView)findViewById(R.id.lvPlaylist);
-        if (playlist.getSubItems().size() > 0) {
-        	adapter = new PlaylistAdapter(this, playlist.getSubItems());
-        	playlistView.setOnItemClickListener(new ClickPlaylistListener(this, playlist.getSubItems()));
+        setContentView(R.layout.activity_view_playlists);
+        mContext = this;
+        playlistView = (ListView)findViewById(R.id.lvPlaylist);
+        pbLoading = (ProgressBar)findViewById(R.id.pbLoadingPlaylist);
+        
+        mPlaylist = (JrPlaylist) JrSession.selectedItem;
+                
+        if (mPlaylist.getSubItems().size() > 0) {
+        	playlistView.setAdapter(new PlaylistAdapter(this, mPlaylist.getSubItems()));
+        	playlistView.setOnItemClickListener(new ClickPlaylistListener(this, mPlaylist.getSubItems()));
         	playlistView.setOnItemLongClickListener(new BrowseItemMenu.ClickListener());
         } else {
-        	adapter = new FileListAdapter(this, playlist.getJrFiles().getFiles());
-        	playlistView.setOnItemClickListener(new ClickFileListener(this, playlist.getJrFiles()));
+        	playlistView.setVisibility(View.INVISIBLE);
+        	pbLoading.setVisibility(View.VISIBLE);
+        	JrFiles filesContainer = (JrFiles)mPlaylist.getJrFiles();
+        	filesContainer.setOnFilesCompleteListener(new OnCompleteListener<List<JrFile>>() {
+				
+				@Override
+				public void onComplete(List<JrFile> result) {
+					playlistView.setAdapter(new FileListAdapter(mContext, (ArrayList<JrFile>) result));
+		        	playlistView.setOnItemClickListener(new ClickFileListener(mContext, mPlaylist.getJrFiles()));
+		        	
+		        	playlistView.setVisibility(View.VISIBLE);
+		        	pbLoading.setVisibility(View.INVISIBLE);
+				}
+			});
+        	filesContainer.getFilesAsync();
         }
-        	
-        
-        playlistView.setAdapter(adapter);
 	}
 	
 	@Override
