f215217f653d5c7807c9e130d2c2bddcfa8fa6e2
heisthemachine <dvedvick@devedcomputers.com>
2013-10-04 22:44:47 -0400

    Made saving/fetching preferences synchronized with each other

--- src/com/lasthopesoftware/bluewater/data/access/JrSession.java
+++ src/com/lasthopesoftware/bluewater/data/access/JrSession.java
@@ -53,63 +53,69 @@ public class JrSession {
 
 	private static TreeMap<String, IJrItem> mCategories;
 	private static ArrayList<IJrItem> mCategoriesList;
+	
+	private static Object syncObject = new Object();
 
 	public static void SaveSession(Context context) {
 		SaveSession(context.getSharedPreferences(PREFS_FILE, 0).edit());
 	}
 
-	public static synchronized void SaveSession(SharedPreferences.Editor prefsEditor) {
-		prefsEditor.putString(ACCESS_CODE_KEY, AccessCode);
-		prefsEditor.putString(USER_AUTH_CODE_KEY, UserAuthCode);
-		prefsEditor.putBoolean(IS_LOCAL_ONLY, IsLocalOnly);
-		prefsEditor.putInt(LIBRARY_KEY, LibraryKey);
-
-		if (Playlist != null) {
-			prefsEditor.putString(PLAYLIST_KEY, Playlist);
+	public static void SaveSession(SharedPreferences.Editor prefsEditor) {
+		synchronized(syncObject) {
+			prefsEditor.putString(ACCESS_CODE_KEY, AccessCode);
+			prefsEditor.putString(USER_AUTH_CODE_KEY, UserAuthCode);
+			prefsEditor.putBoolean(IS_LOCAL_ONLY, IsLocalOnly);
+			prefsEditor.putInt(LIBRARY_KEY, LibraryKey);
+	
+			if (Playlist != null) {
+				prefsEditor.putString(PLAYLIST_KEY, Playlist);
+			}
+	
+			if (PlayingFile != null) {
+				prefsEditor.putInt(NOW_PLAYING_KEY, PlayingFile.getKey());
+				prefsEditor.putInt(NP_POSITION, PlayingFile.getCurrentPosition());
+			}
+	
+			prefsEditor.apply();
 		}
-
-		if (PlayingFile != null) {
-			prefsEditor.putInt(NOW_PLAYING_KEY, PlayingFile.getKey());
-			prefsEditor.putInt(NP_POSITION, PlayingFile.getCurrentPosition());
-		}
-
-		prefsEditor.apply();
 	}
 
 	public static boolean CreateSession(Context context) {
 		return CreateSession(context.getSharedPreferences(PREFS_FILE, 0));
 	}
 
-	public static synchronized boolean CreateSession(SharedPreferences prefs) {
-		AccessCode = prefs.getString(ACCESS_CODE_KEY, "");
-		UserAuthCode = prefs.getString(USER_AUTH_CODE_KEY, "");
-		IsLocalOnly = prefs.getBoolean(IS_LOCAL_ONLY, false);
-		LibraryKey = prefs.getInt(LIBRARY_KEY, -1);
-
-		if (JrSession.AccessCode == null || JrSession.AccessCode.isEmpty() || !tryConnection()) return false;
-
-		Active = true;
-
-		try {
-			Playlist = prefs.getString(PLAYLIST_KEY, "");
-		} catch (ClassCastException ce) {
-			Playlist = null;
+	public static boolean CreateSession(SharedPreferences prefs) {
+		synchronized(syncObject) {
+			AccessCode = prefs.getString(ACCESS_CODE_KEY, "");
+			UserAuthCode = prefs.getString(USER_AUTH_CODE_KEY, "");
+			IsLocalOnly = prefs.getBoolean(IS_LOCAL_ONLY, false);
+			LibraryKey = prefs.getInt(LIBRARY_KEY, -1);
+	
+			if (JrSession.AccessCode == null || JrSession.AccessCode.isEmpty() || !tryConnection()) return false;
+	
+			Active = true;
+	
+			try {
+				Playlist = prefs.getString(PLAYLIST_KEY, "");
+			} catch (ClassCastException ce) {
+				Playlist = null;
+				return Active;
+			}
+	
+			int savedFileKey = prefs.getInt(NOW_PLAYING_KEY, -1);
+			int savedFilePos = prefs.getInt(NP_POSITION, -1);
+	
+	
+			if (savedFileKey < 0) return Active;
+			String savedFileKeyString = String.valueOf(savedFileKey);
+			for (String fileKey : Playlist.split(";")) {
+				if (!savedFileKeyString.equals(fileKey)) continue;
+				PlayingFile = new JrFile(savedFileKey);
+				if (savedFilePos > -1) PlayingFile.seekTo(savedFilePos);
+			}
+	
 			return Active;
 		}
-
-		int savedFileKey = prefs.getInt(NOW_PLAYING_KEY, -1);
-		int savedFilePos = prefs.getInt(NP_POSITION, -1);
-
-
-		if (savedFileKey < 0) return Active;
-		String savedFileKeyString = String.valueOf(savedFileKey);
-		for (String fileKey : Playlist.split(";")) {
-			if (!savedFileKeyString.equals(fileKey)) continue;
-			PlayingFile = new JrFile(savedFileKey);
-			if (savedFilePos > -1) PlayingFile.seekTo(savedFilePos);
-		}
-
-		return Active;
 	}
 
 	public static TreeMap<String, IJrItem> getCategories() {
