a66e456704ae672f06150d3a6b140be3bec9d56e
heisthemachine <dvedvick@devedcomputers.com>
2014-01-20 23:51:01 -0500

    Actual initial views objects

---
+++ src/com/lasthopesoftware/bluewater/data/sqlite/objects/View.java
@@ -0,0 +1,30 @@
+package com.lasthopesoftware.bluewater.data.sqlite.objects;
+
+public class View {
+	private int key;
+	private String name;
+	/**
+	 * @return the key
+	 */
+	public int getKey() {
+		return key;
+	}
+	/**
+	 * @param key the key to set
+	 */
+	public void setKey(int key) {
+		this.key = key;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+}
