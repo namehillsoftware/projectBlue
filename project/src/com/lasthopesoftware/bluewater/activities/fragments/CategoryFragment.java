b2bc4c86333e5a8371f0ed1aaf488b50f6cdc0c7
david <david@david-X370>
2014-10-26 13:01:01 -0500

    Clean-up categoryfragment code

--- project/src/com/lasthopesoftware/bluewater/activities/fragments/CategoryFragment.java
+++ project/src/com/lasthopesoftware/bluewater/activities/fragments/CategoryFragment.java
@@ -39,32 +39,21 @@ import com.lasthopesoftware.threading.ISimpleTask;
 import com.lasthopesoftware.threading.SimpleTaskState;
 
 public class CategoryFragment extends Fragment {
-	private IItem<?> mCategory;
-	private ListView listView;
-	private ProgressBar pbLoading;
-	private RelativeLayout mLayout;
-	
-	private Intent mWaitForConnection;
+	private final Intent mWaitForConnection = new Intent(getActivity(), WaitForConnection.class);
 	
     public static final String ARG_CATEGORY_POSITION = "category_position";
     public static final String IS_PLAYLIST = "Playlist";
 	
-    public CategoryFragment() {
-    	super();
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    	mLayout = new RelativeLayout(getActivity());
-    	mLayout.setLayoutParams(new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+    	final RelativeLayout layout = new RelativeLayout(getActivity());
+    	layout.setLayoutParams(new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
     	
-    	mWaitForConnection = new Intent(getActivity(), WaitForConnection.class);
-    	
-    	pbLoading = new ProgressBar(mLayout.getContext(), null, android.R.attr.progressBarStyleLarge);
+    	final ProgressBar pbLoading = new ProgressBar(getActivity(), null, android.R.attr.progressBarStyleLarge);
     	RelativeLayout.LayoutParams pbParams = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
     	pbParams.addRule(RelativeLayout.CENTER_IN_PARENT);
     	pbLoading.setLayoutParams(pbParams);
-    	mLayout.addView(pbLoading);
+    	layout.addView(pbLoading);
     	
     	LibrarySession.JrFs.getVisibleViewsAsync(new ISimpleTask.OnCompleteListener<String, Void, ArrayList<IItem<?>>>() {
 			
@@ -90,124 +79,130 @@ public class CategoryFragment extends Fragment {
 				}
 				
 				if (result == null) return;
-				mCategory = result.get(getArguments().getInt(ARG_CATEGORY_POSITION));
-				BuildView();
+				final IItem<?> category = result.get(getArguments().getInt(ARG_CATEGORY_POSITION));
+				
+				layout.addView(category instanceof Playlists ? BuildPlaylistView((Playlists)category, pbLoading) : BuildStandardItemView(category, pbLoading));
 			}
 		});
     	
-        return mLayout;
+        return layout;
     }
-    
-    private void BuildView() {
-    	if (mCategory instanceof Playlists) {
-    		listView = new ListView(getActivity());
-    		listView.setVisibility(View.INVISIBLE);
-    		OnCompleteListener<List<Playlist>> onPlaylistCompleteListener = new OnCompleteListener<List<Playlist>>() {
-				
-				@Override
-				public void onComplete(ISimpleTask<String, Void, List<Playlist>> owner, List<Playlist> result) {
-					if (owner.getState() == SimpleTaskState.ERROR) {
-						for (Exception exception : owner.getExceptions()) {
-							if (!(exception instanceof IOException)) continue;
-							
-							PollConnection.Instance.get(getActivity()).addOnConnectionRegainedListener(new OnConnectionRegainedListener() {
-								
-								@Override
-								public void onConnectionRegained() {
-									((Playlists) mCategory).getSubItemsAsync();
-								}
-							});
-							PollConnection.Instance.get(getActivity()).startPolling();
-							getActivity().startActivity(mWaitForConnection);
-							break;
-						}
-						return;
-					}
-					
-					if (result == null) return;
-					
-					listView.setOnItemClickListener(new ClickPlaylistListener(getActivity(), (ArrayList<Playlist>) result));
-					listView.setOnItemLongClickListener(new LongClickFlipListener());
-		    		listView.setAdapter(new PlaylistAdapter(getActivity(), R.id.tvStandard, result));
-		    		pbLoading.setVisibility(View.INVISIBLE);
-		    		listView.setVisibility(View.VISIBLE);					
-				}
-			};
-			((Playlists) mCategory).setOnItemsCompleteListener(onPlaylistCompleteListener);
-			((Playlists) mCategory).getSubItemsAsync();
-    	} else {
-	    	listView = new ExpandableListView(getActivity());
-	    	listView.setVisibility(View.INVISIBLE);
-	    	
-	    	OnCompleteListener<List<Item>> onItemCompleteListener = new OnCompleteListener<List<Item>>() {
 
-				@Override
-				public void onComplete(ISimpleTask<String, Void, List<Item>> owner, List<Item> result) {
-					if (owner.getState() == SimpleTaskState.ERROR) {
-						for (Exception exception : owner.getExceptions()) {
-							if (!(exception instanceof IOException)) continue;
-							
-							PollConnection.Instance.get(getActivity()).addOnConnectionRegainedListener(new OnConnectionRegainedListener() {
-								
-								@Override
-								public void onConnectionRegained() {
-									((Item)mCategory).getSubItemsAsync();
-								}
-							});
-							PollConnection.Instance.get(getActivity()).startPolling();
-							getActivity().startActivity(mWaitForConnection);
-							break;
-						}
-						return;
-					}
-					
-					if (result == null) return;
-					
-					((ExpandableListView)listView).setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+	private ListView BuildPlaylistView(final Playlists category, final View loadingView) {
+    	
+		final ListView listView = new ListView(getActivity());
+		listView.setVisibility(View.INVISIBLE);
+		OnCompleteListener<List<Playlist>> onPlaylistCompleteListener = new OnCompleteListener<List<Playlist>>() {
+			
+			@Override
+			public void onComplete(ISimpleTask<String, Void, List<Playlist>> owner, List<Playlist> result) {
+				if (owner.getState() == SimpleTaskState.ERROR) {
+					for (Exception exception : owner.getExceptions()) {
+						if (!(exception instanceof IOException)) continue;
 						
-						@Override
-						public boolean onGroupClick(ExpandableListView parent, View v,
-								int groupPosition, long id) {
-							final Item selection = (Item)parent.getExpandableListAdapter().getGroup(groupPosition);
-							try {
-								if (selection.getSubItems().size() > 0) return false;
-							} catch (IOException e) {
-								LoggerFactory.getLogger(CategoryFragment.class).warn(e.getMessage(), e);
-								return true;
+						PollConnection.Instance.get(getActivity()).addOnConnectionRegainedListener(new OnConnectionRegainedListener() {
+							
+							@Override
+							public void onConnectionRegained() {
+								((Playlists) category).getSubItemsAsync();
 							}
-							final Intent intent = new Intent(parent.getContext(), ViewFiles.class);
-				    		intent.setAction(ViewFiles.VIEW_ITEM_FILES);
-				    		intent.putExtra(ViewFiles.KEY, selection.getKey());
-				    		intent.putExtra(ViewFiles.VALUE, selection.getValue());
-				    		startActivity(intent);
-				    		return true;
-						}
-					});
-			    	((ExpandableListView)listView).setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
-			    	    @Override
-			    	    public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {        	    	
-			    	    	final Item selection = (Item)parent.getExpandableListAdapter().getChild(groupPosition, childPosition);
-				    		final Intent intent = new Intent(parent.getContext(), ViewFiles.class);
-				    		intent.setAction(ViewFiles.VIEW_ITEM_FILES);
-				    		intent.putExtra(ViewFiles.KEY, selection.getKey());
-				    		intent.putExtra(ViewFiles.VALUE, selection.getValue());
-				    		startActivity(intent);
-			    	        return true;
-			    	    }
-				    });
-			    	listView.setOnItemLongClickListener(new LongClickFlipListener());
-			    	
-			    	((ExpandableListView)listView).setAdapter(new ExpandableItemListAdapter((ArrayList<Item>)result));
-			    	pbLoading.setVisibility(View.INVISIBLE);
-		    		listView.setVisibility(View.VISIBLE);
+						});
+						PollConnection.Instance.get(getActivity()).startPolling();
+						getActivity().startActivity(mWaitForConnection);
+						break;
+					}
+					return;
 				}
-			};
-			((Item)mCategory).setOnItemsCompleteListener(onItemCompleteListener);
-			((Item)mCategory).getSubItemsAsync();
-    	}
-    	mLayout.addView(listView);
+				
+				if (result == null) return;
+				
+				listView.setOnItemClickListener(new ClickPlaylistListener(getActivity(), (ArrayList<Playlist>) result));
+				listView.setOnItemLongClickListener(new LongClickFlipListener());
+	    		listView.setAdapter(new PlaylistAdapter(getActivity(), R.id.tvStandard, result));
+	    		loadingView.setVisibility(View.INVISIBLE);
+	    		listView.setVisibility(View.VISIBLE);					
+			}
+		};
+		category.setOnItemsCompleteListener(onPlaylistCompleteListener);
+		category.getSubItemsAsync();
+	
+		return listView;
     }
-    
+
+	private ExpandableListView BuildStandardItemView(final IItem<?> category, final View loadingView) {
+		final ExpandableListView listView = new ExpandableListView(getActivity());
+    	listView.setVisibility(View.INVISIBLE);
+    	
+    	OnCompleteListener<List<Item>> onItemCompleteListener = new OnCompleteListener<List<Item>>() {
+
+			@Override
+			public void onComplete(ISimpleTask<String, Void, List<Item>> owner, List<Item> result) {
+				if (owner.getState() == SimpleTaskState.ERROR) {
+					for (Exception exception : owner.getExceptions()) {
+						if (!(exception instanceof IOException)) continue;
+						
+						PollConnection.Instance.get(getActivity()).addOnConnectionRegainedListener(new OnConnectionRegainedListener() {
+							
+							@Override
+							public void onConnectionRegained() {
+								((Item)category).getSubItemsAsync();
+							}
+						});
+						PollConnection.Instance.get(getActivity()).startPolling();
+						getActivity().startActivity(mWaitForConnection);
+						break;
+					}
+					return;
+				}
+				
+				if (result == null) return;
+				
+				listView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+					
+					@Override
+					public boolean onGroupClick(ExpandableListView parent, View v,
+							int groupPosition, long id) {
+						final Item selection = (Item)parent.getExpandableListAdapter().getGroup(groupPosition);
+						try {
+							if (selection.getSubItems().size() > 0) return false;
+						} catch (IOException e) {
+							LoggerFactory.getLogger(CategoryFragment.class).warn(e.getMessage(), e);
+							return true;
+						}
+						final Intent intent = new Intent(parent.getContext(), ViewFiles.class);
+			    		intent.setAction(ViewFiles.VIEW_ITEM_FILES);
+			    		intent.putExtra(ViewFiles.KEY, selection.getKey());
+			    		intent.putExtra(ViewFiles.VALUE, selection.getValue());
+			    		startActivity(intent);
+			    		return true;
+					}
+				});
+		    	listView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+		    	    @Override
+		    	    public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {        	    	
+		    	    	final Item selection = (Item)parent.getExpandableListAdapter().getChild(groupPosition, childPosition);
+			    		final Intent intent = new Intent(parent.getContext(), ViewFiles.class);
+			    		intent.setAction(ViewFiles.VIEW_ITEM_FILES);
+			    		intent.putExtra(ViewFiles.KEY, selection.getKey());
+			    		intent.putExtra(ViewFiles.VALUE, selection.getValue());
+			    		startActivity(intent);
+		    	        return true;
+		    	    }
+			    });
+		    	listView.setOnItemLongClickListener(new LongClickFlipListener());
+		    	
+		    	((ExpandableListView)listView).setAdapter(new ExpandableItemListAdapter((ArrayList<Item>)result));
+		    	loadingView.setVisibility(View.INVISIBLE);
+	    		listView.setVisibility(View.VISIBLE);
+			}
+		};
+		((Item)category).setOnItemsCompleteListener(onItemCompleteListener);
+		((Item)category).getSubItemsAsync();
+		
+		return listView;
+	}
+		
+
     public static class ExpandableItemListAdapter extends BaseExpandableListAdapter {
     	private final ArrayList<Item> mCategoryItems;
     	
