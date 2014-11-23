9b08dde74152504e31be73aa519311ec209cfd0d
david <david@david-desktop>
2014-11-22 20:20:52 -0600

    Improve logging

--- project/src/com/lasthopesoftware/bluewater/MainApplication.java
+++ project/src/com/lasthopesoftware/bluewater/MainApplication.java
@@ -7,7 +7,9 @@ import org.slf4j.LoggerFactory;
 
 import android.annotation.SuppressLint;
 import android.app.Application;
+import android.os.Environment;
 import android.os.StrictMode;
+import ch.qos.logback.classic.AsyncAppender;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.android.LogcatAppender;
@@ -15,6 +17,7 @@ import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.rolling.RollingFileAppender;
 import ch.qos.logback.core.rolling.TimeBasedRollingPolicy;
+import ch.qos.logback.core.util.StatusPrinter;
 
 import com.lasthopesoftware.bluewater.exceptions.LoggerUncaughtExceptionHandler;
 
@@ -48,27 +51,49 @@ public class MainApplication extends Application {
 	   
 	    rootLogger.addAppender(logcatAppender);
 
-		final PatternLayoutEncoder filePle = new PatternLayoutEncoder();
+	    if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+	    	final File logDir = new File(getExternalFilesDir(null).getPath() + File.separator + "logs");
+		    if (!logDir.exists())
+		    	logDir.mkdirs();
+		    
+			final PatternLayoutEncoder filePle = new PatternLayoutEncoder();
+	        filePle.setPattern("%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n");
+	        filePle.setContext(lc);
+	        filePle.start();
+		    
+		    final RollingFileAppender<ILoggingEvent> rollingFileAppender = new RollingFileAppender<ILoggingEvent>();
+		    rollingFileAppender.setLazy(true);
+		    rollingFileAppender.setAppend(true);
+		    rollingFileAppender.setContext(lc);
+		    rollingFileAppender.setEncoder(filePle);
+		    
+		    final TimeBasedRollingPolicy<ILoggingEvent> rollingPolicy = new TimeBasedRollingPolicy<ILoggingEvent>();
+		    rollingPolicy.setFileNamePattern(new File(logDir, "%d{yyyy-MM-dd}.log").getAbsolutePath());
+		    rollingPolicy.setMaxHistory(30);
+		    rollingPolicy.setParent(rollingFileAppender);  // parent and context required!
+		    rollingPolicy.setContext(lc);
+		    rollingPolicy.start();
+	
+		    rollingFileAppender.setRollingPolicy(rollingPolicy);
+		    rollingFileAppender.start();
+		    
+		    final AsyncAppender asyncAppender = new AsyncAppender();
+		    asyncAppender.setContext(lc);
+		    asyncAppender.setName("ASYNC");
 
-        filePle.setPattern("%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n");
-        filePle.setContext(lc);
-        filePle.start();
-        
-	    final RollingFileAppender<ILoggingEvent> rollingFileAppender = new RollingFileAppender<ILoggingEvent>();
-	    rollingFileAppender.setAppend(true);
-	    rollingFileAppender.setContext(lc);
-	    rollingFileAppender.setEncoder(filePle);
+		    // UNCOMMENT TO TWEAK OPTIONAL SETTINGS
+//		    // excluding caller data (used for stack traces) improves appender's performance
+//		    asyncAppender.setIncludeCallerData(false);
+//		    // set threshold to 0 to disable discarding and keep all events
+//		    asyncAppender.setDiscardingThreshold(0);
+//		    asyncAppender.setQueueSize(256);
 
-	    final TimeBasedRollingPolicy<ILoggingEvent> rollingPolicy = new TimeBasedRollingPolicy<ILoggingEvent>();
-	    rollingPolicy.setFileNamePattern(new File(getDir("logs", MODE_PRIVATE), "%d{yyyy-MM-dd}.log").getAbsolutePath());
-	    rollingPolicy.setMaxHistory(30);
-	    rollingPolicy.setParent(rollingFileAppender);  // parent and context required!
-	    rollingPolicy.setContext(lc);
-	    rollingPolicy.start();
-
-	    rollingFileAppender.setRollingPolicy(rollingPolicy);
-	    
-	    rootLogger.addAppender(rollingFileAppender);
+		    asyncAppender.addAppender(rollingFileAppender);
+		    asyncAppender.start();
+		    
+		    rootLogger.addAppender(asyncAppender);
+		    StatusPrinter.print(lc);
+	    }
 	    
 		final Logger logger = LoggerFactory.getLogger(getClass());
 		logger.info("Uncaught exceptions logging to custom uncaught exception handler."); 
