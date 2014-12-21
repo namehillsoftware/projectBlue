2eae8ae6e79749227b1df5518334ac1c1eded3c1
david <david@david-X370>
2014-12-21 13:27:21 -0600

    Add mock file player for unit testing

---
+++ test/src/com/lasthopesoftware/bluewater/test/mock/MockFilePlayer.java
@@ -0,0 +1,118 @@
+package com.lasthopesoftware.bluewater.test.mock;
+
+import java.io.IOException;
+
+import com.lasthopesoftware.bluewater.data.service.helpers.playback.IFilePlayer;
+import com.lasthopesoftware.bluewater.data.service.objects.File;
+
+public class MockFilePlayer implements IFilePlayer {
+
+	@Override
+	public File getFile() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void initMediaPlayer() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isMediaPlayerCreated() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isPrepared() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void prepareMediaPlayer() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void prepareMpSynchronously() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void releaseMediaPlayer() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getCurrentPosition() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isBuffered() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getBufferPercentage() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getDuration() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isPlaying() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void seekTo(int pos) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public float getVolume() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setVolume(float volume) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
