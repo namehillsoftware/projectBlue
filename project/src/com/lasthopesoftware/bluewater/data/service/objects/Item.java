c8a26637c36ce60da9946626be718e3b1c9bd770
David <dvedvick@gmail.com>
2015-02-11 21:35:16 -0600

    Remove commented out code

--- project/src/com/lasthopesoftware/bluewater/data/service/objects/Item.java
+++ project/src/com/lasthopesoftware/bluewater/data/service/objects/Item.java
@@ -23,51 +23,6 @@ public class Item extends ItemAsyncBase implements IItem, IFilesContainer {
 		if (mFiles == null) mFiles = new Files("Browse/Files", "ID=" + String.valueOf(this.getKey()));
 		return mFiles;
 	}
-//
-//	@Override
-//	public void addOnItemsCompleteListener(OnCompleteListener<List<Item>> listener) {
-//		if (mOnCompleteListeners == null) mOnCompleteListeners = new ArrayList<OnCompleteListener<List<Item>>>();
-//		
-//		mOnCompleteListeners.add(listener);
-//	}
-//
-//	@Override
-//	public void removeOnItemsCompleteListener(OnCompleteListener<List<Item>> listener) {
-//		if (mOnCompleteListeners != null)
-//			mOnCompleteListeners.remove(listener);
-//	}
-//
-//	@Override
-//	public void setOnItemsStartListener(OnStartListener<List<Item>> listener) {
-//		if (mItemStartListeners.size() < 1) mItemStartListeners.add(listener); 
-//		mItemStartListeners.set(0, listener);
-//	}
-//	
-//	@Override
-//	public void setOnItemsErrorListener(OnErrorListener<List<Item>> listener) {
-//		if (mItemErrorListeners.size() < 1) mItemErrorListeners.add(listener);
-//		mItemErrorListeners.set(0, listener);
-//	}
-//
-//	@Override
-//	protected OnConnectListener<List<Item>> getOnItemConnectListener() {
-//		return mItemConnectListener;
-//	}
-//
-//	@Override
-//	protected List<OnCompleteListener<List<Item>>> getOnItemsCompleteListeners() {
-//		return mOnCompleteListeners;
-//	}
-//
-//	@Override
-//	protected List<OnStartListener<List<Item>>> getOnItemsStartListeners() {
-//		return mItemStartListeners;
-//	}
-//
-//	@Override
-//	protected List<OnErrorListener<List<Item>>> getOnItemsErrorListeners() {
-//		return mItemErrorListeners;
-//	}
 
 	@Override
 	public String[] getSubItemParams() {
