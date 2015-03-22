c70bd5ac514b4aec7b0e1fe8414cff6e31887ca1
David <dvedvick@gmail.com>
2015-03-22 12:18:34 -0500

    Add progress bar for loading tab views

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/servers/library/BrowseLibraryActivity.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/servers/library/BrowseLibraryActivity.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 
 import com.astuetz.PagerSlidingTabStrip;
 import com.lasthopesoftware.bluewater.R;
@@ -37,39 +38,42 @@ import java.util.List;
 
 public class BrowseLibraryActivity extends FragmentActivity {
 
-	private static final String SAVED_TAB_KEY = "com.lasthopesoftware.bluewater.activities.BrowseLibrary.SAVED_TAB_KEY";
-	private static final String SAVED_SCROLL_POS = "com.lasthopesoftware.bluewater.activities.BrowseLibrary.SAVED_SCROLL_POS";
-	
+	private static final String SAVED_TAB_KEY = "com.lasthopesoftware.bluewater.servers.library.BrowseLibraryActivity.SAVED_TAB_KEY";
+	private static final String SAVED_SCROLL_POS = "com.lasthopesoftware.bluewater.servers.library.BrowseLibraryActivity.SAVED_SCROLL_POS";
+    private static final String SAVED_SELECTED_VIEW = "com.lasthopesoftware.bluewater.servers.library.BrowseLibraryActivity.SAVED_SELECTED_VIEW";
+
 	/**
 	 * The {@link ViewPager} that will host the section contents.
 	 */
 	private ViewPager mViewPager;
 	private ListView mLvSelectViews;
 	private DrawerLayout mDrawerLayout;
+    private PagerSlidingTabStrip mLibraryViewsTabs;
+    private ProgressBar mPbLoadingViews;
 
 	private ActionBarDrawerToggle mDrawerToggle = null;
-	
+
 	private BrowseLibraryActivity mBrowseLibrary = this;
-	
+
 	private CharSequence mOldTitle;
-	
+
 	private boolean mIsStopped = false;
-	
+
 	private OnCompleteListener<String, Void, ArrayList<IItem>> mOnGetVisibleViewsCompleteListener;
-		
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_browse_library);
-		
+
 		setTitle(R.string.title_activity_library);
-		
+
 		getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setHomeButtonEnabled(true);
-		
+
 		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 		mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-		
+
 		mOldTitle = getTitle();
 		mDrawerToggle = new ActionBarDrawerToggle(
                 this,                  /* host Activity */
@@ -96,44 +100,48 @@ public class BrowseLibraryActivity extends FragmentActivity {
             }
 
 		};
-		
+
 		mDrawerLayout.setDrawerListener(mDrawerToggle);
 		mLvSelectViews = (ListView) findViewById(R.id.lvLibraryViewSelection);
-		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mViewPager = (ViewPager) findViewById(R.id.libraryViewPager);
+        mLibraryViewsTabs = (PagerSlidingTabStrip) findViewById(R.id.tabsLibraryViews);
+        mPbLoadingViews = (ProgressBar) findViewById(R.id.pbLoadingViews);
+
+        if (savedInstanceState != null) restoreScrollPosition(savedInstanceState);
 	}
-	
+
 	@Override
 	public void onStart() {
 		super.onStart();
-		
+
 		if (!InstantiateSessionConnectionActivity.restoreSessionConnection(this)) getLibrary();
 	}
-	
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (requestCode != InstantiateSessionConnectionActivity.ACTIVITY_ID) return;
-				
-		getLibrary();
+		if (requestCode == InstantiateSessionConnectionActivity.ACTIVITY_ID) getLibrary();
 	}
-	
+
 	private void getLibrary() {
 		mIsStopped = false;
 		if ((mLvSelectViews.getAdapter() != null && mViewPager.getAdapter() != null)) return;
-		
+
+        toggleViewsVisibility(false);
+
 		LibrarySession.GetLibrary(mBrowseLibrary, new OnCompleteListener<Integer, Void, Library>() {
-			
+
 			@Override
 			public void onComplete(ISimpleTask<Integer, Void, Library> owner, final Library result) {
 				FileSystem.Instance.get(mBrowseLibrary, new OnGetFileSystemCompleteListener() {
-					
+
 					@Override
 					public void onGetFileSystemComplete(FileSystem fileSystem) {
 						displayLibrary(result, fileSystem);
 					}
-				});	
+				});
 			}
 		});
-				
+
 	}
 
 	@SuppressWarnings("unchecked")
@@ -141,56 +149,56 @@ public class BrowseLibraryActivity extends FragmentActivity {
 		final LibraryViewsProvider libraryViewsProvider = new LibraryViewsProvider();
 
         libraryViewsProvider.onComplete(new OnCompleteListener<Void, Void, List<Item>>() {
-			
+
 			@Override
 			public void onComplete(ISimpleTask<Void, Void, List<Item>> owner, final List<Item> items) {
 				if (mIsStopped || items == null) return;
-					
+
 				for (IItem item : items) {
 					if (item.getKey() != library.getSelectedView()) continue;
 					mOldTitle = item.getValue();
 					getActionBar().setTitle(mOldTitle);
 					break;
 				}
-				
+
 				mLvSelectViews.setAdapter(new SelectViewAdapter(mLvSelectViews.getContext(), R.layout.layout_select_views, items, library.getSelectedView()));
-				
+
 				fileSystem.getVisibleViewsAsync(getOnVisibleViewsCompleteListener(),
 					new HandleViewIoException(mBrowseLibrary, new OnConnectionRegainedListener() {
 
 						@Override
 						public void onConnectionRegained() {
 							FileSystem.Instance.get(mBrowseLibrary, new OnGetFileSystemCompleteListener() {
-								
+
 								@Override
 								public void onGetFileSystemComplete(FileSystem fileSystem) {
 									fileSystem.getVisibleViewsAsync(getOnVisibleViewsCompleteListener());
 								}
 							});
 						}
-					
+
 				}));
-				
+
 				mLvSelectViews.setOnItemClickListener(new OnItemClickListener() {
-					
+
 					@Override
 					public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 						mDrawerLayout.closeDrawer(Gravity.START);
 						mDrawerToggle.syncState();
-						
+
 						final int selectedViewKey = items.get(position).getKey();
-												
+
 						LibrarySession.GetLibrary(mBrowseLibrary, new OnCompleteListener<Integer, Void, Library>() {
-							
+
 							@Override
 							public void onComplete(ISimpleTask<Integer, Void, Library> owner, final Library library) {
 								if (library.getSelectedView() == selectedViewKey) return;
-								
+
 								library.setSelectedView(selectedViewKey);
 								LibrarySession.SaveLibrary(mBrowseLibrary, library);
-								
+
 								FileSystem.Instance.get(mBrowseLibrary, new OnGetFileSystemCompleteListener() {
-									
+
 									@Override
 									public void onGetFileSystemComplete(FileSystem fileSystem) {
 										displayLibrary(library, fileSystem);
@@ -202,11 +210,11 @@ public class BrowseLibraryActivity extends FragmentActivity {
 				});
 			}
 		}).onError(new HandleViewIoException(mBrowseLibrary, new OnConnectionRegainedListener() {
-			
+
 			@Override
 			public void onConnectionRegained() {
 				FileSystem.Instance.get(mBrowseLibrary, new OnGetFileSystemCompleteListener() {
-					
+
 					@Override
 					public void onGetFileSystemComplete(FileSystem fileSystem) {
                         libraryViewsProvider.execute();
@@ -217,26 +225,28 @@ public class BrowseLibraryActivity extends FragmentActivity {
 
         libraryViewsProvider.execute(AsyncTask.THREAD_POOL_EXECUTOR);
 	}
-	
+
 	private OnCompleteListener<String, Void, ArrayList<IItem>> getOnVisibleViewsCompleteListener() {
-		if (mOnGetVisibleViewsCompleteListener == null) {
-			mOnGetVisibleViewsCompleteListener = new OnCompleteListener<String, Void, ArrayList<IItem>>() {
-				
-				@Override
-				public void onComplete(ISimpleTask<String, Void, ArrayList<IItem>> owner, ArrayList<IItem> result) {
-					if (mIsStopped || result == null) return;
-					
-					final LibraryViewPagerAdapter viewChildPagerAdapter = new LibraryViewPagerAdapter(getSupportFragmentManager());
-					viewChildPagerAdapter.setLibraryViews(result);
-		
-					// Set up the ViewPager with the sections adapter.
-					mViewPager.setAdapter(viewChildPagerAdapter);
-					((PagerSlidingTabStrip) findViewById(R.id.tabsLibraryViews)).setViewPager(mViewPager);
-				}
-			};
-		}
-		
-		return mOnGetVisibleViewsCompleteListener;
+		if (mOnGetVisibleViewsCompleteListener != null) return mOnGetVisibleViewsCompleteListener;
+
+        mOnGetVisibleViewsCompleteListener = new OnCompleteListener<String, Void, ArrayList<IItem>>() {
+
+            @Override
+            public void onComplete(ISimpleTask<String, Void, ArrayList<IItem>> owner, ArrayList<IItem> result) {
+                if (mIsStopped || result == null) return;
+
+                final LibraryViewPagerAdapter viewChildPagerAdapter = new LibraryViewPagerAdapter(getSupportFragmentManager());
+                viewChildPagerAdapter.setLibraryViews(result);
+
+                // Set up the ViewPager with the sections adapter.
+                mViewPager.setAdapter(viewChildPagerAdapter);
+                mLibraryViewsTabs.setViewPager(mViewPager);
+
+                toggleViewsVisibility(true);
+            }
+        };
+
+        return mOnGetVisibleViewsCompleteListener;
 	}
 
 	@Override
@@ -248,42 +258,72 @@ public class BrowseLibraryActivity extends FragmentActivity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		if (mDrawerToggle != null && mDrawerToggle.onOptionsItemSelected(item))
 			return true;
-		
+
 		return ViewUtils.handleMenuClicks(this, item);
 	}
-	
+
 	@Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         // Sync the toggle state after onRestoreInstanceState has occurred.
         if (mDrawerToggle != null) mDrawerToggle.syncState();
     }
-	
+
 	@Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         if (mDrawerToggle != null) mDrawerToggle.onConfigurationChanged(newConfig);
     }
-	
+
 	@Override
-	public void onSaveInstanceState(Bundle savedInstanceState) {
+	public void onSaveInstanceState(final Bundle savedInstanceState) {
 		super.onSaveInstanceState(savedInstanceState);
-		
-		if (mViewPager != null) {
-			savedInstanceState.putInt(SAVED_TAB_KEY, mViewPager.getCurrentItem());
-			savedInstanceState.putInt(SAVED_SCROLL_POS, mViewPager.getScrollY());
-		}
+
+		if (mViewPager == null) return;
+
+        savedInstanceState.putInt(SAVED_TAB_KEY, mViewPager.getCurrentItem());
+        savedInstanceState.putInt(SAVED_SCROLL_POS, mViewPager.getScrollY());
+        LibrarySession.GetLibrary(this, new OnCompleteListener<Integer, Void, Library>() {
+            @Override
+            public void onComplete(ISimpleTask<Integer, Void, Library> owner, Library library) {
+                savedInstanceState.putInt(SAVED_SELECTED_VIEW, library.getSelectedView());
+            }
+        });
 	}
-	
+
 	@Override
-	public void onRestoreInstanceState(Bundle savedInstanceState) {
+	public void onRestoreInstanceState(final Bundle savedInstanceState) {
 		super.onRestoreInstanceState(savedInstanceState);
-		
-		if (mViewPager != null) {
-			mViewPager.setCurrentItem(savedInstanceState.getInt(SAVED_TAB_KEY));
-			mViewPager.setScrollY(savedInstanceState.getInt(SAVED_SCROLL_POS));
-		}
+
+        if (savedInstanceState != null) restoreScrollPosition(savedInstanceState);
 	}
+
+    private void restoreScrollPosition(final Bundle savedInstanceState) {
+        if (mViewPager == null) return;
+
+        LibrarySession.GetLibrary(this, new OnCompleteListener<Integer, Void, Library>() {
+
+            @Override
+            public void onComplete(ISimpleTask<Integer, Void, Library> owner, Library library) {
+                final int savedSelectedView = savedInstanceState.getInt(SAVED_SELECTED_VIEW, -1);
+                if (savedSelectedView < 0 || savedSelectedView != library.getSelectedView()) return;
+
+                final int savedTabKey = savedInstanceState.getInt(SAVED_TAB_KEY, -1);
+                if (savedTabKey > -1)
+                    mViewPager.setCurrentItem(savedTabKey);
+
+                final int savedScrollPosition = savedInstanceState.getInt(SAVED_SCROLL_POS, -1);
+                if (savedScrollPosition > -1)
+                    mViewPager.setScrollY(savedScrollPosition);
+            }
+        });
+    }
+
+    private void toggleViewsVisibility(boolean isVisible) {
+        mLibraryViewsTabs.setVisibility(isVisible ? View.VISIBLE : View.INVISIBLE);
+        mViewPager.setVisibility(isVisible ? View.VISIBLE : View.INVISIBLE);
+        mPbLoadingViews.setVisibility(isVisible ? View.INVISIBLE : View.VISIBLE);
+    }
 	
 	@Override
 	public void onStop() {
