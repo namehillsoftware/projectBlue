933ddd99c94319c384ee818a314454689a03a089
David <dvedvick@gmail.com>
2015-09-13 16:28:05 -0500

    Add sync button to view active downloads

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/view/ViewUtils.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/view/ViewUtils.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 import com.lasthopesoftware.bluewater.R;
 import com.lasthopesoftware.bluewater.disk.sqlite.access.LibrarySession;
 import com.lasthopesoftware.bluewater.servers.ServerListActivity;
+import com.lasthopesoftware.bluewater.servers.library.items.media.files.local.sync.activity.ActiveFileDownloadsActivity;
 import com.lasthopesoftware.bluewater.servers.library.items.media.files.nowplaying.NowPlayingActivity;
 import com.lasthopesoftware.bluewater.servers.library.items.media.files.playback.file.IPlaybackFile;
 import com.lasthopesoftware.bluewater.servers.library.items.media.files.playback.service.PlaybackController;
@@ -28,7 +29,7 @@ public class ViewUtils {
 
     private static boolean mIsNowPlayingVisible = false;
 
-	public final static boolean buildStandardMenu(final Activity activity, final Menu menu) {
+	public static boolean buildStandardMenu(final Activity activity, final Menu menu) {
 		activity.getMenuInflater().inflate(R.menu.menu_blue_water, menu);
 		
 		final MenuItem nowPlayingItem = menu.findItem(R.id.menu_view_now_playing);
@@ -56,7 +57,7 @@ public class ViewUtils {
 		        });
 	        }
         });
-		
+
 		final SearchManager searchManager = (SearchManager) activity.getSystemService(Context.SEARCH_SERVICE);
 	    final SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
 	    searchView.setSearchableInfo(searchManager.getSearchableInfo(activity.getComponentName()));
@@ -67,7 +68,7 @@ public class ViewUtils {
 		return true;
 	}
 	
-	public final static boolean handleMenuClicks(final Context context, final MenuItem item) {
+	public static boolean handleMenuClicks(final Context context, final MenuItem item) {
 		switch (item.getItemId()) {
 			case R.id.menu_connection_settings:
 				context.startActivity(new Intent(context, ServerListActivity.class));
@@ -75,12 +76,15 @@ public class ViewUtils {
 			case R.id.menu_view_now_playing:
 				CreateNowPlayingView(context);
 				return true;
+			case R.id.menu_view_active_downloads:
+				context.startActivity(new Intent(context, ActiveFileDownloadsActivity.class));
+				return true;
 			default:
 				return false;
 		}
 	}
 	
-	public final static boolean handleNavMenuClicks(Activity activity, MenuItem item) {
+	public static boolean handleNavMenuClicks(Activity activity, MenuItem item) {
 		switch (item.getItemId()) {
 			case android.R.id.home:
 				Intent upIntent = NavUtils.getParentActivityIntent(activity);
@@ -104,7 +108,7 @@ public class ViewUtils {
 		
 	}
 		
-	public final static void CreateNowPlayingView(final Context context) {
+	public static void CreateNowPlayingView(final Context context) {
     	final Intent viewIntent = new Intent(context, NowPlayingActivity.class);
 		viewIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
 		context.startActivity(viewIntent);
