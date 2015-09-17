bc3d4f4dabccd2331b3663408e7b50faf77b34b2
david <dvedvick@gmail.com>
2015-09-16 21:03:39 -0500

    Remove runnable implementation from `LibrarySyncHandler`, create runnable on demand when `StartSync` is called.

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/servers/library/items/media/files/local/sync/service/LibrarySyncHandler.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/servers/library/items/media/files/local/sync/service/LibrarySyncHandler.java
@@ -24,43 +24,18 @@ import java.util.Set;
 /**
  * Created by david on 8/30/15.
  */
-public class LibrarySyncHandler implements Runnable {
+public class LibrarySyncHandler {
 
 	private final Context context;
 	private final ConnectionProvider connectionProvider;
 	private final Library library;
 	private final StoredFileDownloader storedFileDownloader;
 
-	private List<StoredItem> storedItems;
-
 	public LibrarySyncHandler(Context context, ConnectionProvider connectionProvider, Library library) {
 		this.context = context;
 		this.connectionProvider = connectionProvider;
 		this.library = library;
 		this.storedFileDownloader = new StoredFileDownloader(context, connectionProvider, library);
-	}
-
-	@Override
-	public void run() {
-		final Set<Integer> allSyncedFileKeys = new HashSet<>();
-		final StoredFileAccess storedFileAccess = new StoredFileAccess(context, library);
-
-		for (StoredItem storedItem : storedItems) {
-			final int serviceId = storedItem.getServiceId();
-			final IFilesContainer filesContainer = storedItem.getItemType() == StoredItem.ItemType.ITEM ? new Item(connectionProvider, serviceId) : new Playlist(connectionProvider, serviceId);
-			final ArrayList<IFile> files = filesContainer.getFiles().getFiles();
-			for (final IFile file : files) {
-				allSyncedFileKeys.add(file.getKey());
-
-				final StoredFile storedFile = storedFileAccess.createOrUpdateFile(file);
-				if (!storedFile.isDownloadComplete())
-					storedFileDownloader.queueFileForDownload(file, storedFile);
-			}
-		}
-
-		storedFileDownloader.process();
-
-		storedFileAccess.pruneStoredFiles(allSyncedFileKeys);
 	}
 
 	public void setOnFileDownloaded(IOneParameterAction<StoredFile> onFileDownloaded) {
@@ -76,11 +51,33 @@ public class LibrarySyncHandler implements Runnable {
 		storedItemAccess.getStoredItems(new ISimpleTask.OnCompleteListener<Void, Void, List<StoredItem>>() {
 
 			@Override
-			public void onComplete(ISimpleTask<Void, Void, List<StoredItem>> owner, List<StoredItem> storedItems) {
-				LibrarySyncHandler.this.storedItems = storedItems;
+			public void onComplete(ISimpleTask<Void, Void, List<StoredItem>> owner, final List<StoredItem> storedItems) {
 				AsyncTask
 					.THREAD_POOL_EXECUTOR
-					.execute(LibrarySyncHandler.this);
+					.execute(new Runnable() {
+						@Override
+						public void run() {
+							final Set<Integer> allSyncedFileKeys = new HashSet<>();
+							final StoredFileAccess storedFileAccess = new StoredFileAccess(context, library);
+
+							for (StoredItem storedItem : storedItems) {
+								final int serviceId = storedItem.getServiceId();
+								final IFilesContainer filesContainer = storedItem.getItemType() == StoredItem.ItemType.ITEM ? new Item(connectionProvider, serviceId) : new Playlist(connectionProvider, serviceId);
+								final ArrayList<IFile> files = filesContainer.getFiles().getFiles();
+								for (final IFile file : files) {
+									allSyncedFileKeys.add(file.getKey());
+
+									final StoredFile storedFile = storedFileAccess.createOrUpdateFile(file);
+									if (!storedFile.isDownloadComplete())
+										storedFileDownloader.queueFileForDownload(file, storedFile);
+								}
+							}
+
+							storedFileDownloader.process();
+
+							storedFileAccess.pruneStoredFiles(allSyncedFileKeys);
+						}
+					});
 			}
 		});
 	}
