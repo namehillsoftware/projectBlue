b5abfcfb245d641f845241b23baf0be1b3132fd2
David <dvedvick@gmail.com>
2015-09-18 07:19:43 -0500

    Log when alarm to receive sync is received.

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/servers/library/items/media/files/local/sync/receivers/SyncAlarmBroadcastReceiver.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/servers/library/items/media/files/local/sync/receivers/SyncAlarmBroadcastReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.j256.ormlite.logger.LoggerFactory;
 import com.lasthopesoftware.bluewater.servers.library.items.media.files.local.sync.service.ItemSyncService;
 
 /**
@@ -14,6 +15,7 @@ public class SyncAlarmBroadcastReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
+		LoggerFactory.getLogger(getClass()).info("Received alarm to begin sync.");
 		ItemSyncService.doSync(context);
 	}
 }
