b9fe023e4595d4b01d034160d922821e5c07521b
David <dvedvick@gmail.com>
2015-10-11 20:03:54 -0500

    Fix merge issues

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/servers/library/items/media/files/local/sync/activity/adapter/viewholder/ActiveFileDownloadsViewHolder.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/servers/library/items/media/files/local/sync/activity/adapter/viewholder/ActiveFileDownloadsViewHolder.java
@@ -1,6 +1,5 @@
 package com.lasthopesoftware.bluewater.servers.library.items.media.files.local.sync.activity.adapter.viewholder;
 
-import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
 import com.lasthopesoftware.bluewater.servers.library.items.media.files.menu.GetFileListItemTextTask;
@@ -8,13 +7,12 @@ import com.lasthopesoftware.bluewater.servers.library.items.media.files.menu.Get
 /**
  * Created by david on 8/23/15.
  */
-public class ActiveFileDownloadsViewHolder extends RecyclerView.ViewHolder {
+public class ActiveFileDownloadsViewHolder {
 
 	public final TextView textView;
 	public GetFileListItemTextTask getFileListItemTextTask;
 
 	public ActiveFileDownloadsViewHolder(TextView textView) {
-		super(textView);
 
 		this.textView = textView;
 	}
