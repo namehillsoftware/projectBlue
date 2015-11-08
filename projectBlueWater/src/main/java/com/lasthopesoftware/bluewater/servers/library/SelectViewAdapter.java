3b6bb6cdfa7818002b0b5077963db525ff6fa177
David <dvedvick@gmail.com>
2015-11-08 13:07:16 -0600

    Show slight blue border on selected view

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/servers/library/SelectViewAdapter.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/servers/library/SelectViewAdapter.java
@@ -1,32 +1,28 @@
 package com.lasthopesoftware.bluewater.servers.library;
 
 import android.content.Context;
-import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.lasthopesoftware.bluewater.R;
 import com.lasthopesoftware.bluewater.servers.library.items.IItem;
 import com.lasthopesoftware.bluewater.servers.library.items.Item;
+import com.lasthopesoftware.bluewater.shared.view.ViewUtils;
 
 import java.util.List;
 
 public class SelectViewAdapter extends ArrayAdapter<Item> {
 
 	private final int selectedViewKey;
-	private static int selectedColor = -1;
 
 	public SelectViewAdapter(Context context, int resource, List<Item> views, final int selectedViewKey) {
 		super(context, resource, views);
 		
 		this.selectedViewKey = selectedViewKey;
-
-		final int colorResource = R.color.clearstream_dark;
-		if (selectedColor == -1)
-			selectedColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ? context.getColor(colorResource) : context.getResources().getColor(colorResource);
 	}
 	
 	@Override
@@ -40,10 +36,13 @@ public class SelectViewAdapter extends ArrayAdapter<Item> {
 		
 		final TextView tvViewName = (TextView) convertView.findViewById(R.id.tvViewName);
 		tvViewName.setText(item.getValue());
-		
-		if (item.getKey() == selectedViewKey)
-			tvViewName.setBackgroundColor(selectedColor);
-		
+
+		if (item.getKey() == selectedViewKey) {
+			RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(tvViewName.getLayoutParams());
+			layoutParams.setMargins(0, 0, ViewUtils.dpToPx(parent.getContext(), 10), 0);
+			tvViewName.setLayoutParams(layoutParams);
+		}
+
 		return convertView;
 	}
 }
