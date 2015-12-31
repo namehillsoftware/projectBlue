d7dfcbf7823b173114b7ead5e64383630524ec37
david <dvedvick@gmail.com>
2015-12-31 17:54:11 -0600

    Remove unused file

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/servers/library/items/media/files/stored/repository/StoredFileSql.java
+++
@@ -1,8 +0,0 @@
-package com.lasthopesoftware.bluewater.servers.library.items.media.files.stored.repository;
-
-/**
- * Created by david on 12/13/15.
- */
-public class StoredFileSql {
-	public final String selectStoredFileById = "SELECT * FROM STOREDFILE WHERE ID = @ID";
-}
