e278096c5d7d2d4bd7ce3f4205ddae11142d5ac0
david <dvedvick@gmail.com>
2016-01-12 21:49:07 -0600

    Keep duration in memory

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/servers/library/items/media/files/nowplaying/NowPlayingActivityProgressTrackerTask.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/servers/library/items/media/files/nowplaying/NowPlayingActivityProgressTrackerTask.java
@@ -4,6 +4,10 @@ import android.os.AsyncTask;
 import android.os.Message;
 
 import com.lasthopesoftware.bluewater.servers.library.items.media.files.playback.file.IPlaybackFile;
+import com.vedsoft.lazyj.Lazy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
@@ -11,29 +15,43 @@ import java.util.concurrent.Executors;
 
 
 public class NowPlayingActivityProgressTrackerTask extends AsyncTask<Void, Void, Void> {
-	private final IPlaybackFile mFilePlayer;
-	private final NowPlayingActivityMessageHandler mHandler;
-	
-	private static final ExecutorService mTrackerExecutor = Executors.newSingleThreadExecutor();
-	
+	private static final Logger logger = LoggerFactory.getLogger(NowPlayingActivityProgressTrackerTask.class);
+	private static final ExecutorService trackerExecutor = Executors.newSingleThreadExecutor();
+
+	private final IPlaybackFile filePlayer;
+
+	private final NowPlayingActivityMessageHandler handler;
+
+	private final Lazy<Integer> fileDuration = new Lazy<Integer>() {
+		@Override
+		protected Integer initialize() {
+			try {
+				return filePlayer.getDuration();
+			} catch (IOException e) {
+				logger.error("There was an error getting file duration");
+				return -1;
+			}
+		}
+	};
+
 	public static NowPlayingActivityProgressTrackerTask trackProgress(IPlaybackFile filePlayer, NowPlayingActivityMessageHandler handler) {
 		final NowPlayingActivityProgressTrackerTask newProgressTrackerThread = new NowPlayingActivityProgressTrackerTask(filePlayer, handler);
-		newProgressTrackerThread.executeOnExecutor(mTrackerExecutor);
+		newProgressTrackerThread.executeOnExecutor(trackerExecutor);
 		return newProgressTrackerThread;
 	}
 	
 	private NowPlayingActivityProgressTrackerTask(IPlaybackFile filePlayer, NowPlayingActivityMessageHandler handler) {
-		mFilePlayer = filePlayer;
-		mHandler = handler;
+		this.filePlayer = filePlayer;
+		this.handler = handler;
 	}
 	
 	@Override
 	protected Void doInBackground(Void... params) {
-		while (!isCancelled() && mFilePlayer != null && mFilePlayer.isMediaPlayerCreated()) {
+		while (!isCancelled() && filePlayer != null && filePlayer.isMediaPlayerCreated()) {
 			try {
 				
-				if (mFilePlayer.isPlaying())
-					mHandler.sendMessage(getUpdatePlayingMessage());
+				if (filePlayer.isPlaying())
+					handler.sendMessage(getUpdatePlayingMessage());
 				
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
@@ -45,14 +63,10 @@ public class NowPlayingActivityProgressTrackerTask extends AsyncTask<Void, Void,
 	}
 	
 	private Message getUpdatePlayingMessage() {
-		Message msg = new Message();
+		final Message msg = new Message();
 		msg.what = NowPlayingActivityMessageHandler.UPDATE_PLAYING;
-		msg.arg1 = mFilePlayer.getCurrentPosition();
-		try {
-			msg.arg2 = mFilePlayer.getDuration();
-		} catch (IOException e) {
-			msg.what = NowPlayingActivityMessageHandler.SHOW_CONNECTION_LOST;
-		}
+		msg.arg1 = filePlayer.getCurrentPosition();
+		msg.arg2 = fileDuration.getObject();
 		
 		return msg;
 	}
