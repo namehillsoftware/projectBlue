5be2f08972b1960d5868bf0708bdf73e6ba724b4
David <dvedvick@gmail.com>
2016-01-24 10:19:49 -0600

    Factor out `SelectServerOnClickListener`

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/servers/list/ServerListAdapter.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/servers/list/ServerListAdapter.java
@@ -3,7 +3,6 @@ package com.lasthopesoftware.bluewater.servers.list;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
 import android.support.v4.content.LocalBroadcastManager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,17 +14,16 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.lasthopesoftware.bluewater.R;
-import com.lasthopesoftware.bluewater.servers.library.BrowseLibraryActivity;
 import com.lasthopesoftware.bluewater.servers.library.repository.Library;
-import com.lasthopesoftware.bluewater.servers.library.repository.LibrarySession;
 import com.lasthopesoftware.bluewater.servers.list.listeners.EditServerClickListener;
+import com.lasthopesoftware.bluewater.servers.list.listeners.SelectServerOnClickListener;
 
 import java.util.List;
 
 public class ServerListAdapter extends BaseAdapter {
 
-	private final List<Library> mLibraries;
-	private final Activity mActivity;
+	private final List<Library> libraries;
+	private final Activity activity;
 
 	private static class ViewHolder {
 		public final TextView textView;
@@ -44,8 +42,8 @@ public class ServerListAdapter extends BaseAdapter {
 	public ServerListAdapter(Activity activity, List<Library> libraries) {
 		super();
 
-		mActivity = activity;
-		mLibraries = libraries;
+		this.activity = activity;
+		this.libraries = libraries;
 	}
 
 	@Override
@@ -55,7 +53,7 @@ public class ServerListAdapter extends BaseAdapter {
 			final RelativeLayout returnView = (RelativeLayout) getInflater(parentContext).inflate(R.layout.layout_standard_text, parent, false);
 			final TextView textView = (TextView) returnView.findViewById(R.id.tvStandard);
 			textView.setText(parentContext.getText(R.string.btn_add_server));
-			returnView.setOnClickListener(new EditServerClickListener(mActivity, -1));
+			returnView.setOnClickListener(new EditServerClickListener(activity, -1));
 
 			if (convertView != null && convertView.getTag() != null && ((ViewHolder)convertView.getTag()).broadcastReceiver != null)
 				LocalBroadcastManager.getInstance(parentContext).unregisterReceiver(((ViewHolder)convertView.getTag()).broadcastReceiver);
@@ -75,21 +73,14 @@ public class ServerListAdapter extends BaseAdapter {
 		}
 
 		final ViewHolder viewHolder = (ViewHolder) convertView.getTag();
-		final Library library = mLibraries.get(--position);
+		final Library library = libraries.get(--position);
 		viewHolder.textView.setText(library.getAccessCode());
 
 		final Button btnSelectServer = viewHolder.btnSelectServer;
 
-		btnSelectServer.setOnClickListener(v -> {
-			final Context context = v.getContext();
-			LibrarySession.ChangeActiveLibrary(context, library.getId(), null);
+		btnSelectServer.setOnClickListener(new SelectServerOnClickListener(library));
 
-			final Intent browseLibraryIntent = new Intent(context, BrowseLibraryActivity.class);
-			browseLibraryIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-			context.startActivity(browseLibraryIntent);
-		});
-
-		viewHolder.btnConfigureServer.setOnClickListener(new EditServerClickListener(mActivity, library.getId()));
+		viewHolder.btnConfigureServer.setOnClickListener(new EditServerClickListener(activity, library.getId()));
 
 		return convertView;
 	}
@@ -100,16 +91,17 @@ public class ServerListAdapter extends BaseAdapter {
 
 	@Override
 	public int getCount() {
-		return mLibraries.size() + 1;
+		return libraries.size() + 1;
 	}
 
 	@Override
 	public Object getItem(int position) {
-		return mLibraries.get(position);
+		return libraries.get(position);
 	}
 
 	@Override
 	public long getItemId(int position) {
-		return position > 0 ? mLibraries.get(--position).getId() : -1;
+		return position > 0 ? libraries.get(--position).getId() : -1;
 	}
+
 }
