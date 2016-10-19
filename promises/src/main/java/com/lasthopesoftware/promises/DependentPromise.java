b6eef36d2cb99dea6d5e352e8e9bc0ee6ebb68fa
David Vedvick <dvedvick@gmail.com>
2016-10-18 23:12:50 -0500

    Synchronize access to fulfill/resolve variables and handlers

--- promises/src/main/java/com/lasthopesoftware/promises/DependentPromise.java
+++ promises/src/main/java/com/lasthopesoftware/promises/DependentPromise.java
@@ -11,11 +11,16 @@ import org.jetbrains.annotations.Nullable;
 class DependentPromise<TInput, TResult> implements IPromise<TResult> {
 
 	private final FourParameterRunnable<TInput, Exception, OneParameterRunnable<TResult>, OneParameterRunnable<Exception>> executor;
+
 	private DependentPromise<TResult, ?> resolution;
 	private DependentPromise<Exception, ?> rejection;
+	private final Object resolutionSync = new Object();
+	private final Object rejectionSync = new Object();
 
 	private TResult fulfilledResult;
 	private Exception fulfilledError;
+	private final Object fulfilledResultSync = new Object();
+	private final Object fulfilledErrorSync = new Object();
 
 	DependentPromise(@NotNull FourParameterRunnable<TInput, Exception, OneParameterRunnable<TResult>, OneParameterRunnable<Exception>> executor) {
 		this.executor = executor;
@@ -23,20 +28,30 @@ class DependentPromise<TInput, TResult> implements IPromise<TResult> {
 
 	final void provide(@Nullable TInput input, @Nullable Exception exception) {
 		this.executor.run(input, exception, result -> {
-			fulfilledResult = result;
-
-			if (resolution != null)
-				resolution.provide(result, null);
-		}, error -> {
-			fulfilledError = error;
-
-			if (rejection != null) {
-				rejection.provide(error, null);
-				return;
+			synchronized (fulfilledResultSync) {
+				fulfilledResult = result;
 			}
 
-			if (resolution != null)
-				resolution.provide(null, error);
+			synchronized (resolutionSync) {
+				if (resolution != null)
+					resolution.provide(result, null);
+			}
+		}, error -> {
+			synchronized (fulfilledErrorSync) {
+				fulfilledError = error;
+			}
+
+			synchronized (rejectionSync) {
+				if (rejection != null) {
+					rejection.provide(error, null);
+					return;
+				}
+			}
+
+			synchronized (resolutionSync) {
+				if (resolution != null)
+					resolution.provide(null, error);
+			}
 		});
 	}
 
@@ -45,10 +60,19 @@ class DependentPromise<TInput, TResult> implements IPromise<TResult> {
 	public <TNewResult> IPromise<TNewResult> then(@NotNull ThreeParameterRunnable<TResult, OneParameterRunnable<TNewResult>, OneParameterRunnable<Exception>> onFulfilled) {
 		final DependentPromise<TResult, TNewResult> newResolution = new DependentPromise<>(new ErrorPropagatingResolveExecutor<>(onFulfilled));
 
-		resolution = newResolution;
+		synchronized (resolutionSync) {
+			resolution = newResolution;
+		}
 
-		if (fulfilledResult != null || fulfilledError != null)
-			newResolution.provide(fulfilledResult, fulfilledError);
+		synchronized (fulfilledResultSync) {
+			if (fulfilledResult != null)
+				newResolution.provide(fulfilledResult, null);
+		}
+
+		synchronized (fulfilledErrorSync) {
+			if (fulfilledError != null)
+				newResolution.provide(null, fulfilledError);
+		}
 
 		return newResolution;
 	}
@@ -70,10 +94,14 @@ class DependentPromise<TInput, TResult> implements IPromise<TResult> {
 	public <TNewRejectedResult> IPromise<TNewRejectedResult> error(@NotNull ThreeParameterRunnable<Exception, OneParameterRunnable<TNewRejectedResult>, OneParameterRunnable<Exception>> onRejected) {
 		final DependentPromise<Exception, TNewRejectedResult> newRejection = new DependentPromise<>(new ErrorPropagatingResolveExecutor<>(onRejected));
 
-		rejection = newRejection;
+		synchronized (rejectionSync) {
+			rejection = newRejection;
+		}
 
-		if (fulfilledError != null)
-			newRejection.provide(fulfilledError, null);
+		synchronized (fulfilledErrorSync) {
+			if (fulfilledError != null)
+				newRejection.provide(fulfilledError, null);
+		}
 
 		return newRejection;
 	}
