75424f8db48811d9060a8e490baca9c614774fa9
David Vedvick <dvedvick@gmail.com>
2016-10-20 07:23:34 -0500

    Add a unit test for tracking the resolved state

---
+++ promises/src/main/java/com/lasthopesoftware/promises/specs/GivenAPromiseThatResolves/WithANullResult/WhenThePromiseIsFulfilled.java
@@ -0,0 +1,27 @@
+package com.lasthopesoftware.promises.specs.GivenAPromiseThatResolves.WithANullResult;
+
+import com.lasthopesoftware.promises.ExpectedPromise;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by david on 10/20/16.
+ */
+
+public class WhenThePromiseIsFulfilled {
+
+	private Object result;
+
+	@Before
+	public void before() {
+		new ExpectedPromise<>(() -> null)
+				.then(result -> { this.result = result; });
+	}
+
+	@Test
+	public void thenTheResultIsNull() {
+		Assert.assertNull(result);
+	}
+}
