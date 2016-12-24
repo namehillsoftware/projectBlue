adc53d67dcf23a51887ad0e6b56056516f8e1593
David Vedvick <dvedvick@gmail.com>
2016-12-24 16:45:33 -0600

    Flesh out unit tests more

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/library/items/playlists/playback/specs/GivenAStandardPlaybackQueueProvider/WhenSwitchingBetweenANonCyclicAndACyclicQueueWhileAFileIsPlaying.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/library/items/playlists/playback/specs/GivenAStandardPlaybackQueueProvider/WhenSwitchingBetweenANonCyclicAndACyclicQueueWhileAFileIsPlaying.java
@@ -2,10 +2,12 @@ package com.lasthopesoftware.bluewater.client.library.items.playlists.playback.s
 
 import com.lasthopesoftware.bluewater.client.library.items.media.files.playback.file.IPlaybackHandler;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -16,6 +18,12 @@ import static org.mockito.Mockito.verify;
 public class WhenSwitchingBetweenANonCyclicAndACyclicQueueWhileAFileIsPlaying {
 
 	private IPlaybackHandler playbackHandler;
+	private IPlaybackHandler expectedPlaybackHandler;
+
+	@BeforeClass
+	public static void before() {
+
+	}
 
 	@Test
 	public void thenThePlaybackOfTheCurrentFileIsNeverPaused() {
@@ -25,5 +33,15 @@ public class WhenSwitchingBetweenANonCyclicAndACyclicQueueWhileAFileIsPlaying {
 	@Test
 	public void thenTheCurrentPlaybackHandlerIsNeverClosed() throws IOException {
 		verify(this.playbackHandler, times(0)).close();
+	}
+
+	@Test
+	public void thenTheCurrentPlaybackHandlerIsPlaying() {
+		assertThat(this.playbackHandler.isPlaying()).isTrue();
+	}
+	
+	@Test
+	public void thenThePlaybackHandlerRemainsTheSame() {
+		assertThat(this.playbackHandler).isEqualTo(this.expectedPlaybackHandler);
 	}
 }
