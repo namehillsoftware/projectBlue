ff993ec73561c4c83e587bba92913c4ef5e692da
david <dvedvick@gmail.com>
2017-02-19 20:31:11 -0600

    Remove `LibrarySession` from `ActiveFileDownloadsFragment`

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/library/access/IChosenLibraryIdentifierProvider.java
+++
@@ -1,9 +0,0 @@
-package com.lasthopesoftware.bluewater.client.library.access;
-
-/**
- * Created by david on 2/12/17.
- */
-
-public interface IChosenLibraryIdentifierProvider {
-	int getChosenLibraryId();
-}
--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/library/items/media/files/stored/fragment/ActiveFileDownloadsFragment.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/library/items/media/files/stored/fragment/ActiveFileDownloadsFragment.java
@@ -16,16 +16,24 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 
+import com.annimon.stream.Collectors;
+import com.annimon.stream.Stream;
 import com.lasthopesoftware.bluewater.R;
+import com.lasthopesoftware.bluewater.client.library.access.ISpecificLibraryProvider;
+import com.lasthopesoftware.bluewater.client.library.access.LibraryRepository;
+import com.lasthopesoftware.bluewater.client.library.access.SpecificLibraryProvider;
 import com.lasthopesoftware.bluewater.client.library.items.media.files.File;
 import com.lasthopesoftware.bluewater.client.library.items.media.files.IFile;
 import com.lasthopesoftware.bluewater.client.library.items.media.files.stored.StoredFileAccess;
 import com.lasthopesoftware.bluewater.client.library.items.media.files.stored.fragment.adapter.ActiveFileDownloadsAdapter;
 import com.lasthopesoftware.bluewater.client.library.items.media.files.stored.repository.StoredFile;
-import com.lasthopesoftware.bluewater.client.library.repository.LibrarySession;
+import com.lasthopesoftware.bluewater.client.servers.selection.SelectedBrowserLibraryIdentifierProvider;
+import com.lasthopesoftware.bluewater.shared.promises.resolutions.Dispatch;
 import com.lasthopesoftware.bluewater.sync.service.SyncService;
+import com.vedsoft.futures.callables.VoidFunc;
+import com.vedsoft.lazyj.AbstractThreadLocalLazy;
+import com.vedsoft.lazyj.ILazy;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -38,6 +46,16 @@ public class ActiveFileDownloadsFragment extends Fragment {
     private BroadcastReceiver onFileQueuedReceiver;
     private BroadcastReceiver onFileDownloadedReceiver;
     private LocalBroadcastManager localBroadcastManager;
+
+	private ILazy<ISpecificLibraryProvider> lazyLibraryProvider = new AbstractThreadLocalLazy<ISpecificLibraryProvider>() {
+		@Override
+		protected ISpecificLibraryProvider initialize() throws Exception {
+			final LibraryRepository libraryRepository = new LibraryRepository(getActivity());
+			return new SpecificLibraryProvider(
+				new SelectedBrowserLibraryIdentifierProvider(getActivity()).getSelectedLibraryId(),
+				libraryRepository);
+		}
+	};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -57,77 +75,79 @@ public class ActiveFileDownloadsFragment extends Fragment {
 		listView.setVisibility(View.INVISIBLE);
 		progressBar.setVisibility(View.VISIBLE);
 
-		LibrarySession.getActiveLibrary(getActivity(), library -> {
-			final StoredFileAccess storedFileAccess = new StoredFileAccess(getActivity(), library);
-			storedFileAccess.getDownloadingStoredFiles()
-				.onComplete((storedFiles) -> {
-					final ArrayList<StoredFile> localStoredFiles = new ArrayList<>(storedFiles.size());
-					for (StoredFile storedFile : storedFiles) {
-						if (storedFile.getLibraryId() == library.getId())
-							localStoredFiles.add(storedFile);
-					}
+		lazyLibraryProvider
+			.getObject()
+			.getLibrary()
+			.then(Dispatch.toContext(VoidFunc.runningCarelessly(library -> {
+				final StoredFileAccess storedFileAccess = new StoredFileAccess(getActivity(), library);
+				storedFileAccess.getDownloadingStoredFiles()
+					.onComplete((storedFiles) -> {
+						final List<StoredFile> localStoredFiles =
+							Stream.of(storedFiles)
+								.filter(f -> f.getLibraryId() == library.getId())
+								.collect(Collectors.toList());
 
-					final ActiveFileDownloadsAdapter activeFileDownloadsAdapter = new ActiveFileDownloadsAdapter(getActivity(), localStoredFiles);
+						final ActiveFileDownloadsAdapter activeFileDownloadsAdapter = new ActiveFileDownloadsAdapter(getActivity(), localStoredFiles);
 
-					if (onFileDownloadedReceiver != null)
-						localBroadcastManager.unregisterReceiver(onFileDownloadedReceiver);
+						if (onFileDownloadedReceiver != null)
+							localBroadcastManager.unregisterReceiver(onFileDownloadedReceiver);
 
-					onFileDownloadedReceiver = new BroadcastReceiver() {
-						@Override
-						public void onReceive(Context context, Intent intent) {
-							final int storedFileId = intent.getIntExtra(SyncService.storedFileEventKey, -1);
+						onFileDownloadedReceiver = new BroadcastReceiver() {
+							@Override
+							public void onReceive(Context context, Intent intent) {
+								final int storedFileId = intent.getIntExtra(SyncService.storedFileEventKey, -1);
 
-							for (StoredFile storedFile : localStoredFiles) {
-								if (storedFile.getId() != storedFileId) continue;
+								for (StoredFile storedFile : localStoredFiles) {
+									if (storedFile.getId() != storedFileId) continue;
 
-								final List<IFile> files = activeFileDownloadsAdapter.getFiles();
-								for (IFile file : files) {
-									if (file.getKey() != storedFile.getServiceId()) continue;
+									final List<IFile> files = activeFileDownloadsAdapter.getFiles();
+									for (IFile file : files) {
+										if (file.getKey() != storedFile.getServiceId()) continue;
 
-									activeFileDownloadsAdapter.remove(file);
-									files.remove(file);
+										activeFileDownloadsAdapter.remove(file);
+										files.remove(file);
+										break;
+									}
+
 									break;
 								}
-
-								break;
 							}
-						}
-					};
+						};
 
-					localBroadcastManager.registerReceiver(onFileDownloadedReceiver, new IntentFilter(SyncService.onFileDownloadedEvent));
+						localBroadcastManager.registerReceiver(onFileDownloadedReceiver, new IntentFilter(SyncService.onFileDownloadedEvent));
 
-					if (onFileQueuedReceiver != null)
-						localBroadcastManager.unregisterReceiver(onFileQueuedReceiver);
+						if (onFileQueuedReceiver != null)
+							localBroadcastManager.unregisterReceiver(onFileQueuedReceiver);
 
-					onFileQueuedReceiver = new BroadcastReceiver() {
-						@Override
-						public void onReceive(Context context, Intent intent) {
-							final int storedFileId = intent.getIntExtra(SyncService.storedFileEventKey, -1);
-							if (storedFileId == -1) return;
+						onFileQueuedReceiver = new BroadcastReceiver() {
+							@Override
+							public void onReceive(Context context, Intent intent) {
+								final int storedFileId = intent.getIntExtra(SyncService.storedFileEventKey, -1);
+								if (storedFileId == -1) return;
 
-							for (StoredFile storedFile : localStoredFiles) {
-								if (storedFile.getId() == storedFileId) return;
+								for (StoredFile storedFile : localStoredFiles) {
+									if (storedFile.getId() == storedFileId) return;
+								}
+
+								storedFileAccess
+										.getStoredFile(storedFileId)
+										.onComplete((storedFile) -> {
+											if (storedFile == null || storedFile.getLibraryId() != library.getId()) return;
+
+											localStoredFiles.add(storedFile);
+											activeFileDownloadsAdapter.add(new File(storedFile.getServiceId()));
+										});
 							}
+						};
 
-							storedFileAccess
-									.getStoredFile(storedFileId)
-									.onComplete((storedFile) -> {
-										if (storedFile == null || storedFile.getLibraryId() != library.getId()) return;
+						localBroadcastManager.registerReceiver(onFileQueuedReceiver, new IntentFilter(SyncService.onFileQueuedEvent));
 
-										localStoredFiles.add(storedFile);
-										activeFileDownloadsAdapter.add(new File(storedFile.getServiceId()));
-									});
-						}
-					};
+						listView.setAdapter(activeFileDownloadsAdapter);
 
-					localBroadcastManager.registerReceiver(onFileQueuedReceiver, new IntentFilter(SyncService.onFileQueuedEvent));
-
-					listView.setAdapter(activeFileDownloadsAdapter);
-
-					progressBar.setVisibility(View.INVISIBLE);
-					listView.setVisibility(View.VISIBLE);
-				});
-		});
+						progressBar.setVisibility(View.INVISIBLE);
+						listView.setVisibility(View.VISIBLE);
+					});
+		}), getActivity()));
 
 		final Button toggleSyncButton = (Button) viewFileslayout.findViewById(R.id.toggleSyncButton);
 		final CharSequence startSyncLabel = getActivity().getText(R.string.start_sync_button);
