dff90acdaec9d505ff88a3ed52933f25f663d2a9
david <dvedvick@gmail.com>
2017-03-23 23:00:20 -0500

    Test cancelling a promise before it is executed

---
+++ promises/src/specs/java/com/lasthopesoftware/promises/GivenAPromiseThatIsCancelled/BeforeThePromiseIsExecuted/WhenTheCancellationIsCalled.java
@@ -0,0 +1,58 @@
+package com.lasthopesoftware.promises.GivenAPromiseThatIsCancelled.BeforeThePromiseIsExecuted;
+
+import com.lasthopesoftware.promises.IPromise;
+import com.lasthopesoftware.promises.IRejectedPromise;
+import com.lasthopesoftware.promises.IResolvedPromise;
+import com.lasthopesoftware.promises.Promise;
+import com.vedsoft.futures.runnables.TwoParameterAction;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by david on 10/17/16.
+ */
+
+public class WhenTheCancellationIsCalled {
+
+	private static Throwable thrownException;
+	private static Throwable caughtException;
+
+	@BeforeClass
+	public static void before() {
+		thrownException = new Exception();
+		final ExternallyResolvableTask<String> resolvableTask = new ExternallyResolvableTask<>();
+		final Promise<String> promise = new Promise<>(resolvableTask);
+
+		IPromise<Object> cancellablePromise = promise.then(
+			(result, resolve, reject, onCancelled) -> onCancelled.runWith(() -> reject.withError(thrownException)));
+
+		cancellablePromise.error((exception, onCancelled) -> caughtException = exception);
+
+		cancellablePromise.cancel();
+
+		resolvableTask.resolve("");
+	}
+
+	@Test
+	public void thenTheRejectionIsCorrect() {
+		assertThat(thrownException).isEqualTo(caughtException);
+	}
+
+	private static class ExternallyResolvableTask<TResult> implements TwoParameterAction<IResolvedPromise<TResult>, IRejectedPromise> {
+
+		private IResolvedPromise<TResult> resolve;
+
+		public void resolve(TResult resolution) {
+			if (resolve != null)
+				resolve.withResult(resolution);
+		}
+
+		@Override
+		public void runWith(IResolvedPromise<TResult> parameterOne, IRejectedPromise parameterTwo) {
+			resolve = parameterOne;
+		}
+	}
+}
