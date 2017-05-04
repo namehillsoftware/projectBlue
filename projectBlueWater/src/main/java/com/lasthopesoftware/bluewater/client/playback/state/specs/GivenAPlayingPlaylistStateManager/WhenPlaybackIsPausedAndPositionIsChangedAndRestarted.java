886f461e988f44d0cfafd7eaa71d9ff205ca578a
david <dvedvick@gmail.com>
2017-05-03 22:32:15 -0500

    Test pausing, skipping and then playing a file

---
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/state/specs/GivenAPlayingPlaylistStateManager/WhenPlaybackIsPausedAndPositionIsChangedAndRestarted.java
@@ -0,0 +1,124 @@
+package com.lasthopesoftware.bluewater.client.playback.state.specs.GivenAPlayingPlaylistStateManager;
+
+import com.lasthopesoftware.bluewater.client.library.access.ILibraryStorage;
+import com.lasthopesoftware.bluewater.client.library.access.ISpecificLibraryProvider;
+import com.lasthopesoftware.bluewater.client.library.items.media.files.ServiceFile;
+import com.lasthopesoftware.bluewater.client.library.items.media.files.nowplaying.storage.NowPlaying;
+import com.lasthopesoftware.bluewater.client.library.items.media.files.nowplaying.storage.NowPlayingRepository;
+import com.lasthopesoftware.bluewater.client.library.items.media.files.playback.file.PositionedPlaybackFile;
+import com.lasthopesoftware.bluewater.client.library.items.media.files.playback.file.preparation.specs.fakes.FakeDeferredPlaybackPreparerProvider;
+import com.lasthopesoftware.bluewater.client.library.items.playlists.playback.specs.GivenAStandardPreparedPlaylistProvider.WithAStatefulPlaybackHandler.ThatCanFinishPlayback.ResolveablePlaybackHandler;
+import com.lasthopesoftware.bluewater.client.library.repository.Library;
+import com.lasthopesoftware.bluewater.client.playback.queues.CompletingFileQueueProvider;
+import com.lasthopesoftware.bluewater.client.playback.state.PlaylistManager;
+import com.lasthopesoftware.bluewater.client.playback.state.volume.PlaylistVolumeManager;
+import com.lasthopesoftware.promises.Promise;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.concurrent.CountDownLatch;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by david on 3/12/17.
+ */
+
+public class WhenPlaybackIsPausedAndPositionIsChangedAndRestarted {
+
+	private static PlaylistManager playlistManager;
+	private static NowPlaying nowPlaying;
+	private static PositionedPlaybackFile mostRecentPositionedFile;
+
+	@BeforeClass
+	public static void before() throws InterruptedException {
+		final FakeDeferredPlaybackPreparerProvider fakePlaybackPreparerProvider = new FakeDeferredPlaybackPreparerProvider();
+
+		final Library library = new Library();
+		library.setId(1);
+
+		final ISpecificLibraryProvider libraryProvider = mock(ISpecificLibraryProvider.class);
+		when(libraryProvider.getLibrary()).thenReturn(new Promise<>(library));
+
+		final ILibraryStorage libraryStorage = mock(ILibraryStorage.class);
+		when(libraryStorage.saveLibrary(any())).thenReturn(new Promise<>(library));
+
+		final NowPlayingRepository nowPlayingRepository = new NowPlayingRepository(libraryProvider, libraryStorage);
+
+		playlistManager = new PlaylistManager(
+			fakePlaybackPreparerProvider,
+			Collections.singletonList(new CompletingFileQueueProvider()),
+			nowPlayingRepository,
+			new PlaylistVolumeManager(1.0f));
+
+		playlistManager
+			.startPlaylist(
+				Arrays.asList(
+					new ServiceFile(1),
+					new ServiceFile(2),
+					new ServiceFile(3),
+					new ServiceFile(4),
+					new ServiceFile(5)), 0, 0)
+			.then(obs -> obs.subscribe(f -> mostRecentPositionedFile = f));
+
+		fakePlaybackPreparerProvider.deferredResolution.resolve().resolve();
+		final ResolveablePlaybackHandler resolveablePlaybackHandler = fakePlaybackPreparerProvider.deferredResolution.resolve();
+
+		resolveablePlaybackHandler.setCurrentPosition(30);
+
+		playlistManager.pause();
+
+		playlistManager.skipToNext();
+
+		playlistManager.resume();
+
+		fakePlaybackPreparerProvider.deferredResolution.resolve();
+
+		final CountDownLatch countDownLatch = new CountDownLatch(1);
+		nowPlayingRepository
+			.getNowPlaying()
+			.then(np -> {
+				nowPlaying = np;
+				countDownLatch.countDown();
+				return null;
+			});
+
+		countDownLatch.await();
+	}
+
+	@Test
+	public void thenThePlaybackStateIsPlaying() {
+		assertThat(playlistManager.isPlaying()).isTrue();
+	}
+
+	@Test
+	public void thenTheSavedFilePositionIsCorrect() {
+		assertThat(nowPlaying.filePosition).isEqualTo(30);
+	}
+
+	@Test
+	public void thenTheSavedPlaylistPositionIsCorrect() {
+		assertThat(nowPlaying.playlistPosition).isEqualTo(3);
+	}
+
+	@Test
+	public void thenTheSavedPlaylistIsCorrect() {
+		assertThat(nowPlaying.playlist)
+			.containsExactly(new ServiceFile(1),
+				new ServiceFile(2),
+				new ServiceFile(3),
+				new ServiceFile(4),
+				new ServiceFile(5));
+	}
+
+	@Test
+	public void thenTheObservedFileIsCorrect() {
+		assertThat(mostRecentPositionedFile.getPlaylistPosition()).isEqualTo(3);
+	}
+}
