2532d47126532c5ccef318e9cf069806fe7ae287
david <dvedvick@gmail.com>
2017-07-24 21:31:29 -0500

    Serialize `FileNameTextViewSetter` instances

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/library/items/media/files/menu/FileNameTextViewSetter.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/library/items/media/files/menu/FileNameTextViewSetter.java
@@ -11,55 +11,63 @@ import com.lasthopesoftware.bluewater.client.library.items.media.files.propertie
 import com.lasthopesoftware.bluewater.client.library.items.media.files.properties.FilePropertiesProvider;
 import com.lasthopesoftware.bluewater.client.library.items.media.files.properties.repository.FilePropertyCache;
 import com.lasthopesoftware.bluewater.shared.promises.extensions.DispatchedPromise;
+import com.lasthopesoftware.messenger.Messenger;
 import com.lasthopesoftware.messenger.promises.Promise;
-import com.lasthopesoftware.messenger.promises.propagation.CancellationProxy;
+import com.lasthopesoftware.messenger.promises.propagation.PromiseProxy;
+import com.lasthopesoftware.messenger.promises.queued.QueuedPromise;
+import com.lasthopesoftware.messenger.promises.queued.cancellation.CancellationToken;
+import com.vedsoft.futures.runnables.OneParameterAction;
 
 import java.util.Map;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
-import static com.vedsoft.futures.callables.VoidFunc.runCarelessly;
+public class FileNameTextViewSetter implements OneParameterAction<Messenger<Map<String, String>>> {
 
-public class FileNameTextViewSetter {
+	private static final Executor fileNameTextViewExecutor = Executors.newSingleThreadExecutor();
 
 	private final TextView textView;
+	private final Handler handler;
+	private final ServiceFile serviceFile;
 
 	public static Promise<Map<String, String>> startNew(ServiceFile serviceFile, TextView textView) {
-		final FileNameTextViewSetter fileNameTextViewSetter = new FileNameTextViewSetter(textView);
-		final Handler handler = new Handler(textView.getContext().getMainLooper());
-
-		fileNameTextViewSetter.setLoading();
-
-		return new Promise<>((messenger) -> {
-			final IConnectionProvider connectionProvider = SessionConnection.getSessionConnectionProvider();
-			final FilePropertyCache filePropertyCache = FilePropertyCache.getInstance();
-			final CachedFilePropertiesProvider cachedFilePropertiesProvider = new CachedFilePropertiesProvider(connectionProvider, filePropertyCache, new FilePropertiesProvider(connectionProvider, filePropertyCache));
-
-			final Promise<Map<String, String>> promise = cachedFilePropertiesProvider.promiseFileProperties(serviceFile.getKey());
-			promise.next(runCarelessly(messenger::sendResolution));
-			promise.error(runCarelessly(messenger::sendRejection));
-
-			final CancellationProxy cancellationProxy = new CancellationProxy();
-			messenger.cancellationRequested(cancellationProxy);
-			cancellationProxy.doCancel(promise);
-
-			final Promise<Void> textViewUpdatePromise =
-				promise.then(properties -> new DispatchedPromise<>((cancellableToken) -> {
-					final String fileName = properties.get(FilePropertiesProvider.NAME);
-
-					if (!cancellableToken.isCancelled() && fileName != null)
-						textView.setText(fileName);
-
-					return null;
-				}, handler));
-
-			cancellationProxy.doCancel(textViewUpdatePromise);
-		});
-	}
-
-	private FileNameTextViewSetter(TextView textView) {
-		this.textView = textView;
-	}
-
-	private void setLoading() {
 		textView.setText(R.string.lbl_loading);
+
+		return new QueuedPromise<>(new FileNameTextViewSetter(textView, serviceFile), fileNameTextViewExecutor);
+	}
+
+	private FileNameTextViewSetter(TextView textView, ServiceFile serviceFile) {
+		this.textView = textView;
+		this.handler = new Handler(textView.getContext().getMainLooper());
+		this.serviceFile = serviceFile;
+	}
+
+	@Override
+	public void runWith(Messenger<Map<String, String>> messenger) {
+		final CancellationToken cancellationToken = new CancellationToken();
+		messenger.cancellationRequested(cancellationToken);
+
+		if (cancellationToken.isCancelled()) return;
+
+		final IConnectionProvider connectionProvider = SessionConnection.getSessionConnectionProvider();
+		final FilePropertyCache filePropertyCache = FilePropertyCache.getInstance();
+		final CachedFilePropertiesProvider cachedFilePropertiesProvider = new CachedFilePropertiesProvider(connectionProvider, filePropertyCache, new FilePropertiesProvider(connectionProvider, filePropertyCache));
+
+		final Promise<Map<String, String>> promise = cachedFilePropertiesProvider.promiseFileProperties(serviceFile.getKey());
+
+		final PromiseProxy<Map<String, String>> promiseProxy = new PromiseProxy<>(messenger);
+		promiseProxy.proxy(promise);
+
+		final Promise<Map<String, String>> textViewUpdatePromise =
+			promise.then(properties -> new DispatchedPromise<>(ct -> {
+				final String fileName = properties.get(FilePropertiesProvider.NAME);
+
+				if (!ct.isCancelled() && fileName != null)
+					textView.setText(fileName);
+
+				return properties;
+			}, handler));
+
+		promiseProxy.proxy(textViewUpdatePromise);
 	}
 }
--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/promises/resolutions/Dispatch.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/promises/resolutions/Dispatch.java
@@ -4,12 +4,9 @@ import android.content.Context;
 import android.os.Handler;
 
 import com.lasthopesoftware.bluewater.shared.promises.extensions.DispatchedPromise;
-import com.lasthopesoftware.messenger.Messenger;
 import com.lasthopesoftware.messenger.promises.Promise;
 import com.vedsoft.futures.callables.CarelessFunction;
 import com.vedsoft.futures.callables.CarelessOneParameterFunction;
-import com.vedsoft.futures.runnables.OneParameterAction;
-import com.vedsoft.futures.runnables.TwoParameterAction;
 
 public class Dispatch {
 	public static <TResult, TNewResult> CarelessOneParameterFunction<TResult, Promise<TNewResult>> toContext(CarelessOneParameterFunction<TResult, TNewResult> task, Context context) {
@@ -18,10 +15,6 @@ public class Dispatch {
 
 	public static <TResult, TNewResult> CarelessOneParameterFunction<TResult, Promise<TNewResult>> toHandler(CarelessOneParameterFunction<TResult, TNewResult> task, Handler handler) {
 		return new OneParameterExecutors.ReducingFunction<>(task, handler);
-	}
-
-	public static <Result, NewResult> CarelessOneParameterFunction<Result, Promise<NewResult>> toHandler(TwoParameterAction<Result, Messenger<NewResult>> task, Handler handler) {
-		return new OneParameterExecutors.ReducingAction<>(task, handler);
 	}
 
 	private static class OneParameterExecutors {
@@ -45,29 +38,6 @@ public class Dispatch {
 
 			@Override
 			public Promise<TNewResult> resultFrom(TResult result) throws Throwable {
-				this.result = result;
-				return new DispatchedPromise<>(this, handler);
-			}
-		}
-
-		static class ReducingAction<Result, NewResult> implements CarelessOneParameterFunction<Result, Promise<NewResult>>, OneParameterAction<Messenger<NewResult>> {
-
-			private final TwoParameterAction<Result, Messenger<NewResult>> action;
-			private final Handler handler;
-			private Result result;
-
-			ReducingAction(TwoParameterAction<Result, Messenger<NewResult>> action, Handler handler) {
-				this.action = action;
-				this.handler = handler;
-			}
-
-			@Override
-			public void runWith(Messenger<NewResult> newResultMessenger) {
-				action.runWith(result, newResultMessenger);
-			}
-
-			@Override
-			public Promise<NewResult> resultFrom(Result result) throws Throwable {
 				this.result = result;
 				return new DispatchedPromise<>(this, handler);
 			}
