1c8d87f45a13a759d4b7096c0823013a78aad77b
david <dvedvick@gmail.com>
2017-07-24 21:49:01 -0500

    Consolidate cancellation tracking

--- messenger/src/main/java/com/lasthopesoftware/messenger/promises/propagation/CancellationProxy.java
+++ messenger/src/main/java/com/lasthopesoftware/messenger/promises/propagation/CancellationProxy.java
@@ -2,25 +2,26 @@ package com.lasthopesoftware.messenger.promises.propagation;
 
 
 import com.lasthopesoftware.messenger.promises.Promise;
+import com.lasthopesoftware.messenger.promises.queued.cancellation.CancellationToken;
 
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public final class CancellationProxy implements Runnable {
 	private final Queue<Promise<?>> cancellablePromises = new LinkedBlockingQueue<>();
-	private volatile boolean isCancelled;
+	private final CancellationToken cancellationToken = new CancellationToken();
 
 	public Runnable doCancel(Promise<?> promise) {
 		cancellablePromises.offer(promise);
 
-		if (isCancelled) run();
+		if (cancellationToken.isCancelled()) run();
 
 		return this;
 	}
 
 	@Override
-	public synchronized void run() {
-		isCancelled = true;
+	public void run() {
+		cancellationToken.run();
 
 		Promise<?> cancellingPromise;
 		while ((cancellingPromise = cancellablePromises.poll()) != null)
