9b4f667ca37b3411578950f04baddb3d716cd4c1
david <dvedvick@gmail.com>
2017-08-11 07:26:28 -0500

    Use thread inheritance in `ProgramVersionProvider`

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/servers/version/ProgramVersionProvider.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/servers/version/ProgramVersionProvider.java
@@ -21,57 +21,59 @@ public class ProgramVersionProvider implements IProgramVersionProvider {
 
 	@Override
 	public Promise<ProgramVersion> promiseServerVersion() {
-		return new Promise<>((messenger) -> {
-			new Thread(() -> {
-				try {
-					synchronized (syncObject) {
-						final HttpURLConnection connection;
-						try {
-							connection = connectionProvider.getConnection("Alive");
-						} catch (IOException e) {
-							messenger.sendRejection(e);
-							return;
-						}
-
-						try {
-							try (final InputStream is = connection.getInputStream()) {
-								final StandardRequest standardRequest = StandardRequest.fromInputStream(is);
-								if (standardRequest == null) {
-									messenger.sendResolution(null);
-									return;
-								}
-
-								final String semVerString = standardRequest.items.get("ProgramVersion");
-								if (semVerString == null) {
-									messenger.sendResolution(null);
-									return;
-								}
-
-								final String[] semVerParts = semVerString.split("\\.");
-
-								int major = 0, minor = 0, patch = 0;
-
-								if (semVerParts.length > 0)
-									major = Integer.parseInt(semVerParts[0]);
-
-								if (semVerParts.length > 1)
-									minor = Integer.parseInt(semVerParts[1]);
-
-								if (semVerParts.length > 2)
-									patch = Integer.parseInt(semVerParts[2]);
-
-								messenger.sendResolution(new ProgramVersion(major, minor, patch));
+		return new Promise<>((messenger) ->
+			new Thread("server-version-thread-" + serverVersionThreads++) {
+				@Override
+				public void run() {
+					try {
+						synchronized (syncObject) {
+							final HttpURLConnection connection;
+							try {
+								connection = connectionProvider.getConnection("Alive");
 							} catch (IOException e) {
 								messenger.sendRejection(e);
+								return;
 							}
-						} finally {
-							connection.disconnect();
+
+							try {
+								try (final InputStream is = connection.getInputStream()) {
+									final StandardRequest standardRequest = StandardRequest.fromInputStream(is);
+									if (standardRequest == null) {
+										messenger.sendResolution(null);
+										return;
+									}
+
+									final String semVerString = standardRequest.items.get("ProgramVersion");
+									if (semVerString == null) {
+										messenger.sendResolution(null);
+										return;
+									}
+
+									final String[] semVerParts = semVerString.split("\\.");
+
+									int major = 0, minor = 0, patch = 0;
+
+									if (semVerParts.length > 0)
+										major = Integer.parseInt(semVerParts[0]);
+
+									if (semVerParts.length > 1)
+										minor = Integer.parseInt(semVerParts[1]);
+
+									if (semVerParts.length > 2)
+										patch = Integer.parseInt(semVerParts[2]);
+
+									messenger.sendResolution(new ProgramVersion(major, minor, patch));
+								} catch (IOException e) {
+									messenger.sendRejection(e);
+								}
+							} finally {
+								connection.disconnect();
+							}
 						}
+					} finally {
+						--serverVersionThreads;
 					}
-				} finally {
-					--serverVersionThreads;
 				}
-			}, "server-version-thread-" + serverVersionThreads++).start();
-		});
+		}.start());
 	}
 }
