fa6a848a14d38eccdd4755f7ce7781111c4931c8
david <dvedvick@gmail.com>
2017-10-14 08:40:23 -0500

    Send resolution immediately when collection is empty

--- messenger/src/specs/java/com/lasthopesoftware/messenger/promises/aggregation/GivenAnEmptyCollectionOfPromises/WhenResolvingTheCollection.java
+++ messenger/src/specs/java/com/lasthopesoftware/messenger/promises/aggregation/GivenAnEmptyCollectionOfPromises/WhenResolvingTheCollection.java
@@ -1,24 +1,31 @@
 package com.lasthopesoftware.messenger.promises.aggregation.GivenAnEmptyCollectionOfPromises;
 
-import com.lasthopesoftware.messenger.Messenger;
+import com.lasthopesoftware.messenger.Message;
 import com.lasthopesoftware.messenger.SingleMessageBroadcaster;
 import com.lasthopesoftware.messenger.promises.aggregation.CollectedResultsResolver;
 
 import org.junit.BeforeClass;
+import org.junit.Test;
 
+import java.util.Collection;
 import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class WhenResolvingTheCollection {
 
+	private static Message<Collection<Void>> response;
+
 	@BeforeClass
 	public static void before() {
-		final Messenger<Void> messenger = new SingleMessageBroadcaster<>();
+		final SingleMessageBroadcaster<Collection<Void>> messenger = new SingleMessageBroadcaster<>();
+		messenger.awaitResolution(v -> response = v);
 
-		final CollectedResultsResolver<Void> collectedResultsResolver =
-			new CollectedResultsResolver<Void>(new SingleMessageBroadcaster<>(), Collections.emptyList());
+		new CollectedResultsResolver<>(messenger, Collections.emptyList());
 	}
 
+	@Test
 	public void thenTheCollectionResolutionResolvesWithAnEmptyCollection() {
-
+		assertThat(response.resolution).isEmpty();
 	}
 }
