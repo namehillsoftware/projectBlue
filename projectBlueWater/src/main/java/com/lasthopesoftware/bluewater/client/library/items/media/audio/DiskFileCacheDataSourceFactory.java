f6f74970908dcbbac22c449d88e05c5ef9557a2f
david <dvedvick@gmail.com>
2017-10-16 17:22:50 -0500

    Use `OkHttp` for HTTP client

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/library/items/media/audio/DiskFileCacheDataSourceFactory.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/library/items/media/audio/DiskFileCacheDataSourceFactory.java
@@ -2,25 +2,31 @@ package com.lasthopesoftware.bluewater.client.library.items.media.audio;
 
 import android.content.Context;
 
+import com.google.android.exoplayer2.ext.okhttp.OkHttpDataSourceFactory;
 import com.google.android.exoplayer2.upstream.DataSource;
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.upstream.TransferListener;
 import com.google.android.exoplayer2.util.Util;
 import com.lasthopesoftware.bluewater.R;
 import com.lasthopesoftware.bluewater.client.library.items.media.files.ServiceFile;
 import com.lasthopesoftware.bluewater.client.library.items.media.files.cached.DiskFileCache;
 import com.lasthopesoftware.bluewater.client.library.repository.Library;
+import com.namehillsoftware.lazyj.Lazy;
+
+import okhttp3.OkHttpClient;
 
 public class DiskFileCacheDataSourceFactory implements DataSource.Factory {
 
-	private final DefaultHttpDataSourceFactory httpDataSourceFactory;
+	private static final Lazy<OkHttpClient> okHttpClient = new Lazy<>(OkHttpClient::new);
+
+	private final OkHttpDataSourceFactory httpDataSourceFactory;
 	private final ServiceFile serviceFile;
 	private final DiskFileCache diskFileCache;
 
 	public DiskFileCacheDataSourceFactory(Context context, DiskFileCache diskFileCache, TransferListener<? super DataSource> transferListener, Library library, ServiceFile serviceFile) {
 		this.serviceFile = serviceFile;
 		this.diskFileCache = diskFileCache;
-		httpDataSourceFactory = new DefaultHttpDataSourceFactory(
+		httpDataSourceFactory = new OkHttpDataSourceFactory(
+			okHttpClient.getObject(),
 			Util.getUserAgent(context, context.getString(R.string.app_name)),
 			transferListener);
 
