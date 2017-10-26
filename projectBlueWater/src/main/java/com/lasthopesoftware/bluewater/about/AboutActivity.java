aefd7de351fd02eedc9379b9ece4ed81ef8b02cc
david <dvedvick@gmail.com>
2017-10-26 07:30:21 -0500

    Add application information

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/about/AboutActivity.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/about/AboutActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.lasthopesoftware.bluewater.BuildConfig;
 import com.lasthopesoftware.bluewater.R;
 
 public class AboutActivity extends AppCompatActivity {
@@ -16,6 +17,13 @@ public class AboutActivity extends AppCompatActivity {
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 		final TextView textView = findViewById(R.id.aboutDescription);
-		textView.setText(R.string.aboutAppText);
+		textView.setText(
+			String.format(
+				getString(R.string.aboutAppText),
+				getString(R.string.app_name),
+				BuildConfig.VERSION_NAME,
+				String.valueOf(BuildConfig.VERSION_CODE),
+				getString(R.string.company_name),
+				getString(R.string.copyright_year)));
 	}
 }
