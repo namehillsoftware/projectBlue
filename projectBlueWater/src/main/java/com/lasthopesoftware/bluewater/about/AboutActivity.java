7519020144b8e3afc09383cc797f5fe95511699e
david <dvedvick@gmail.com>
2017-11-08 22:14:36 -0600

    Add image to about activity

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/about/AboutActivity.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/about/AboutActivity.java
@@ -1,15 +1,28 @@
 package com.lasthopesoftware.bluewater.about;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.lasthopesoftware.bluewater.BuildConfig;
 import com.lasthopesoftware.bluewater.R;
+import com.lasthopesoftware.bluewater.shared.android.view.ScaledWrapImageView;
+import com.namehillsoftware.lazyj.AbstractSynchronousLazy;
+import com.namehillsoftware.lazyj.ILazy;
 
 public class AboutActivity extends AppCompatActivity {
 
 	private final BuildAboutTitle aboutTitleBuilder = new AboutTitleBuilder(this);
+
+	private final ILazy<Bitmap> lazyLogoBitmap = new AbstractSynchronousLazy<Bitmap>() {
+		@Override
+		protected Bitmap initialize() throws Exception {
+			return BitmapFactory.decodeResource(getResources(), R.drawable.music_canoe_hi_res_logo);
+		}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +40,20 @@ public class AboutActivity extends AppCompatActivity {
 				String.valueOf(BuildConfig.VERSION_CODE),
 				getString(R.string.company_name),
 				getString(R.string.copyright_year)));
+
+		final RelativeLayout logoImageContainer = findViewById(R.id.logoImageContainer);
+
+		final ScaledWrapImageView scaledWrapImageView = new ScaledWrapImageView(this);
+		scaledWrapImageView.setImageBitmap(lazyLogoBitmap.getObject());
+
+		logoImageContainer.addView(scaledWrapImageView);
+	}
+
+	@Override
+	protected void onDestroy() {
+		if (lazyLogoBitmap.isInitialized())
+			lazyLogoBitmap.getObject().recycle();
+
+		super.onDestroy();
 	}
 }
