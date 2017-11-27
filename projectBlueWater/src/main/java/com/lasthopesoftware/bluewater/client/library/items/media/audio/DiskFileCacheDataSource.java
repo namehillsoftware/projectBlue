bd2f83a0c56b9ea63b7c2294280c2cd8355ddcd6
david <dvedvick@gmail.com>
2017-11-26 20:34:03 -0600

    Attempt to use okio for audio caching

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/library/items/media/audio/DiskFileCacheDataSource.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/library/items/media/audio/DiskFileCacheDataSource.java
@@ -8,14 +8,14 @@ import com.google.android.exoplayer2.upstream.DataSpec;
 import com.google.android.exoplayer2.upstream.HttpDataSource;
 import com.lasthopesoftware.bluewater.client.library.items.media.files.ServiceFile;
 import com.lasthopesoftware.bluewater.client.library.items.media.files.cached.DiskFileCache;
-import com.lasthopesoftware.bluewater.client.library.items.media.files.cached.stream.CachedFileOutputStream;
-import com.namehillsoftware.handoff.promises.Promise;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.Arrays;
+
+import okio.Buffer;
+import okio.BufferedSink;
 
 
 class DiskFileCacheDataSource implements DataSource {
@@ -25,7 +25,7 @@ class DiskFileCacheDataSource implements DataSource {
 	private final HttpDataSource defaultHttpDataSource;
 	private final String serviceFileKey;
 	private final DiskFileCache diskFileCache;
-	private Promise<CachedFileOutputStream> promisedOutputStream;
+	private BufferedSink bufferedSink;
 
 	DiskFileCacheDataSource(HttpDataSource defaultHttpDataSource, ServiceFile serviceFile, DiskFileCache diskFileCache) {
 		this.defaultHttpDataSource = defaultHttpDataSource;
@@ -36,7 +36,7 @@ class DiskFileCacheDataSource implements DataSource {
 	@Override
 	public long open(DataSpec dataSpec) throws IOException {
 		if (dataSpec.position == 0)
-			promisedOutputStream = diskFileCache.promiseCachedFileOutputStream(serviceFileKey);
+			 bufferedSink = new Buffer();
 
 		return defaultHttpDataSource.open(dataSpec);
 	}
@@ -45,31 +45,18 @@ class DiskFileCacheDataSource implements DataSource {
 	public int read(byte[] buffer, int offset, int readLength) throws IOException {
 		final int result = defaultHttpDataSource.read(buffer, offset, readLength);
 
-		if (promisedOutputStream == null) return result;
+		if (bufferedSink == null) return result;
 
-		if (result == C.RESULT_END_OF_INPUT) {
-			promisedOutputStream
-				.eventually(CachedFileOutputStream::flush)
-				.eventually(cachedFileOutputStream -> {
-					cachedFileOutputStream.close();
-					return cachedFileOutputStream.commitToCache();
-				});
-
+		if (result != C.RESULT_END_OF_INPUT) {
+			bufferedSink.write(buffer, offset, readLength);
 			return result;
 		}
 
-		final byte[] copiedBuffer = Arrays.copyOfRange(buffer, offset, offset + result);
-
-		promisedOutputStream = promisedOutputStream
-			.eventually(cachedFileOutputStream -> {
-				final Promise<CachedFileOutputStream> promisedWrite = cachedFileOutputStream.promiseWrite(copiedBuffer, 0, copiedBuffer.length);
-				promisedWrite.excuse(e -> {
-					logger.warn("An error occurred storing the audio file", e);
-					cachedFileOutputStream.close();
-					return null;
-				});
-
-				return promisedWrite;
+		diskFileCache
+			.put(serviceFileKey, bufferedSink.buffer().readByteArray())
+			.excuse(e -> {
+				logger.warn("An error occurred storing the audio file", e);
+				return null;
 			});
 
 		return result;
@@ -84,11 +71,7 @@ class DiskFileCacheDataSource implements DataSource {
 	public void close() throws IOException {
 		defaultHttpDataSource.close();
 
-		if (promisedOutputStream == null) return;
-
-		promisedOutputStream.then(cachedFileOutputStream -> {
-			cachedFileOutputStream.close();
-			return null;
-		});
+		if (bufferedSink != null)
+			bufferedSink.close();
 	}
 }
