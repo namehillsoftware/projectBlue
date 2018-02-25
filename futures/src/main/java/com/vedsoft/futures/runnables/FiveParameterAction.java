2c5d73301da5c6e206dbaebeb38aacd382b3062a
david <dvedvick@gmail.com>
2018-02-25 09:16:13 -0600

    Remove unused interfaces

--- futures/src/main/java/com/vedsoft/futures/runnables/FiveParameterAction.java
+++
@@ -1,9 +0,0 @@
-package com.vedsoft.futures.runnables;
-
-/**
- * Created by david on 10/25/16.
- */
-
-public interface FiveParameterAction<TFirstParameter, TSecondParameter, TThirdParameter, TFourthParameter, TFifthParameter> {
-	void runWith(TFirstParameter parameterOne, TSecondParameter parameterTwo, TThirdParameter parameterThree, TFourthParameter parameterFour, TFifthParameter parameterFive);
-}
--- futures/src/main/java/com/vedsoft/futures/runnables/FourParameterAction.java
+++
@@ -1,8 +0,0 @@
-package com.vedsoft.futures.runnables;
-
-/**
- * Created by david on 12/5/15.
- */
-public interface FourParameterAction<TFirstParameter, TSecondParameter, TThirdParameter, TFourthParameter> {
-	void runWith(TFirstParameter parameterOne, TSecondParameter parameterTwo, TThirdParameter parameterThree, TFourthParameter parameterFour);
-}
