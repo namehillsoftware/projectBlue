e4c94d52f6e7c2d7ad9de69f3ebb7f33a8d99b84
dvedvick@gmail.com <18. Samwise the Brave>
2018-04-08 22:10:51 -0500

    Test observing the position

---
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/file/mediaplayer/specs/GivenAPlayingMediaPlayer/WhenObservingThePlaybackPosition.java
@@ -0,0 +1,43 @@
+package com.lasthopesoftware.bluewater.client.playback.file.mediaplayer.specs.GivenAPlayingMediaPlayer;
+
+import android.media.MediaPlayer;
+
+import com.lasthopesoftware.bluewater.client.playback.file.PlayingFileProgress;
+import com.lasthopesoftware.bluewater.client.playback.file.mediaplayer.MediaPlayerPlaybackHandler;
+
+import org.joda.time.Period;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+//@RunWith(RobolectricTestRunner.class)
+public class WhenObservingThePlaybackPosition {
+
+	private static PlayingFileProgress progress;
+
+	@BeforeClass
+	public static void before() {
+		final MediaPlayer mockMediaPlayer = mock(MediaPlayer.class);
+		when(mockMediaPlayer.isPlaying()).thenReturn(true);
+		when(mockMediaPlayer.getCurrentPosition()).thenReturn(50);
+		when(mockMediaPlayer.getDuration()).thenReturn(100);
+
+		final MediaPlayerPlaybackHandler mediaPlayerPlaybackHandler = new MediaPlayerPlaybackHandler(mockMediaPlayer);
+		progress = mediaPlayerPlaybackHandler
+			.observeProgress(Period.ZERO)
+			.blockingFirst();
+	}
+
+	@Test
+	public void thenThePlaybackPositionIsCorrect() {
+		assertThat(progress.position).isEqualTo(50);
+	}
+
+	@Test
+	public void thenThePlaybackDurationIsCorrect() {
+		assertThat(progress.duration).isEqualTo(100);
+	}
+}
