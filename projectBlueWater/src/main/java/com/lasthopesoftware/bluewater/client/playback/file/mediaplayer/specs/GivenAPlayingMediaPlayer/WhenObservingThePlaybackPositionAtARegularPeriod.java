cf2c4f35e08d7c140a29aa196f68c458186de79d
dvedvick@gmail.com <18. Samwise the Brave>
2018-04-10 06:26:13 -0500

    Test collection of progress

---
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/file/mediaplayer/specs/GivenAPlayingMediaPlayer/WhenObservingThePlaybackPositionAtARegularPeriod.java
@@ -0,0 +1,60 @@
+package com.lasthopesoftware.bluewater.client.playback.file.mediaplayer.specs.GivenAPlayingMediaPlayer;
+
+import android.media.MediaPlayer;
+
+import com.lasthopesoftware.bluewater.client.playback.file.PlayingFileProgress;
+import com.lasthopesoftware.bluewater.client.playback.file.mediaplayer.MediaPlayerPlaybackHandler;
+
+import org.joda.time.Period;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observables.ConnectableObservable;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class WhenObservingThePlaybackPositionAtARegularPeriod {
+
+	private static List<PlayingFileProgress> collectedProgresses = new ArrayList<>();
+
+	@BeforeClass
+	public static void before() {
+		final MediaPlayer mockMediaPlayer = mock(MediaPlayer.class);
+		when(mockMediaPlayer.isPlaying()).thenReturn(true);
+		when(mockMediaPlayer.getCurrentPosition()).thenReturn(50, 50, 50, 50, 50);
+		when(mockMediaPlayer.getDuration())
+			.thenReturn(100);
+
+
+		final MediaPlayerPlaybackHandler mediaPlayerPlaybackHandler = new MediaPlayerPlaybackHandler(mockMediaPlayer);
+		final ConnectableObservable<PlayingFileProgress> progressObservable = mediaPlayerPlaybackHandler
+			.observeProgress(Period.millis(500))
+			.publish();
+
+		final Disposable disposable = progressObservable.connect();
+
+		final Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				disposable.dispose();
+			}
+		}, 2600);
+
+		progressObservable
+			.subscribe(collectedProgresses::add);
+	}
+
+	@Test
+	public void thenThePlaybackPositionIsCorrect() {
+		assertThat(collectedProgresses.size()).isEqualTo(5);
+	}
+}
