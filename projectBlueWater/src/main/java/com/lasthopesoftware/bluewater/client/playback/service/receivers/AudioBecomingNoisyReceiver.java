22294cce40bf75c6deea8d3a58197c233e271aca
david <dvedvick@gmail.com>
2018-06-10 13:41:52 -0500

    Change audio becoming noisy receiver

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/service/receivers/AudioBecomingNoisyReceiver.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/service/receivers/AudioBecomingNoisyReceiver.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 
 import com.lasthopesoftware.bluewater.client.playback.service.PlaybackService;
 
+import static android.media.AudioManager.ACTION_AUDIO_BECOMING_NOISY;
+
 public class AudioBecomingNoisyReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		PlaybackService.pause(context);
+		if (ACTION_AUDIO_BECOMING_NOISY.equals(intent.getAction()))
+			PlaybackService.pause(context);
 	}
 }
