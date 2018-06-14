63376920196c64f0c704e3e1969d514d1665e5ca
david <dvedvick@gmail.com>
2018-06-13 21:21:20 -0500

    Include external media dirs

--- projectBlueWater/src/main/java/com/lasthopesoftware/storage/directories/PublicDirectoryLookup.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/storage/directories/PublicDirectoryLookup.java
@@ -1,5 +1,7 @@
 package com.lasthopesoftware.storage.directories;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Environment;
 
 import com.annimon.stream.Stream;
@@ -8,9 +10,20 @@ import com.namehillsoftware.handoff.promises.Promise;
 import java.io.File;
 
 public class PublicDirectoryLookup implements GetPublicDirectories {
+
+	private final Context context;
+
+	public PublicDirectoryLookup(Context context) {
+		this.context = context;
+	}
+
 	@Override
 	public Promise<Stream<File>> promisePublicDrives() {
-		return new Promise<>(Stream
-			.of(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC)));
+		final Stream<File> publicDirectory = Stream.of(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC));
+		return Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP
+			? new Promise<>(publicDirectory)
+			: new Promise<>(Stream.concat(
+				publicDirectory,
+				Stream.of(context.getExternalMediaDirs())));
 	}
 }
