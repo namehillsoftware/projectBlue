9d70160e2933962f5b21ffa5010e23f4ef94e8a5
david <dvedvick@gmail.com>
2018-08-26 09:41:39 -0500

    Support pausing and resuming playback

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/engine/exoplayer/ActiveExoPlaylistPlayer.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/engine/exoplayer/ActiveExoPlaylistPlayer.java
@@ -7,18 +7,21 @@ import com.lasthopesoftware.bluewater.client.playback.file.PositionedPlayingFile
 import io.reactivex.observables.ConnectableObservable;
 
 public class ActiveExoPlaylistPlayer implements IActivePlayer {
+	private final ExoPlayer exoPlayer;
+
 	public ActiveExoPlaylistPlayer(ExoPlayer exoPlayer) {
+		this.exoPlayer = exoPlayer;
 		exoPlayer.setPlayWhenReady(true);
 	}
 
 	@Override
 	public void pause() {
-
+		this.exoPlayer.setPlayWhenReady(false);
 	}
 
 	@Override
 	public void resume() {
-
+		this.exoPlayer.setPlayWhenReady(true);
 	}
 
 	@Override
---
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/engine/exoplayer/specs/GivenATypicalExoPlayer/WhenPausingPlayback.java
@@ -0,0 +1,57 @@
+package com.lasthopesoftware.bluewater.client.playback.engine.exoplayer.specs.GivenATypicalExoPlayer;
+
+import com.google.android.exoplayer2.ExoPlayer;
+import com.lasthopesoftware.bluewater.client.playback.engine.exoplayer.ActiveExoPlaylistPlayer;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class WhenPausingPlayback {
+
+	private static final PlaybackReportingExoPlayer exoPlayer = mock(PlaybackReportingExoPlayer.class);
+
+	@BeforeClass
+	public static void before() {
+		final ActiveExoPlaylistPlayer exoPlaylistPlayer = new ActiveExoPlaylistPlayer(exoPlayer);
+		exoPlaylistPlayer.pause();
+	}
+
+	@Test
+	public void thenPlaybackIsPaused() {
+		assertThat(exoPlayer.getPlayWhenReady()).isFalse();
+	}
+
+	public static class AndStartingItAgain {
+		private static final PlaybackReportingExoPlayer exoPlayer = mock(PlaybackReportingExoPlayer.class);
+
+		@BeforeClass
+		public static void before() {
+			final ActiveExoPlaylistPlayer exoPlaylistPlayer = new ActiveExoPlaylistPlayer(exoPlayer);
+			exoPlaylistPlayer.pause();
+			exoPlaylistPlayer.resume();
+		}
+
+		@Test
+		public void thenPlaybackIsPaused() {
+			assertThat(exoPlayer.getPlayWhenReady()).isTrue();
+		}
+	}
+
+	private abstract static class PlaybackReportingExoPlayer implements ExoPlayer {
+
+		private boolean playWhenReady;
+
+		@Override
+		public final void setPlayWhenReady(boolean playWhenReady) {
+			this.playWhenReady = playWhenReady;
+		}
+
+		@Override
+		public final boolean getPlayWhenReady() {
+			return playWhenReady;
+		}
+	}
+}
