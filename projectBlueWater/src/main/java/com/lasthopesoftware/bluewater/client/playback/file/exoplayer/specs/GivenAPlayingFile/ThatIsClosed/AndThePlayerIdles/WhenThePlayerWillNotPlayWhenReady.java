6c7d866a2ac623718dd3d43a80a222154cbe5bcf
David Vedvick <dvedvick@gmail.com>
2018-10-24 22:02:06 -0500

    Test idling after close

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/file/exoplayer/specs/GivenAPlayingFile/ThatIsClosed/AndThePlayerIdles/WhenThePlayerWillNotPlayWhenReady.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/file/exoplayer/specs/GivenAPlayingFile/ThatIsClosed/AndThePlayerIdles/WhenThePlayerWillNotPlayWhenReady.java
@@ -1,4 +1,4 @@
-package com.lasthopesoftware.bluewater.client.playback.file.exoplayer.specs.GivenAPlayingFile.ThatIsThenPaused.AndThePlayerIdles;
+package com.lasthopesoftware.bluewater.client.playback.file.exoplayer.specs.GivenAPlayingFile.ThatIsClosed.AndThePlayerIdles;
 
 import com.annimon.stream.Stream;
 import com.google.android.exoplayer2.ExoPlayer;
@@ -9,7 +9,6 @@ import com.lasthopesoftware.bluewater.client.playback.file.PlayingFile;
 import com.lasthopesoftware.bluewater.client.playback.file.exoplayer.ExoPlayerPlaybackHandler;
 import com.lasthopesoftware.bluewater.shared.promises.extensions.specs.FuturePromise;
 import com.namehillsoftware.handoff.promises.Promise;
-
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.stubbing.Answer;
@@ -20,20 +19,15 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class WhenThePlayerWillNotPlayWhenReady {
 	private static final Collection<Player.EventListener> eventListeners = new ArrayList<>();
 	private static final ExoPlayer mockExoPlayer = mock(ExoPlayer.class);;
 
 	@BeforeClass
-	public static void before() throws InterruptedException, ExecutionException {
+	public static void before() throws InterruptedException, ExecutionException, TimeoutException {
 		when(mockExoPlayer.getPlayWhenReady()).thenReturn(true);
 		when(mockExoPlayer.getCurrentPosition()).thenReturn(50L);
 		when(mockExoPlayer.getDuration()).thenReturn(100L);
@@ -43,24 +37,13 @@ public class WhenThePlayerWillNotPlayWhenReady {
 		}).when(mockExoPlayer).addListener(any());
 
 		ExoPlayerPlaybackHandler exoPlayerPlaybackHandler = new ExoPlayerPlaybackHandler(mockExoPlayer);
-		final Promise<PlayingFile> playbackPromise = exoPlayerPlaybackHandler.promisePlayback();
 
-		playbackPromise
-				.eventually(p -> {
-					final Promise<PlayableFile> playableFilePromise = p.promisePause();
-					Stream.of(eventListeners)
-						.forEach(e -> e.onPlayerStateChanged(false, Player.STATE_IDLE));
-					return playableFilePromise;
-				});
+		new FuturePromise<>(exoPlayerPlaybackHandler.promisePlayback()).get(1, TimeUnit.SECONDS);
 
-		final Promise<PlayedFile> playedPromise =
-				playbackPromise
-						.eventually(PlayingFile::promisePlayedFile);
+		exoPlayerPlaybackHandler.close();
 
-		try {
-			new FuturePromise<>(playedPromise).get(1, TimeUnit.SECONDS);
-		} catch (TimeoutException ignored) {
-		}
+		Stream.of(eventListeners)
+				.forEach(e -> e.onPlayerStateChanged(false, Player.STATE_IDLE));
 	}
 
 	@Test
