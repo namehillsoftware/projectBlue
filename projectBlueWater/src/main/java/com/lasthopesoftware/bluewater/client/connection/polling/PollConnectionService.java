bf1209a64d719563009e40931275fe0f1cdf8691
David Vedvick <dvedvick@gmail.com>
2018-11-18 16:47:30 -0600

    Simplify `PollConnectionService`

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/connection/polling/PollConnectionService.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/connection/polling/PollConnectionService.java
@@ -5,37 +5,20 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.os.AsyncTask;
 import android.os.IBinder;
+import com.lasthopesoftware.bluewater.client.connection.IConnectionProvider;
 import com.lasthopesoftware.bluewater.client.connection.session.SessionConnection;
 import com.lasthopesoftware.bluewater.shared.GenericBinder;
 import com.namehillsoftware.handoff.promises.Promise;
-import com.namehillsoftware.lazyj.AbstractSynchronousLazy;
-import com.namehillsoftware.lazyj.CreateAndHold;
 import com.namehillsoftware.lazyj.Lazy;
 import org.joda.time.Duration;
 
 import java.util.HashSet;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class PollConnectionService extends Service {
 
-	private static final ExecutorService pollService = Executors.newSingleThreadExecutor();
-
-	private static final HashSet<Runnable> mUniqueOnConnectionLostListeners = new HashSet<>();
-
-	private final Lazy<GenericBinder<PollConnectionService>> lazyBinder = new Lazy<>(() -> new GenericBinder<>(this));
-
-	private final CreateAndHold<WaitForConnectionTask> lazyTask = new AbstractSynchronousLazy<WaitForConnectionTask>() {
-		@Override
-		protected WaitForConnectionTask create() {
-			return new WaitForConnectionTask(PollConnectionService.this);
-		}
-	};
-
 	public static class Instance {
+
 		public static Promise<PollConnectionService> promise(Context context) {
 			return new Promise<>(m -> context.bindService(new Intent(context, PollConnectionService.class), new ServiceConnection() {
 
@@ -51,6 +34,8 @@ public class PollConnectionService extends Service {
 		}
 	}
 
+	private static final HashSet<Runnable> mUniqueOnConnectionLostListeners = new HashSet<>();
+
 	/* Differs from the normal on start listener in that it uses a static list that will be re-populated when a new Poll Connection task starts.
 	 */
 	public static void addOnConnectionLostListener(Runnable listener) {
@@ -65,150 +50,86 @@ public class PollConnectionService extends Service {
 		}
 	}
 
+	private final Lazy<GenericBinder<PollConnectionService>> lazyBinder = new Lazy<>(() -> new GenericBinder<>(this));
+
+	private final HashSet<Runnable> mUniqueOnConnectionRegainedListeners = new HashSet<>();
+	private final HashSet<Runnable> mUniqueOnCancelListeners = new HashSet<>();
+
+	private boolean isPolling;
+
+	private boolean isCancelled;
+
 	@Override
 	public IBinder onBind(Intent intent) {
 		return lazyBinder.getObject();
 	}
 
-	public synchronized void startPolling() {
-		if (lazyTask.getObject().getStatus() == AsyncTask.Status.PENDING) lazyTask.getObject().executeOnExecutor(pollService);
+	public void startPolling() {
+		if (isPolling) return;
+		isPolling = true;
+
+		synchronized (mUniqueOnConnectionLostListeners) {
+			for (Runnable onConnectionLostListener : mUniqueOnConnectionLostListeners) onConnectionLostListener.run();
+		}
+
+		promisePolledSessionConnection(1000);
+	}
+
+	private Promise<IConnectionProvider> promisePolledSessionConnection(int connectionTime) {
+		if (isCancelled) {
+			for (Runnable onCancelListener : mUniqueOnCancelListeners) onCancelListener.run();
+
+			stopSelf();
+
+			return Promise.empty();
+		}
+
+		final int nextConnectionTime = connectionTime < 32000 ? connectionTime * 2 : connectionTime;
+		return SessionConnection.getInstance(this)
+			.promiseTestedSessionConnection(Duration.millis(connectionTime))
+			.eventually(
+				c -> {
+					if (c == null) return promisePolledSessionConnection(nextConnectionTime);
+
+					for (Runnable onConnectionRegainedListener : mUniqueOnConnectionRegainedListeners) onConnectionRegainedListener.run();
+
+					// Let on cancelled clear the completed listeners
+					stopSelf();
+
+					return Promise.empty();
+				},
+				e -> promisePolledSessionConnection(nextConnectionTime));
 	}
 
 	public synchronized void stopPolling() {
-		if (lazyTask.isCreated())
-			lazyTask.getObject().cancel(true);
+		isCancelled = true;
 	}
 
 	/* Differs from the normal onCompleteListener in that the onCompleteListener list is emptied every time the Poll Connection Task is runWith
 	 */
 	public void addOnConnectionRegainedListener(Runnable listener) {
-		lazyTask.getObject().addOnConnectionRegainedListener(listener);
+		synchronized(mUniqueOnConnectionRegainedListeners) {
+			mUniqueOnConnectionRegainedListeners.add(listener);
+		}
 	}
 
 	/* Differs from the normal onCompleteListener in that the onCompleteListener list is emptied every time the Poll Connection Task is runWith
 	 */
 	public void addOnPollingCancelledListener(Runnable listener) {
-		lazyTask.getObject().addOnPollingCancelledListener(listener);
+		synchronized(mUniqueOnCancelListeners) {
+			mUniqueOnCancelListeners.add(listener);
+		}
 	}
 
 	public void removeOnConnectionRegainedListener(Runnable listener) {
-		lazyTask.getObject().removeOnConnectionRegainedListener(listener);
+		synchronized(mUniqueOnConnectionRegainedListeners) {
+			mUniqueOnConnectionRegainedListeners.remove(listener);
+		}
 	}
 
 	public void removeOnPollingCancelledListener(Runnable listener) {
-		lazyTask.getObject().removeOnPollingCancelledListener(listener);
-	}
-
-	private static class WaitForConnectionTask extends AsyncTask<String, Void, Void> {
-
-		private final HashSet<Runnable> mUniqueOnConnectionRegainedListeners = new HashSet<>();
-		private final HashSet<Runnable> mUniqueOnCancelListeners = new HashSet<>();
-
-		private final AtomicBoolean mIsConnectionRestored = new AtomicBoolean();
-		private final AtomicBoolean mIsRefreshing = new AtomicBoolean();
-
-		private final Context context;
-
-		private int mSleepTime = 2250;
-		private int mConnectionTime = 2000;
-
-		private WaitForConnectionTask(Context context) {
-			this.context = context;
-		}
-
-		@Override
-		protected void onPreExecute() {
-			synchronized (mUniqueOnConnectionLostListeners) {
-				for (Runnable onConnectionLostListener : mUniqueOnConnectionLostListeners) onConnectionLostListener.run();
-			}
-		}
-
-		@Override
-		protected Void doInBackground(String... params) {
-			// Don't use timeout since if it can't resolve a host it will throw an exception immediately
-			// TODO need a blocking refresh configuration (that throws an error when runWith on a UI thread) for this one scenario
-
-			while (!isCancelled() && !mIsConnectionRestored.get()) {
-
-				try {
-					Thread.sleep(mSleepTime);
-					// Also arbitrarily increase the sleep time slowly up 32000 ms
-					if (mSleepTime < 30000) mSleepTime *= 1.5;
-				} catch (InterruptedException ie) {
-					return null;
-				}
-
-				if (isCancelled()) return null;
-
-				if (mIsRefreshing.get()) continue;
-				mIsRefreshing.set(true);
-
-				SessionConnection.getInstance(context).promiseTestedSessionConnection(Duration.millis(mConnectionTime))
-						.then(c -> {
-							if (c == null) {
-								if (mConnectionTime < 32000) mConnectionTime *= 2;
-								return null;
-							}
-							mIsRefreshing.set(false);
-							mIsConnectionRestored.set(true);
-							return null;
-						})
-						.excuse(e -> {
-							if (mConnectionTime < 32000) mConnectionTime *= 2;
-							return null;
-						});
-			}
-
-			return null;
-		}
-
-		@Override
-		protected void onPostExecute(Void result) {
-			for (Runnable onConnectionRegainedListener : mUniqueOnConnectionRegainedListeners) onConnectionRegainedListener.run();
-
-			// Let on cancelled clear the completed listeners
-			if (!isCancelled())
-				clearCompleteListeners();
-		}
-
-		@Override
-		protected void onCancelled(Void result) {
-			for (Runnable onCancelListener : mUniqueOnCancelListeners) onCancelListener.run();
-
-			clearCompleteListeners();
-		}
-
-		/* Differs from the normal onCompleteListener in that the onCompleteListener list is emptied every time the Poll Connection Task is runWith
-		 */
-		void addOnConnectionRegainedListener(Runnable listener) {
-			synchronized(mUniqueOnConnectionRegainedListeners) {
-				mUniqueOnConnectionRegainedListeners.add(listener);
-			}
-		}
-
-		/* Differs from the normal onCompleteListener in that the onCompleteListener list is emptied every time the Poll Connection Task is runWith
-		 */
-		void addOnPollingCancelledListener(Runnable listener) {
-			synchronized(mUniqueOnCancelListeners) {
-				mUniqueOnCancelListeners.add(listener);
-			}
-		}
-
-		void removeOnConnectionRegainedListener(Runnable listener) {
-			synchronized(mUniqueOnConnectionRegainedListeners) {
-				mUniqueOnConnectionRegainedListeners.remove(listener);
-			}
-		}
-
-		void removeOnPollingCancelledListener(Runnable listener) {
-			synchronized(mUniqueOnCancelListeners) {
-				mUniqueOnCancelListeners.remove(listener);
-			}
-		}
-
-		private void clearCompleteListeners() {
-			mUniqueOnConnectionRegainedListeners.clear();
-			mUniqueOnCancelListeners.clear();
+		synchronized(mUniqueOnCancelListeners) {
+			mUniqueOnCancelListeners.remove(listener);
 		}
 	}
 }
