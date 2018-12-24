f1d710eed4ad07f1038404cc6e8f96a050120dd5
dvedvick@gmail.com <dvedvick@gmail.com>
2018-12-24 10:16:26 -0600

    Modernize ExoPlayer calls

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/file/exoplayer/preparation/ExoPlayerPlayableFilePreparationSourceProvider.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/file/exoplayer/preparation/ExoPlayerPlayableFilePreparationSourceProvider.java
@@ -2,14 +2,16 @@ package com.lasthopesoftware.bluewater.client.playback.file.exoplayer.preparatio
 
 import android.content.Context;
 import android.os.Handler;
-import com.google.android.exoplayer2.*;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.RenderersFactory;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
-import com.google.android.exoplayer2.upstream.DefaultAllocator;
 import com.google.android.exoplayer2.upstream.cache.Cache;
 import com.lasthopesoftware.bluewater.client.connection.IConnectionProvider;
 import com.lasthopesoftware.bluewater.client.library.items.media.files.uri.BestMatchUriProvider;
 import com.lasthopesoftware.bluewater.client.library.repository.Library;
+import com.lasthopesoftware.bluewater.client.playback.engine.exoplayer.AudioRenderersFactory;
 import com.lasthopesoftware.bluewater.client.playback.engine.preparation.IPlayableFilePreparationSourceProvider;
 import com.lasthopesoftware.bluewater.client.playback.file.exoplayer.preparation.mediasource.ExtractorMediaSourceFactoryProvider;
 import com.lasthopesoftware.bluewater.client.playback.file.preparation.PlayableFilePreparationSource;
@@ -39,7 +41,7 @@ public class ExoPlayerPlayableFilePreparationSourceProvider implements IPlayable
 			library,
 			cache);
 
-		renderersFactory = new DefaultRenderersFactory(context);
+		renderersFactory = new AudioRenderersFactory(context);
 	}
 
 	@Override
@@ -63,13 +65,13 @@ public class ExoPlayerPlayableFilePreparationSourceProvider implements IPlayable
 	}
 
 	private static LoadControl getNewLoadControl() {
-		return new DefaultLoadControl(
-			new DefaultAllocator(false, C.DEFAULT_BUFFER_SEGMENT_SIZE),
+		final DefaultLoadControl.Builder builder = new DefaultLoadControl.Builder();
+		builder.setBufferDurationsMs(
 			DefaultLoadControl.DEFAULT_MIN_BUFFER_MS,
 			maxBufferMs.getObject(),
 			DefaultLoadControl.DEFAULT_BUFFER_FOR_PLAYBACK_MS,
-			DefaultLoadControl.DEFAULT_BUFFER_FOR_PLAYBACK_AFTER_REBUFFER_MS,
-			DefaultLoadControl.DEFAULT_TARGET_BUFFER_BYTES,
-			DefaultLoadControl.DEFAULT_PRIORITIZE_TIME_OVER_SIZE_THRESHOLDS);
+			DefaultLoadControl.DEFAULT_BUFFER_FOR_PLAYBACK_AFTER_REBUFFER_MS);
+
+		return builder.createDefaultLoadControl();
 	}
 }
