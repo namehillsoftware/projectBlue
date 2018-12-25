8dc75a3685c2051c96443d5e2e49e69085c06da1
David Vedvick <dvedvick@gmail.com>
2018-12-25 12:06:36 -0600

    Remove logger

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/library/views/access/LibraryViewsByConnectionProvider.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/library/views/access/LibraryViewsByConnectionProvider.java
@@ -5,16 +5,12 @@ import com.lasthopesoftware.bluewater.client.library.access.RevisionChecker;
 import com.lasthopesoftware.bluewater.client.library.items.Item;
 import com.lasthopesoftware.bluewater.client.library.items.access.ItemResponse;
 import com.namehillsoftware.handoff.promises.Promise;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class LibraryViewsByConnectionProvider implements ProvideLibraryViewsUsingConnection {
 
-    private static final Logger logger = LoggerFactory.getLogger(LibraryViewsByConnectionProvider.class);
-
-    public final static String browseLibraryParameter = "Browse/Children";
+	public final static String browseLibraryParameter = "Browse/Children";
 
     private static List<Item> cachedFileSystemItems;
     private static Integer revision;
