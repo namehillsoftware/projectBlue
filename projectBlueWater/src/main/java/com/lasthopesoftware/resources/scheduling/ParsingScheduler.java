60b10190948185940fe5fb8a7594c3d874bf57f3
David Vedvick <dvedvick@gmail.com>
2018-12-26 14:35:43 -0600

    Increase parsing scheduler complexity

--- projectBlueWater/src/main/java/com/lasthopesoftware/resources/scheduling/ParsingScheduler.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/resources/scheduling/ParsingScheduler.java
@@ -1,14 +1,26 @@
 package com.lasthopesoftware.resources.scheduling;
 
+import android.os.Build;
 import com.namehillsoftware.lazyj.CreateAndHold;
 import com.namehillsoftware.lazyj.Lazy;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
 
 public class ParsingScheduler implements ScheduleParsingWork {
 
-	private static final CreateAndHold<Executor> executor = new Lazy<>(Executors::newCachedThreadPool);
+	private static final CreateAndHold<Executor> executor = new Lazy<>(() -> {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+			return new ForkJoinPool
+				(Runtime.getRuntime().availableProcessors(),
+					ForkJoinPool.defaultForkJoinWorkerThreadFactory,
+					null, true);
+		}
+
+		return Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+	});
+
 	private static final CreateAndHold<ParsingScheduler> scheduler = new Lazy<>(ParsingScheduler::new);
 
 	public static ParsingScheduler instance() {
