e5228805ca9b9d59e0a7355c82edbb82365411be
David Vedvick <dvedvick@gmail.com>
2018-12-26 23:13:45 -0600

    Add tests against the Connection Tester

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/connection/testing/ConnectionTester.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/connection/testing/ConnectionTester.java
@@ -10,29 +10,25 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.io.InputStream;
 
-import static com.lasthopesoftware.bluewater.shared.promises.ForwardedResponse.forward;
-
 public class ConnectionTester implements TestConnections {
 
 	private static final Logger mLogger = LoggerFactory.getLogger(ConnectionTester.class);
 
 	@Override
 	public Promise<Boolean> promiseIsConnectionPossible(IConnectionProvider connectionProvider) {
-		return connectionProvider.promiseResponse("Alive").then(this::doTestSynchronously).then(forward(), e -> false);
+		return connectionProvider.promiseResponse("Alive").then(this::doTestSynchronously, e -> false);
 	}
 
 	private boolean doTestSynchronously(Response response) {
-			try {
-				try (final InputStream is = response.body().byteStream()) {
-					final StandardRequest responseDao = StandardRequest.fromInputStream(is);
+		try (final InputStream is = response.body().byteStream()) {
+			final StandardRequest responseDao = StandardRequest.fromInputStream(is);
 
-					return responseDao != null && responseDao.isStatus();
-				} catch (IOException e) {
-					mLogger.error("Error closing connection, device failure?", e);
-				}
-			} catch (IllegalArgumentException e) {
-				mLogger.warn("Illegal argument passed in", e);
-			}
+			return responseDao != null && responseDao.isStatus();
+		} catch (IOException e) {
+			mLogger.error("Error closing connection, device failure?", e);
+		} catch (IllegalArgumentException e) {
+			mLogger.warn("Illegal argument passed in", e);
+		}
 
 		return false;
 	}
---
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/connection/testing/specs/GivenAConnectionWithBadData/WhenCheckingIfTheConnectionIsPossible.java
@@ -0,0 +1,34 @@
+package com.lasthopesoftware.bluewater.client.connection.testing.specs.GivenAConnectionWithBadData;
+
+import com.lasthopesoftware.bluewater.client.connection.specs.FakeConnectionProvider;
+import com.lasthopesoftware.bluewater.client.connection.testing.ConnectionTester;
+import com.lasthopesoftware.bluewater.shared.promises.extensions.specs.FuturePromise;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.concurrent.ExecutionException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class WhenCheckingIfTheConnectionIsPossible {
+
+	private static boolean result;
+
+	@BeforeClass
+	public static void before() throws ExecutionException, InterruptedException {
+		final ConnectionTester connectionTester = new ConnectionTester();
+		final FakeConnectionProvider connectionProvider = new FakeConnectionProvider();
+		connectionProvider.mapResponse(p -> new FakeConnectionProvider.ResponseTuple(200,
+			("{Response Status=\"NOT-OK\"}}" +
+				"<Item Name=\"Master\">1192</Item>" +
+				"<Item Name=\"Sync\">1192</Item>" +
+				"{{Item Name=\"LibraryStartup\">1501430846</Item>" +
+			"</Response>").getBytes()), "Alive");
+		result = new FuturePromise<>(connectionTester.promiseIsConnectionPossible(connectionProvider)).get();
+	}
+
+	@Test
+	public void thenTheResultIsCorrect() {
+		assertThat(result).isFalse();
+	}
+}
