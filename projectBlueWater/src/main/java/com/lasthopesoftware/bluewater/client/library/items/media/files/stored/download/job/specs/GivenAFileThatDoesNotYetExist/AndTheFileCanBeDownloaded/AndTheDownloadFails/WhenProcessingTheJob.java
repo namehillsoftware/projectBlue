69aa3c0f0da5fdcb7988fbc8e1f3875e10c37107
David Vedvick <dvedvick@gmail.com>
2019-01-06 11:03:26 -0600

    Validate the file is downloading

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/library/items/media/files/stored/download/job/specs/GivenAFileThatDoesNotYetExist/AndTheFileCanBeDownloaded/AndTheDownloadFails/WhenProcessingTheJob.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/library/items/media/files/stored/download/job/specs/GivenAFileThatDoesNotYetExist/AndTheFileCanBeDownloaded/AndTheDownloadFails/WhenProcessingTheJob.java
@@ -8,6 +8,8 @@ import com.lasthopesoftware.bluewater.client.library.items.media.files.stored.IS
 import com.lasthopesoftware.bluewater.client.library.items.media.files.stored.download.exceptions.StoredFileJobException;
 import com.lasthopesoftware.bluewater.client.library.items.media.files.stored.download.job.StoredFileJob;
 import com.lasthopesoftware.bluewater.client.library.items.media.files.stored.download.job.StoredFileJobProcessor;
+import com.lasthopesoftware.bluewater.client.library.items.media.files.stored.download.job.StoredFileJobState;
+import com.lasthopesoftware.bluewater.client.library.items.media.files.stored.download.job.StoredFileJobStatus;
 import com.lasthopesoftware.bluewater.client.library.items.media.files.stored.repository.StoredFile;
 import com.lasthopesoftware.bluewater.client.library.repository.Library;
 import org.junit.BeforeClass;
@@ -15,6 +17,8 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -24,6 +28,7 @@ public class WhenProcessingTheJob {
 
 	private static StoredFileJobException storedFileJobException;
 	private static final StoredFile storedFile = new StoredFile(new Library(), 1, new ServiceFile(1), "test-path", true);
+	private static final List<StoredFileJobState> states = new ArrayList<>();
 
 	@RequiresApi(api = Build.VERSION_CODES.N)
 	@BeforeClass
@@ -48,12 +53,19 @@ public class WhenProcessingTheJob {
 			(is, f) -> { throw new IOException(); });
 
 		try {
-			storedFileJobProcessor.observeStoredFileDownload(
-				new StoredFileJob(new ServiceFile(1), storedFile)).blockingSubscribe();
+			for (StoredFileJobStatus status : storedFileJobProcessor.observeStoredFileDownload(
+					new StoredFileJob(new ServiceFile(1), storedFile)).blockingIterable()) {
+				states.add(status.storedFileJobState);
+			}
 		} catch (Throwable e) {
 			if (e.getCause() instanceof StoredFileJobException)
 				storedFileJobException = (StoredFileJobException)e.getCause();
 		}
+	}
+
+	@Test
+	public void thenTheStoredFileIsDownlaoading() {
+		assertThat(states).containsExactly(StoredFileJobState.Downloading);
 	}
 
 	@Test
