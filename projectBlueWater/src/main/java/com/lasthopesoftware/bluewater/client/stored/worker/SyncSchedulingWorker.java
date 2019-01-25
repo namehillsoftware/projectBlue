97b3fc5454908079da1140a393dfff69583c8599
dvedvick@gmail.com <dvedvick@gmail.com>
2019-01-24 18:43:38 -0600

    Simplify sync scheduling worker

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/stored/worker/SyncSchedulingWorker.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/stored/worker/SyncSchedulingWorker.java
@@ -2,21 +2,15 @@ package com.lasthopesoftware.bluewater.client.stored.worker;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import androidx.work.*;
-import com.annimon.stream.Stream;
-import com.google.common.util.concurrent.ListenableFuture;
 import com.lasthopesoftware.bluewater.client.stored.service.StoredSyncService;
 import com.lasthopesoftware.bluewater.client.stored.worker.constraints.SyncWorkerConstraints;
 import com.lasthopesoftware.bluewater.shared.MagicPropertyBuilder;
-import com.namehillsoftware.handoff.promises.Promise;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 public class SyncSchedulingWorker extends Worker {
@@ -29,7 +23,7 @@ public class SyncSchedulingWorker extends Worker {
 		final PeriodicWorkRequest.Builder periodicWorkRequest = new PeriodicWorkRequest.Builder(SyncSchedulingWorker.class, 3, TimeUnit.HOURS);
 		periodicWorkRequest.setConstraints(constraints(context));
 		return WorkManager.getInstance()
-			.enqueueUniquePeriodicWork(workName, ExistingPeriodicWorkPolicy.REPLACE, periodicWorkRequest.build());
+			.enqueueUniquePeriodicWork(workName, ExistingPeriodicWorkPolicy.KEEP, periodicWorkRequest.build());
 	}
 
 	private static Constraints constraints(Context context) {
@@ -37,27 +31,8 @@ public class SyncSchedulingWorker extends Worker {
 		return new SyncWorkerConstraints(manager).getCurrentConstraints();
 	}
 
-	public static Promise<Boolean> promiseIsScheduled() {
-		return promiseWorkInfos()
-			.then(workInfos -> Stream.of(workInfos).anyMatch(wi -> wi.getState() == WorkInfo.State.ENQUEUED));
-	}
-
 	public static Operation cancel() {
 		return WorkManager.getInstance().cancelUniqueWork(workName);
-	}
-
-	private static Promise<List<WorkInfo>> promiseWorkInfos() {
-		return new Promise<>(m -> {
-			final ListenableFuture<List<WorkInfo>> workInfosByName = WorkManager.getInstance().getWorkInfosForUniqueWork(workName);
-			m.cancellationRequested(() -> workInfosByName.cancel(false));
-			workInfosByName.addListener(() -> {
-				try {
-					m.sendResolution(workInfosByName.get());
-				} catch (ExecutionException | InterruptedException e) {
-					m.sendRejection(e);
-				}
-			}, AsyncTask.THREAD_POOL_EXECUTOR);
-		});
 	}
 
 	private final Context context;
