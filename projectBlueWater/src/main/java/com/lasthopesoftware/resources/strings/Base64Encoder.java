36a3b28655f22b5b31921e8d5d4b9ef0154a7588
dvedvick@gmail.com <dvedvick@gmail.com>
2019-01-31 21:37:24 -0600

    Trim base 64 encoding

--- projectBlueWater/src/main/java/com/lasthopesoftware/resources/strings/Base64Encoder.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/resources/strings/Base64Encoder.java
@@ -5,6 +5,6 @@ import android.util.Base64;
 public class Base64Encoder implements EncodeToBase64 {
 	@Override
 	public String encodeString(String decodedString) {
-		return Base64.encodeToString(decodedString.getBytes(), Base64.DEFAULT);
+		return Base64.encodeToString(decodedString.getBytes(), Base64.DEFAULT).trim();
 	}
 }
