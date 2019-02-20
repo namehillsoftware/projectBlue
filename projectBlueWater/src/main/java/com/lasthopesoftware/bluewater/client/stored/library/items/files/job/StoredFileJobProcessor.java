2b2c3655b35a683079848bb18c8285bfd4d167b8
David Vedvick <dvedvick@gmail.com>
2019-02-19 23:30:19 -0600

    Recursively process the stored file job queue

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/stored/library/items/files/job/StoredFileJobProcessor.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/stored/library/items/files/job/StoredFileJobProcessor.java
@@ -14,6 +14,7 @@ import com.lasthopesoftware.storage.write.permissions.IFileWritePossibleArbitrat
 import com.namehillsoftware.handoff.promises.Promise;
 import com.namehillsoftware.handoff.promises.propagation.CancellationProxy;
 import com.namehillsoftware.handoff.promises.response.VoidResponse;
+import io.reactivex.Emitter;
 import io.reactivex.Observable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,6 +23,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Set;
 
 public class StoredFileJobProcessor implements ProcessStoredFileJobs {
@@ -49,82 +52,24 @@ public class StoredFileJobProcessor implements ProcessStoredFileJobs {
 	public Observable<StoredFileJobStatus> observeStoredFileDownload(Iterable<StoredFileJob> jobs) {
 		final CancellationProxy cancellationProxy = new CancellationProxy();
 		final Observable<StoredFileJobStatus> streamedFileDownload = Observable.create(emitter -> {
-			final Set<StoredFileJob> queuedStoredFileJobs = new HashSet<>();
-			Promise<Void> promisedStreamQueue = Promise.empty();
+			final LinkedList<StoredFileJob> jobsQueue = new LinkedList<>();
+			final Set<StoredFileJob> queuedJobs = new HashSet<>();
 			for (final StoredFileJob job : jobs) {
-				if (!queuedStoredFileJobs.add(job)) continue;
+				if (!queuedJobs.add(job)) continue;
 
 				final StoredFile storedFile = job.getStoredFile();
 				final File file = storedFileFileProvider.getFile(storedFile);
 
 				emitter.onNext(new StoredFileJobStatus(file, storedFile, StoredFileJobState.Queued));
-
-				if (file.exists()) {
-					if (!fileReadPossibleArbitrator.isFileReadPossible(file)) {
-						emitter.onNext(new StoredFileJobStatus(file, storedFile, StoredFileJobState.Unreadable));
-						continue;
-					}
-
-					if (storedFile.isDownloadComplete()) {
-						emitter.onNext(new StoredFileJobStatus(file, storedFile, StoredFileJobState.Downloaded));
-						continue;
-					}
-				}
-
-				if (!fileWritePossibleArbitrator.isFileWritePossible(file)) {
-					emitter.onError(new StoredFileWriteException(file, storedFile));
-					return;
-				}
-
-				final File parent = file.getParentFile();
-				if (parent != null && !parent.exists() && !parent.mkdirs()) {
-					emitter.onError(new StorageCreatePathException(parent));
-					return;
-				}
-
-				if (cancellationProxy.isCancelled()) {
-					emitter.onNext(getCancelledStoredFileJobResult(file, storedFile));
-					return;
-				}
-
-				promisedStreamQueue = promisedStreamQueue.eventually(q -> {
-					if (cancellationProxy.isCancelled()) return Promise.empty();
-
-					emitter.onNext(new StoredFileJobStatus(file, storedFile, StoredFileJobState.Downloading));
-
-					final Promise<InputStream> promisedDownload = storedFiles.promiseDownload(storedFile);
-					cancellationProxy.doCancel(promisedDownload);
-					return promisedDownload;
-				}).then(inputStream -> {
-					try (final InputStream is = inputStream) {
-						if (cancellationProxy.isCancelled()) return getCancelledStoredFileJobResult(file, storedFile);
-
-						this.fileStreamWriter.writeStreamToFile(is, file);
-
-						storedFileAccess.markStoredFileAsDownloaded(storedFile);
-
-						return new StoredFileJobStatus(file, storedFile, StoredFileJobState.Downloaded);
-					} catch (IOException ioe) {
-						logger.error("Error writing file!", ioe);
-						return new StoredFileJobStatus(file, storedFile, StoredFileJobState.Queued);
-					} catch (Throwable t) {
-						throw new StoredFileJobException(storedFile, t);
-					}
-				}, error -> {
-					logger.error("Error getting connection", error);
-
-					if (error instanceof IOException)
-						return new StoredFileJobStatus(file, storedFile, StoredFileJobState.Queued);
-
-					if (error instanceof StoredFileJobException)
-						throw error;
-
-					throw new StoredFileJobException(storedFile, error);
-				})
-				.then(new VoidResponse<>(emitter::onNext));
+				jobsQueue.offer(job);
 			}
 
-			promisedStreamQueue.then(
+			final RecursiveQueueProcessor queueProcessor = new RecursiveQueueProcessor(
+				jobsQueue,
+				emitter,
+				cancellationProxy);
+
+			queueProcessor.processQueue().then(
 				new VoidResponse<>(v -> emitter.onComplete()),
 				new VoidResponse<>(emitter::onError));
 		});
@@ -134,5 +79,85 @@ public class StoredFileJobProcessor implements ProcessStoredFileJobs {
 
 	private StoredFileJobStatus getCancelledStoredFileJobResult(File file, StoredFile storedFile) {
 		return new StoredFileJobStatus(file, storedFile, StoredFileJobState.Cancelled);
+	}
+
+	private class RecursiveQueueProcessor {
+		private final Queue<StoredFileJob> jobsQueue;
+		private final Emitter<StoredFileJobStatus> emitter;
+		private final CancellationProxy cancellationProxy;
+
+		RecursiveQueueProcessor(Queue<StoredFileJob> jobsQueue, Emitter<StoredFileJobStatus> jobStatusEmitter, CancellationProxy cancellationProxy) {
+			this.jobsQueue = jobsQueue;
+			this.emitter = jobStatusEmitter;
+			this.cancellationProxy = cancellationProxy;
+		}
+
+		Promise<Void> processQueue() {
+			final StoredFileJob job = jobsQueue.poll();
+			if (job == null) return Promise.empty();
+
+			final StoredFile storedFile = job.getStoredFile();
+			final File file = storedFileFileProvider.getFile(storedFile);
+
+			if (file.exists()) {
+				if (!fileReadPossibleArbitrator.isFileReadPossible(file)) {
+					emitter.onNext(new StoredFileJobStatus(file, storedFile, StoredFileJobState.Unreadable));
+					return processQueue();
+				}
+
+				if (storedFile.isDownloadComplete()) {
+					emitter.onNext(new StoredFileJobStatus(file, storedFile, StoredFileJobState.Downloaded));
+					return processQueue();
+				}
+			}
+
+			if (!fileWritePossibleArbitrator.isFileWritePossible(file)) {
+				emitter.onError(new StoredFileWriteException(file, storedFile));
+				return processQueue();
+			}
+
+			final File parent = file.getParentFile();
+			if (parent != null && !parent.exists() && !parent.mkdirs()) {
+				emitter.onError(new StorageCreatePathException(parent));
+				return processQueue();
+			}
+
+			if (cancellationProxy.isCancelled()) {
+				emitter.onNext(getCancelledStoredFileJobResult(file, storedFile));
+				return Promise.empty();
+			}
+
+			emitter.onNext(new StoredFileJobStatus(file, storedFile, StoredFileJobState.Downloading));
+
+			final Promise<InputStream> promisedDownload = storedFiles.promiseDownload(storedFile);
+			cancellationProxy.doCancel(promisedDownload);
+
+			return promisedDownload.then(inputStream -> {
+				try (final InputStream is = inputStream) {
+					if (cancellationProxy.isCancelled()) return getCancelledStoredFileJobResult(file, storedFile);
+
+					fileStreamWriter.writeStreamToFile(is, file);
+
+					storedFileAccess.markStoredFileAsDownloaded(storedFile);
+
+					return new StoredFileJobStatus(file, storedFile, StoredFileJobState.Downloaded);
+				} catch (IOException ioe) {
+					logger.error("Error writing file!", ioe);
+					return new StoredFileJobStatus(file, storedFile, StoredFileJobState.Queued);
+				} catch (Throwable t) {
+					throw new StoredFileJobException(storedFile, t);
+				}
+			}, error -> {
+				if (error instanceof IOException)
+					return new StoredFileJobStatus(file, storedFile, StoredFileJobState.Queued);
+
+				if (error instanceof StoredFileJobException)
+					throw error;
+
+				throw new StoredFileJobException(storedFile, error);
+			})
+			.then(new VoidResponse<>(emitter::onNext))
+			.eventually(v -> processQueue());
+		}
 	}
 }
