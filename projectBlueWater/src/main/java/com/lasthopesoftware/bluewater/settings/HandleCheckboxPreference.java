7588d34b2a0d9ebeee7898d4c3fdfee1f2d2c70f
David Vedvick <dvedvick@gmail.com>
2019-08-25 15:48:16 -0500

    Update sync when settings change

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/settings/HandleCheckboxPreference.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/settings/HandleCheckboxPreference.java
@@ -2,45 +2,29 @@ package com.lasthopesoftware.bluewater.settings;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.widget.CheckBox;
+import com.lasthopesoftware.bluewater.client.stored.scheduling.SyncSchedulingWorker;
 
 /**
  * Created by david on 11/14/15.
  */
-class HandleCheckboxPreference extends AsyncTask<Void, Void, Boolean> {
-	private final String settingKey;
-	private final CheckBox settingCheckbox;
-	private final SharedPreferences sharedPreferences;
-
-	public static void handle(Context context, String settingKey, CheckBox settingCheckbox) {
-		new HandleCheckboxPreference(context, settingKey, settingCheckbox).execute();
-	}
-
-	private HandleCheckboxPreference(Context context, String settingKey, CheckBox settingCheckbox) {
+class HandleCheckboxPreference {
+	static void handle(Context context, String settingKey, CheckBox settingCheckbox) {
 		settingCheckbox.setEnabled(false);
-		this.settingKey = settingKey;
-		this.settingCheckbox = settingCheckbox;
-		sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-	}
-
-	@Override
-	protected Boolean doInBackground(Void... params) {
-		return sharedPreferences.getBoolean(settingKey, false);
-	}
-
-	@Override
-	protected void onPostExecute(Boolean result) {
-		super.onPostExecute(result);
-
-		settingCheckbox.setChecked(result);
+		final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+		final boolean preference = sharedPreferences.getBoolean(settingKey, false);
+		settingCheckbox.setChecked(preference);
 		settingCheckbox.setOnCheckedChangeListener(
 			(buttonView, isChecked) ->
+			{
 				sharedPreferences
 					.edit()
 					.putBoolean(settingKey, isChecked)
-					.apply());
+					.apply();
+
+				SyncSchedulingWorker.scheduleSync(buttonView.getContext());
+			});
 		settingCheckbox.setEnabled(true);
 	}
 }
