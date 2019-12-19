7d8ce026bb1f62f35e665d94ff2cc09065df1eb3
David Vedvick <dvedvick@gmail.com>
2019-12-18 23:09:48 -0600

    Add positive checkf or stored file downloads

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/stored/library/sync/SyncChecker.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/stored/library/sync/SyncChecker.java
@@ -3,8 +3,15 @@ package com.lasthopesoftware.bluewater.client.stored.library.sync;
 import com.namehillsoftware.handoff.promises.Promise;
 
 public class SyncChecker implements CheckForSync {
+
+	private final CollectServiceFilesForSync serviceFilesForSync;
+
+	public SyncChecker(CollectServiceFilesForSync serviceFilesForSync) {
+		this.serviceFilesForSync = serviceFilesForSync;
+	}
+
 	@Override
 	public Promise<Boolean> promiseIsSyncNeeded() {
-		return new Promise<>(false);
+		return serviceFilesForSync.promiseServiceFilesToSync().then(sf -> sf.size() > 0);
 	}
 }
--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/stored/library/sync/specs/GivenLibrariesToSync/WhenCheckingIfSyncIsNecessary.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/stored/library/sync/specs/GivenLibrariesToSync/WhenCheckingIfSyncIsNecessary.java
@@ -1,5 +1,6 @@
-package com.lasthopesoftware.bluewater.client.stored.library.sync.specs.GivenNoLibrariesToSync;
+package com.lasthopesoftware.bluewater.client.stored.library.sync.specs.GivenLibrariesToSync;
 
+import com.lasthopesoftware.bluewater.client.library.items.media.files.ServiceFile;
 import com.lasthopesoftware.bluewater.client.stored.library.sync.SyncChecker;
 import com.lasthopesoftware.bluewater.shared.promises.extensions.specs.FuturePromise;
 import com.namehillsoftware.handoff.promises.Promise;
@@ -7,7 +8,7 @@ import com.namehillsoftware.handoff.promises.Promise;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,12 +19,12 @@ public class WhenCheckingIfSyncIsNecessary {
 
 	@BeforeClass
 	public static void before() throws ExecutionException, InterruptedException {
-		final SyncChecker syncChecker = new SyncChecker(() -> new Promise<>(Collections.emptySet()));
+		final SyncChecker syncChecker = new SyncChecker(() -> new Promise<>(Arrays.asList(new ServiceFile(1), new ServiceFile(18))));
 		isSyncNeeded = new FuturePromise<>(syncChecker.promiseIsSyncNeeded()).get();
 	}
 
 	@Test
-	public void thenSyncIsNotNeeded() {
-		assertThat(isSyncNeeded).isFalse();
+	public void thenSyncIsNeeded() {
+		assertThat(isSyncNeeded).isTrue();
 	}
 }
