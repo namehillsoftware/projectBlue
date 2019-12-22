a0bc4eb69c6a8e84eaff90182e062bbcc24c173e
David Vedvick <dvedvick@gmail.com>
2019-12-21 18:09:05 -0600

    Add initial implementation

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/connection/libraries/LibraryConnectionProvider.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/connection/libraries/LibraryConnectionProvider.kt
@@ -4,7 +4,7 @@ import com.lasthopesoftware.bluewater.client.connection.BuildingConnectionStatus
 import com.lasthopesoftware.bluewater.client.connection.ConnectionProvider
 import com.lasthopesoftware.bluewater.client.connection.IConnectionProvider
 import com.lasthopesoftware.bluewater.client.connection.builder.live.ProvideLiveUrl
-import com.lasthopesoftware.bluewater.client.connection.session.SessionConnection.BuildingSessionConnectionStatus
+import com.lasthopesoftware.bluewater.client.connection.okhttp.OkHttpFactory
 import com.lasthopesoftware.bluewater.client.connection.url.IUrlProvider
 import com.lasthopesoftware.bluewater.client.library.access.ILibraryProvider
 import com.lasthopesoftware.bluewater.client.library.access.ILibraryStorage
@@ -14,25 +14,41 @@ import com.lasthopesoftware.bluewater.client.library.repository.LibraryId
 import com.lasthopesoftware.bluewater.client.library.views.access.ProvideLibraryViewsUsingConnection
 import com.lasthopesoftware.bluewater.shared.promises.extensions.ProgressingPromise
 import com.namehillsoftware.handoff.promises.Promise
-import com.namehillsoftware.handoff.promises.response.ImmediateResponse
-import com.namehillsoftware.handoff.promises.response.PromisedResponse
 import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 
-class LibraryConnectionProvider(private val libraryProvider: ILibraryProvider, private val libraryStorage: ILibraryStorage, private val liveUrlProvider: ProvideLiveUrl, private val libraryViewsProvider: ProvideLibraryViewsUsingConnection) : ProvideLibraryConnections {
+class LibraryConnectionProvider(private val libraryProvider: ILibraryProvider, private val libraryStorage: ILibraryStorage, private val liveUrlProvider: ProvideLiveUrl, private val libraryViewsProvider: ProvideLibraryViewsUsingConnection, private val okHttpFactory: OkHttpFactory) : ProvideLibraryConnections {
 
-	private val cachedConnectionProviders = ConcurrentHashMap<Int, IConnectionProvider>()
-	private val promisedConnectionProvidersCache = HashMap<Int, Promise<IConnectionProvider>>()
+	private val cachedConnectionProviders = ConcurrentHashMap<LibraryId, IConnectionProvider>()
+	private val promisedConnectionProvidersCache = HashMap<LibraryId, ProgressingPromise<BuildingConnectionStatus, IConnectionProvider>>()
+	private val buildingConnectionPromiseSync = Any()
 
 	override fun promiseLibraryConnection(libraryId: LibraryId): ProgressingPromise<BuildingConnectionStatus, IConnectionProvider> {
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+		var cachedConnectionProvider = cachedConnectionProviders[libraryId]
+		if (cachedConnectionProvider != null) return ProgressingPromise(cachedConnectionProvider)
+
+		synchronized(buildingConnectionPromiseSync) {
+			cachedConnectionProvider = cachedConnectionProviders[libraryId]
+			if (cachedConnectionProvider != null) return ProgressingPromise(cachedConnectionProvider)
+
+			if (!promisedConnectionProvidersCache.containsKey(libraryId)) promisedConnectionProvidersCache[libraryId] = ProgressingPromise(null)
+			val cachedPromisedProvider = promisedConnectionProvidersCache[libraryId]
+			val nextPromisedConnectionProvider = cachedPromisedProvider!!.eventually(
+				{ c: IConnectionProvider? ->
+					c?.let { Promise(it) }
+						?: promiseUpdatedCachedConnection(libraryId)
+				}
+			) { e: Throwable? -> promiseUpdatedCachedConnection(libraryId) }
+			promisedConnectionProvidersCache[libraryId] = nextPromisedConnectionProvider
+			return nextPromisedConnectionProvider
+		}
 	}
 
 	override fun promiseTestedLibraryConnection(libraryId: LibraryId): ProgressingPromise<BuildingConnectionStatus, IConnectionProvider> {
 		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 	}
 
-	private fun promiseUpdatedCachedConnection(libraryId: Int): Promise<IConnectionProvider?>? {
+	private fun promiseUpdatedCachedConnection(libraryId: LibraryId): ProgressingPromise<BuildingConnectionStatus, IConnectionProvider> {
 		return promiseBuiltSessionConnection(libraryId)
 			.then { c: IConnectionProvider? ->
 				if (c != null) cachedConnectionProviders[libraryId] = c
@@ -40,56 +56,60 @@ class LibraryConnectionProvider(private val libraryProvider: ILibraryProvider, p
 			}
 	}
 
-	private fun promiseBuiltSessionConnection(selectedLibraryId: Int): Promise<IConnectionProvider?> {
-		doStateChange(BuildingSessionConnectionStatus.GettingLibrary)
-		return libraryProvider
-			.getLibrary(selectedLibraryId)
-			.eventually({ library: Library? ->
-				if (library == null || library.accessCode == null || library.accessCode.isEmpty()) {
-					doStateChange(BuildingSessionConnectionStatus.GettingLibraryFailed)
-					return Promise.empty<IConnectionProvider>()
-				}
-				doStateChange(BuildingSessionConnectionStatus.BuildingConnection)
-				liveUrlProvider
-					.promiseLiveUrl(library)
-					.eventually({ urlProvider: IUrlProvider? ->
-						if (urlProvider == null) {
-							doStateChange(BuildingSessionConnectionStatus.BuildingConnectionFailed)
-							return Promise.empty<IConnectionProvider>()
+	private fun promiseBuiltSessionConnection(selectedLibraryId: LibraryId): ProgressingPromise<BuildingConnectionStatus, IConnectionProvider> {
+		return object : ProgressingPromise<BuildingConnectionStatus, IConnectionProvider>() {
+			init {
+				reportProgress(BuildingConnectionStatus.GettingLibrary)
+				libraryProvider
+					.getLibrary(selectedLibraryId.id)
+					.then({ library: Library? ->
+						if (library?.accessCode?.isEmpty() == true) {
+							reportProgress(BuildingConnectionStatus.GettingLibraryFailed)
+							resolve(null)
+							return@then
 						}
-						val localConnectionProvider: IConnectionProvider = ConnectionProvider(urlProvider, okHttpFactory)
-						if (library.selectedView >= 0) {
-							doStateChange(BuildingSessionConnectionStatus.BuildingSessionComplete)
-							return Promise(localConnectionProvider)
-						}
-						doStateChange(BuildingSessionConnectionStatus.GettingView)
-						libraryViewsProvider
-							.promiseLibraryViewsFromConnection(localConnectionProvider)
-							.eventually({ libraryViews: List<Item>? ->
-								if (libraryViews == null || libraryViews.size == 0) {
-									doStateChange(BuildingSessionConnectionStatus.GettingViewFailed)
-									return Promise.empty<IConnectionProvider>()
+						reportProgress(BuildingConnectionStatus.BuildingConnection)
+
+						liveUrlProvider
+							.promiseLiveUrl(library)
+							.then({ urlProvider: IUrlProvider? ->
+								if (urlProvider == null) {
+									reportProgress(BuildingConnectionStatus.BuildingConnectionFailed)
+									resolve(null)
+									return@then
 								}
-								val selectedView = libraryViews[0].key
-								library.selectedView = selectedView
-								library.selectedViewType = Library.ViewType.StandardServerView
-								libraryStorage
-									.saveLibrary(library)
-									.then(ImmediateResponse<Library, IConnectionProvider> { savedLibrary: Library? ->
-										doStateChange(BuildingSessionConnectionStatus.BuildingSessionComplete)
-										localConnectionProvider
-									})
-							}, PromisedResponse<Throwable, IConnectionProvider?> { e: Throwable? ->
-								doStateChange(BuildingSessionConnectionStatus.GettingViewFailed)
-								Promise(e)
-							})
-					}, PromisedResponse<Throwable, IConnectionProvider?> { e: Throwable? ->
-						doStateChange(BuildingSessionConnectionStatus.BuildingConnectionFailed)
-						Promise(e)
-					})
-			}, PromisedResponse<Throwable, IConnectionProvider?> { e: Throwable? ->
-				doStateChange(BuildingSessionConnectionStatus.GettingLibraryFailed)
-				Promise(e)
-			})
+
+								val localConnectionProvider: IConnectionProvider = ConnectionProvider(urlProvider, okHttpFactory)
+								if (library!!.selectedView >= 0) {
+									reportProgress(BuildingConnectionStatus.BuildingSessionComplete)
+									resolve(localConnectionProvider)
+									return@then
+								}
+
+								reportProgress(BuildingConnectionStatus.GettingView)
+
+								libraryViewsProvider
+									.promiseLibraryViewsFromConnection(localConnectionProvider)
+									.then({ libraryViews: List<Item>? ->
+										if (libraryViews == null || libraryViews.isEmpty()) {
+											reportProgress(BuildingConnectionStatus.GettingViewFailed)
+											resolve(null)
+											return@then
+										}
+
+										val selectedView = libraryViews[0].key
+										library.selectedView = selectedView
+										library.selectedViewType = Library.ViewType.StandardServerView
+										libraryStorage
+											.saveLibrary(library)
+											.then { savedLibrary: Library? ->
+												reportProgress(BuildingConnectionStatus.BuildingSessionComplete)
+												resolve(localConnectionProvider)
+											}
+									}, { reportProgress(BuildingConnectionStatus.GettingViewFailed) })
+							}, { reportProgress(BuildingConnectionStatus.BuildingConnectionFailed) })
+					}, { reportProgress(BuildingConnectionStatus.GettingLibraryFailed) })
+			}
+		}
 	}
 }
--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/promises/extensions/ProgressingPromise.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/promises/extensions/ProgressingPromise.kt
@@ -5,15 +5,24 @@ import com.namehillsoftware.handoff.promises.Promise
 import com.vedsoft.futures.runnables.OneParameterAction
 import java.util.concurrent.ConcurrentLinkedQueue
 
-abstract class ProgressingPromise<Progress, Resolution> : Promise<Resolution> {
+open class ProgressingPromise<Progress, Resolution> : Promise<Resolution> {
 	private val updateListeners = ConcurrentLinkedQueue<OneParameterAction<Progress>>()
+	private var p: Progress? = null
 
+	constructor(resolution: Resolution) : super(resolution)
 	constructor(messengerOperator: MessengerOperator<Resolution>?) : super(messengerOperator)
 	protected constructor()
 
-	abstract val progress: Progress
+	var progress: Progress?
+		get() {
+			return p
+		}
+		private set(value) {
+			p = value
+		}
 
 	protected fun reportProgress(progress: Progress) {
+		this.progress = progress;
 		for (action in updateListeners) action.runWith(progress)
 	}
 
