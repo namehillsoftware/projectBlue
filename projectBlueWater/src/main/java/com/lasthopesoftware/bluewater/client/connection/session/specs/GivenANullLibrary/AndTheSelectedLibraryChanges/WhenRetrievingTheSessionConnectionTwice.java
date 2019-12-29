f4b512d7601945eed3754da2d3a8b0c2bec2ca1a
David Vedvick <dvedvick@gmail.com>
2019-12-29 10:57:42 -0600

    Remove test integration

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/connection/session/specs/GivenANullLibrary/AndTheSelectedLibraryChanges/WhenRetrievingTheSessionConnectionTwice.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/connection/session/specs/GivenANullLibrary/AndTheSelectedLibraryChanges/WhenRetrievingTheSessionConnectionTwice.java
@@ -2,21 +2,19 @@ package com.lasthopesoftware.bluewater.client.connection.session.specs.GivenANul
 
 import androidx.test.core.app.ApplicationProvider;
 
+import com.lasthopesoftware.bluewater.client.connection.ConnectionProvider;
 import com.lasthopesoftware.bluewater.client.connection.IConnectionProvider;
-import com.lasthopesoftware.bluewater.client.connection.builder.live.ProvideLiveUrl;
-import com.lasthopesoftware.bluewater.client.connection.libraries.LibraryConnectionProvider;
+import com.lasthopesoftware.bluewater.client.connection.libraries.ProvideLibraryConnections;
 import com.lasthopesoftware.bluewater.client.connection.okhttp.OkHttpFactory;
 import com.lasthopesoftware.bluewater.client.connection.session.SessionConnection;
 import com.lasthopesoftware.bluewater.client.connection.session.specs.SessionConnectionReservation;
-import com.lasthopesoftware.bluewater.client.connection.testing.TestConnections;
 import com.lasthopesoftware.bluewater.client.connection.url.IUrlProvider;
-import com.lasthopesoftware.bluewater.client.library.access.ILibraryProvider;
-import com.lasthopesoftware.bluewater.client.library.repository.Library;
+import com.lasthopesoftware.bluewater.client.library.repository.LibraryId;
 import com.lasthopesoftware.bluewater.client.servers.selection.ISelectedLibraryIdentifierProvider;
+import com.lasthopesoftware.bluewater.shared.promises.extensions.ProgressingPromise;
 import com.lasthopesoftware.bluewater.shared.promises.extensions.specs.FuturePromise;
 import com.lasthopesoftware.resources.specs.ScopedLocalBroadcastManagerBuilder;
 import com.lasthopesoftware.specs.AndroidContext;
-import com.namehillsoftware.handoff.promises.Promise;
 
 import org.junit.Test;
 
@@ -24,7 +22,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.concurrent.ExecutionException;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -36,16 +34,9 @@ public class WhenRetrievingTheSessionConnectionTwice extends AndroidContext {
 	@Override
 	public void before() throws ExecutionException, InterruptedException, IllegalAccessException, InstantiationException, InvocationTargetException {
 
-		final Library library = new Library()
-			.setId(2)
-			.setAccessCode("aB5nf");
-
-		final ILibraryProvider libraryProvider = mock(ILibraryProvider.class);
-		when(libraryProvider.getLibrary(anyInt())).thenReturn(Promise.empty());
-		when(libraryProvider.getLibrary(2)).thenReturn(new Promise<>(library));
-
-		final ProvideLiveUrl liveUrlProvider = mock(ProvideLiveUrl.class);
-		when(liveUrlProvider.promiseLiveUrl(library)).thenReturn(new Promise<>(firstUrlProvider));
+		final ProvideLibraryConnections libraryConnections = mock(ProvideLibraryConnections.class);
+		when(libraryConnections.promiseLibraryConnection(any())).thenReturn(new ProgressingPromise<>((IConnectionProvider)null));
+		when(libraryConnections.promiseLibraryConnection(new LibraryId(2))).thenReturn(new ProgressingPromise<>(new ConnectionProvider(firstUrlProvider, OkHttpFactory.getInstance())));
 
 		final FakeSelectedLibraryProvider fakeSelectedLibraryProvider = new FakeSelectedLibraryProvider();
 
@@ -54,11 +45,7 @@ public class WhenRetrievingTheSessionConnectionTwice extends AndroidContext {
 			final SessionConnection sessionConnection = new SessionConnection(
 				ScopedLocalBroadcastManagerBuilder.newScopedBroadcastManager(ApplicationProvider.getApplicationContext()),
 				() -> 2,
-				new LibraryConnectionProvider(
-					libraryProvider,
-					liveUrlProvider,
-					mock(TestConnections.class),
-					OkHttpFactory.getInstance()));
+				libraryConnections);
 
 			connectionProvider = new FuturePromise<>(sessionConnection.promiseSessionConnection()).get();
 
