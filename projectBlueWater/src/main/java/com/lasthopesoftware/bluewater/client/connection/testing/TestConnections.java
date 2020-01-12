a1ab1db1ae02a9af529b6de1b387b4963f642d9d
David Vedvick <dvedvick@gmail.com>
2020-01-12 08:37:34 -0600

    Convert connection tester to kotlin

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/connection/testing/TestConnections.java
+++
@@ -1,8 +0,0 @@
-package com.lasthopesoftware.bluewater.client.connection.testing;
-
-import com.lasthopesoftware.bluewater.client.connection.IConnectionProvider;
-import com.namehillsoftware.handoff.promises.Promise;
-
-public interface TestConnections {
-	Promise<Boolean> promiseIsConnectionPossible(IConnectionProvider connectionProvider);
-}
