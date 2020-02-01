6fd9b869643c53c6263dfbdf04b75879c3b8c313
David Vedvick <dvedvick@gmail.com>
2020-02-01 14:13:33 -0600

    Make file name text view setter more reliable

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/browsing/items/media/files/menu/FileNameTextViewSetter.java
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/browsing/items/media/files/menu/FileNameTextViewSetter.java
@@ -9,6 +9,7 @@ import com.lasthopesoftware.bluewater.client.browsing.items.media.files.properti
 import com.lasthopesoftware.bluewater.client.browsing.items.media.files.properties.KnownFileProperties;
 import com.lasthopesoftware.bluewater.client.browsing.items.media.files.properties.SessionFilePropertiesProvider;
 import com.lasthopesoftware.bluewater.client.browsing.items.media.files.properties.repository.FilePropertyCache;
+import com.lasthopesoftware.bluewater.client.connection.IConnectionProvider;
 import com.lasthopesoftware.bluewater.client.connection.session.SessionConnection;
 import com.lasthopesoftware.bluewater.shared.exceptions.LoggerUncaughtExceptionHandler;
 import com.lasthopesoftware.bluewater.shared.promises.extensions.LoopedInPromise;
@@ -16,6 +17,9 @@ import com.lasthopesoftware.resources.scheduling.ParsingScheduler;
 import com.namehillsoftware.handoff.promises.Promise;
 import com.namehillsoftware.handoff.promises.propagation.CancellationProxy;
 import com.namehillsoftware.handoff.promises.queued.QueuedPromise;
+import com.namehillsoftware.handoff.promises.response.ImmediateResponse;
+import com.namehillsoftware.handoff.promises.response.PromisedResponse;
+import com.namehillsoftware.handoff.promises.response.VoidResponse;
 
 import org.joda.time.Duration;
 import org.slf4j.Logger;
@@ -38,10 +42,12 @@ public class FileNameTextViewSetter {
 
 	private static final Duration timeoutDuration = Duration.standardMinutes(1);
 
+	private final Object textViewUpdateSync = new Object();
+
 	private final TextView textView;
 	private final Handler handler;
 
-	private volatile Promise<Void> actualPromisedTextViewUpdate = Promise.empty();
+	private volatile Promise<Void> promisedState = Promise.empty();
 	private volatile PromisedTextViewUpdate currentlyPromisedTextViewUpdate;
 
 	public FileNameTextViewSetter(TextView textView) {
@@ -50,19 +56,39 @@ public class FileNameTextViewSetter {
 	}
 
 	public synchronized Promise<Void> promiseTextViewUpdate(ServiceFile serviceFile) {
-		actualPromisedTextViewUpdate.cancel();
+		promisedState.cancel();
 
-		actualPromisedTextViewUpdate = actualPromisedTextViewUpdate
-			.eventually(v -> {
+		promisedState = promisedState
+			.eventually(
+				new EventualTextViewUpdate<>(serviceFile),
+				new EventualTextViewUpdate<>(serviceFile));
+
+		return promisedState;
+	}
+
+	private class EventualTextViewUpdate<Resolution> implements PromisedResponse<Resolution, Void> {
+
+		private final ServiceFile serviceFile;
+
+		EventualTextViewUpdate(ServiceFile serviceFile) {
+			this.serviceFile = serviceFile;
+		}
+
+		@Override
+		public Promise<Void> promiseResponse(Resolution resolution) {
+			synchronized (textViewUpdateSync) {
 				currentlyPromisedTextViewUpdate = new PromisedTextViewUpdate(serviceFile);
 				currentlyPromisedTextViewUpdate.beginUpdate();
 				return currentlyPromisedTextViewUpdate;
-			});
-
-		return actualPromisedTextViewUpdate;
+			}
+		}
 	}
 
-	private class PromisedTextViewUpdate extends Promise<Void> implements Runnable {
+	private class PromisedTextViewUpdate extends Promise<Void> implements
+		Runnable,
+		ImmediateResponse<Map<String, String>, Void>,
+		PromisedResponse<IConnectionProvider, Map<String, String>>
+	{
 
 		private final CancellationProxy cancellationProxy = new CancellationProxy();
 		private final ServiceFile serviceFile;
@@ -74,86 +100,107 @@ public class FileNameTextViewSetter {
 		}
 
 		void beginUpdate() {
-			if (handler.getLooper().getThread() == Thread.currentThread())
+			if (handler.getLooper().getThread() == Thread.currentThread()) {
 				run();
-			else
-				handler.post(this);
+				return;
+			}
+
+			if (handler.post(this)) return;
+
+			logger.warn("Handler failed to post text view update: " + handler);
+			resolve(null);
 		}
 
 		@Override
 		public void run() {
 			textView.setText(R.string.lbl_loading);
 
-			final Promise<Void> promisedViewSet =
+			final Promise<Void> promisedViewSetting =
 				SessionConnection.getInstance(textView.getContext()).promiseSessionConnection()
-					.eventually(connectionProvider -> {
-						if (isUpdateCancelled()) {
-							resolve(null);
-							return new Promise<>(Collections.emptyMap());
-						}
-
-						final FilePropertyCache filePropertyCache = FilePropertyCache.getInstance();
-						final CachedSessionFilePropertiesProvider cachedSessionFilePropertiesProvider =
-							new CachedSessionFilePropertiesProvider(connectionProvider, filePropertyCache,
-								new SessionFilePropertiesProvider(connectionProvider, filePropertyCache, ParsingScheduler.instance()));
-
-						final Promise<Map<String, String>> filePropertiesPromise = cachedSessionFilePropertiesProvider.promiseFileProperties(serviceFile);
-
-						cancellationProxy.doCancel(filePropertiesPromise);
-
-						return filePropertiesPromise;
-					})
-					.eventually(LoopedInPromise.response(properties -> {
-						if (isUpdateCancelled()) return null;
-
-						final String fileName = properties.get(KnownFileProperties.NAME);
-
-						if (fileName != null)
-							textView.setText(fileName);
-
-						return null;
-					}, handler));
+					.eventually(this)
+					.eventually(LoopedInPromise.response(this, handler));
 
 			final Promise<Void> delayPromise = delay(timeoutDuration);
 			cancellationProxy.doCancel(delayPromise);
 
-			Promise.whenAny(promisedViewSet, delayPromise)
+			Promise.whenAny(promisedViewSetting, delayPromise)
 				.must(() -> {
-					cancellationProxy.run();
+					// First, cancel everything if the delay promise finished first
+					delayPromise.then(new VoidResponse<>($ -> cancellationProxy.run()));
+					// Then cancel the delay promise, in case the promised view setting
+					// finished first
+					delayPromise.cancel();
+
+					// Finally, always resolve the parent promise
 					resolve(null);
 				})
 				.excuse(forward())
 				.eventually(e -> new QueuedPromise<>(() -> {
-					if (isUpdateCancelled()) return null;
-
-					if (e instanceof CancellationException) return null;
-
-					if (e instanceof SocketException) {
-						final String message = e.getMessage();
-						if (message != null && message.toLowerCase().contains("socket closed"))
-							return null;
-					}
-
-					if (e instanceof IOException) {
-						final String message = e.getMessage();
-						if (message != null && message.toLowerCase().contains("canceled"))
-							return null;
-					}
-
-					if (e instanceof SSLProtocolException) {
-						final String message = e.getMessage();
-						if (message != null && message.toLowerCase().contains("ssl handshake aborted"))
-							return null;
-					}
-
-					logger.error("An error occurred getting the file properties for the file with ID " + serviceFile.getKey(), e);
-
+					handleError(e);
 					return null;
 				}, LoggerUncaughtExceptionHandler.getErrorExecutor()));
 		}
 
+		@Override
+		public Promise<Map<String, String>> promiseResponse(IConnectionProvider connectionProvider) {
+			if (isNotCurrentPromise() || isUpdateCancelled())
+				return new Promise<>(Collections.emptyMap());
+
+			final FilePropertyCache filePropertyCache = FilePropertyCache.getInstance();
+			final CachedSessionFilePropertiesProvider cachedSessionFilePropertiesProvider =
+				new CachedSessionFilePropertiesProvider(connectionProvider, filePropertyCache,
+					new SessionFilePropertiesProvider(connectionProvider, filePropertyCache, ParsingScheduler.instance()));
+
+			final Promise<Map<String, String>> filePropertiesPromise = cachedSessionFilePropertiesProvider.promiseFileProperties(serviceFile);
+
+			cancellationProxy.doCancel(filePropertiesPromise);
+
+			return filePropertiesPromise;
+		}
+
+		@Override
+		public Void respond(Map<String, String> properties) {
+			if (isNotCurrentPromise() || isUpdateCancelled()) return null;
+
+			final String fileName = properties.get(KnownFileProperties.NAME);
+
+			if (fileName != null)
+				textView.setText(fileName);
+
+			return null;
+		}
+
+		private void handleError(Throwable e) {
+			if (isUpdateCancelled()) return;
+
+			if (e instanceof CancellationException) return;
+
+			if (e instanceof SocketException) {
+				final String message = e.getMessage();
+				if (message != null && message.toLowerCase().contains("socket closed")) return;
+			}
+
+			if (e instanceof IOException) {
+				final String message = e.getMessage();
+				if (message != null && message.toLowerCase().contains("canceled")) return;
+			}
+
+			if (e instanceof SSLProtocolException) {
+				final String message = e.getMessage();
+				if (message != null && message.toLowerCase().contains("ssl handshake aborted")) return;
+			}
+
+			logger.error("An error occurred getting the file properties for the file with ID " + serviceFile.getKey(), e);
+		}
+
+		private boolean isNotCurrentPromise() {
+			synchronized (textViewUpdateSync) {
+				return currentlyPromisedTextViewUpdate != this;
+			}
+		}
+
 		private boolean isUpdateCancelled() {
-			return currentlyPromisedTextViewUpdate != this || cancellationProxy.isCancelled();
+			return cancellationProxy.isCancelled();
 		}
 	}
 }
