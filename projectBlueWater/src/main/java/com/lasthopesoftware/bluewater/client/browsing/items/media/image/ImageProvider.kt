82628db615baf55ce982490e8073f890d1901ed8
dvedvick@gmail.com <dvedvick@gmail.com>
2020-04-19 22:10:59 -0500

    Handle server error codes gracefully

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/browsing/items/media/image/ImageProvider.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/browsing/items/media/image/ImageProvider.kt
@@ -10,20 +10,16 @@ import com.namehillsoftware.handoff.promises.queued.MessageWriter
 import com.namehillsoftware.handoff.promises.queued.QueuedPromise
 
 open class ImageProvider(private val selectedLibraryId: ISelectedLibraryIdentifierProvider, private val rawImages: GetRawImages) {
-	open fun promiseFileBitmap(serviceFile: ServiceFile?): Promise<Bitmap?>? {
+	open fun promiseFileBitmap(serviceFile: ServiceFile?): Promise<Bitmap?> {
 		return rawImages.promiseImageBytes(selectedLibraryId.selectedLibraryId, serviceFile!!)
 			.eventually { bytes -> QueuedPromise(BitmapWriter(bytes), ParsingScheduler.instance().scheduler) }
 	}
 
 	private class BitmapWriter internal constructor(private val imageBytes: ByteArray) : MessageWriter<Bitmap?> {
 		override fun prepareMessage(): Bitmap? {
-			return if (imageBytes.isNotEmpty()) getBitmapFromBytes(imageBytes) else null
-		}
-	}
-
-	companion object {
-		private fun getBitmapFromBytes(imageBytes: ByteArray): Bitmap? {
-			return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+			return (
+				if (imageBytes.isNotEmpty()) BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+				else null)
 		}
 	}
 }
