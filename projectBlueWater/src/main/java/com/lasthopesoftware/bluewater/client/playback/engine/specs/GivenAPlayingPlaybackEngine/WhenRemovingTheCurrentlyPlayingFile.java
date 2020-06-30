a714dba226e3cc109d1371b90db06e9475e3e7bf
David Vedvick <dvedvick@gmail.com>
2020-06-29 23:02:02 -0500

    Add expectation for removing currently playing file

---
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/engine/specs/GivenAPlayingPlaybackEngine/WhenRemovingTheCurrentlyPlayingFile.java
@@ -0,0 +1,92 @@
+package com.lasthopesoftware.bluewater.client.playback.engine.specs.GivenAPlayingPlaybackEngine;
+
+import com.lasthopesoftware.bluewater.client.browsing.items.media.files.ServiceFile;
+import com.lasthopesoftware.bluewater.client.browsing.items.media.files.access.stringlist.FileStringListUtilities;
+import com.lasthopesoftware.bluewater.client.browsing.items.media.files.nowplaying.storage.NowPlayingRepository;
+import com.lasthopesoftware.bluewater.client.browsing.items.media.files.properties.KnownFileProperties;
+import com.lasthopesoftware.bluewater.client.browsing.items.media.files.properties.repository.FilePropertiesContainer;
+import com.lasthopesoftware.bluewater.client.browsing.items.media.files.properties.repository.IFilePropertiesContainerRepository;
+import com.lasthopesoftware.bluewater.client.browsing.library.access.ILibraryStorage;
+import com.lasthopesoftware.bluewater.client.browsing.library.access.ISpecificLibraryProvider;
+import com.lasthopesoftware.bluewater.client.browsing.library.repository.Library;
+import com.lasthopesoftware.bluewater.client.playback.engine.PlaybackEngine;
+import com.lasthopesoftware.bluewater.client.playback.engine.bootstrap.PlaylistPlaybackBootstrapper;
+import com.lasthopesoftware.bluewater.client.playback.engine.preparation.PreparedPlaybackQueueResourceManagement;
+import com.lasthopesoftware.bluewater.client.playback.file.PositionedPlayingFile;
+import com.lasthopesoftware.bluewater.client.playback.file.preparation.queues.CompletingFileQueueProvider;
+import com.lasthopesoftware.bluewater.client.playback.file.preparation.specs.fakes.FakeDeferredPlayableFilePreparationSourceProvider;
+import com.lasthopesoftware.bluewater.client.playback.volume.PlaylistVolumeManager;
+import com.lasthopesoftware.bluewater.shared.UrlKeyHolder;
+import com.lasthopesoftware.bluewater.shared.promises.extensions.specs.FuturePromise;
+import com.namehillsoftware.handoff.promises.Promise;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+public class WhenRemovingTheCurrentlyPlayingFile {
+
+	private static CompletingFileQueueProvider fileQueueProvider = spy(new CompletingFileQueueProvider());
+	private static final Library library = new Library();
+	private static PositionedPlayingFile positionedPlayingFile;
+
+	@BeforeClass
+	public static void before() throws InterruptedException, ExecutionException, TimeoutException {
+		final FakeDeferredPlayableFilePreparationSourceProvider fakePlaybackPreparerProvider = new FakeDeferredPlayableFilePreparationSourceProvider();
+
+		library.setId(1);
+		library.setSavedTracksString(new FuturePromise<>(FileStringListUtilities.promiseSerializedFileStringList(Arrays.asList(
+			new ServiceFile(1),
+			new ServiceFile(2),
+			new ServiceFile(3),
+			new ServiceFile(4),
+			new ServiceFile(5)))).get());
+		library.setNowPlayingId(3);
+
+		final ISpecificLibraryProvider libraryProvider = () -> new Promise<>(library);
+		final ILibraryStorage libraryStorage = Promise::new;
+
+		final IFilePropertiesContainerRepository filePropertiesContainerRepository = mock(IFilePropertiesContainerRepository.class);
+		when(filePropertiesContainerRepository.getFilePropertiesContainer(new UrlKeyHolder<>("", new ServiceFile(5))))
+			.thenReturn(new FilePropertiesContainer(1, new HashMap<String, String>() {{
+				put(KnownFileProperties.DURATION, "100");
+			}}));
+
+		final PlaybackEngine playbackEngine = new PlaybackEngine(
+			new PreparedPlaybackQueueResourceManagement(
+				fakePlaybackPreparerProvider,
+				() -> 1),
+			Collections.singletonList(fileQueueProvider),
+			new NowPlayingRepository(libraryProvider, libraryStorage),
+			new PlaylistPlaybackBootstrapper(new PlaylistVolumeManager(1.0f)));
+
+		new FuturePromise<>(playbackEngine.resume()).get(1, TimeUnit.SECONDS);
+
+		fakePlaybackPreparerProvider.deferredResolution.resolve();
+
+		playbackEngine.setOnPlayingFileChanged(c -> positionedPlayingFile = c);
+
+		new FuturePromise<>(playbackEngine.removeFileAtPosition(3)).get(1, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void thenTheCurrentlyPlayingFilePositionIsTheSame() {
+		assertThat(library.getNowPlayingId()).isEqualTo(3);
+	}
+
+	@Test
+	public void thenTheFileQueueIsShiftedToTheNextFile() {
+		assertThat(positionedPlayingFile.getServiceFile()).isEqualTo(new ServiceFile(4));
+	}
+}
