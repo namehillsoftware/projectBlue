e489da8b52956b9e7d98e9cfda9cda8776c9663e
David Vedvick <dvedvick@gmail.com>
2020-07-30 21:04:38 -0500

    Set File Downloads layout manager

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/stored/library/items/files/fragment/ActiveFileDownloadsFragment.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/stored/library/items/files/fragment/ActiveFileDownloadsFragment.kt
@@ -13,6 +13,7 @@ import android.widget.ProgressBar
 import android.widget.RelativeLayout
 import androidx.fragment.app.Fragment
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.lasthopesoftware.bluewater.R
 import com.lasthopesoftware.bluewater.client.browsing.library.access.LibraryRepository
@@ -35,12 +36,17 @@ class ActiveFileDownloadsFragment : Fragment() {
 	private val localBroadcastManager = lazy { LocalBroadcastManager.getInstance(activity!!) }
 
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-		val viewFileslayout = inflater.inflate(R.layout.layout_downloads, container, false) as RelativeLayout
-		val progressBar = viewFileslayout.findViewById<ProgressBar>(R.id.pbLoadingItems)
-		val listView: RecyclerView = viewFileslayout.findViewById(R.id.itemsRecyclerView)
+		val viewFilesLayout = inflater.inflate(R.layout.layout_downloads, container, false) as RelativeLayout
+		val progressBar = viewFilesLayout.findViewById<ProgressBar>(R.id.pbLoadingItems)
+		val listView = viewFilesLayout.findViewById<RecyclerView>(R.id.itemsRecyclerView)
 		listView.visibility = View.INVISIBLE
 		progressBar.visibility = View.VISIBLE
-		val activity = activity ?: return viewFileslayout
+
+		val activity = activity ?: return viewFilesLayout
+		val activeFileDownloadsAdapter = ActiveFileDownloadsAdapter(activity)
+		listView.adapter = activeFileDownloadsAdapter
+		listView.layoutManager = LinearLayoutManager(activity)
+
 		val libraryRepository = LibraryRepository(activity)
 		val selectedBrowserLibraryProvider = SelectedBrowserLibraryProvider(
 			SelectedBrowserLibraryIdentifierProvider(activity),
@@ -59,8 +65,6 @@ class ActiveFileDownloadsFragment : Fragment() {
 							.filter { f: StoredFile? -> f!!.libraryId == library.id }
 							.associateBy { f -> f.id }
 							.toMutableMap()
-
-						val activeFileDownloadsAdapter = ActiveFileDownloadsAdapter(activity)
 
 						onFileDownloadedReceiver?.run { localBroadcastManager.value.unregisterReceiver(this)	}
 						localBroadcastManager.value.registerReceiver(
@@ -93,13 +97,12 @@ class ActiveFileDownloadsFragment : Fragment() {
 							}.apply { onFileQueuedReceiver = this },
 							IntentFilter(StoredFileSynchronization.onFileQueuedEvent))
 
-						listView.adapter = activeFileDownloadsAdapter
 						progressBar.visibility = View.INVISIBLE
 						listView.visibility = View.VISIBLE
 					}, activity))
 			}
 
-		val toggleSyncButton = viewFileslayout.findViewById<Button>(R.id.toggleSyncButton)
+		val toggleSyncButton = viewFilesLayout.findViewById<Button>(R.id.toggleSyncButton)
 		val startSyncLabel = activity.getText(R.string.start_sync_button)
 		val stopSyncLabel = activity.getText(R.string.stop_sync_button)
 		toggleSyncButton.text = if (!StoredSyncService.isSyncRunning) startSyncLabel else stopSyncLabel
@@ -125,7 +128,7 @@ class ActiveFileDownloadsFragment : Fragment() {
 
 		toggleSyncButton.setOnClickListener { v-> if (StoredSyncService.isSyncRunning) StoredSyncService.cancelSync(v.context) else StoredSyncService.doSyncUninterrupted(v.context) }
 		toggleSyncButton.isEnabled = true
-		return viewFileslayout
+		return viewFilesLayout
 	}
 
 	override fun onDestroy() {
