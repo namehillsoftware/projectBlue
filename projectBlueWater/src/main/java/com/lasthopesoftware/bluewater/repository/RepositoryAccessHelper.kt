613721fde8824c7c9ac033a514cd4b10d786eb05
David Vedvick <dvedvick@gmail.com>
2020-08-05 13:20:24 -0500

    Synchronize database access, add to list rather than set with index

---
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/repository/RepositoryAccessHelper.kt
@@ -0,0 +1,54 @@
+package com.lasthopesoftware.bluewater.repository
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import com.lasthopesoftware.bluewater.client.browsing.items.media.files.cached.repository.CachedFile
+import com.lasthopesoftware.bluewater.client.browsing.library.repository.Library
+import com.lasthopesoftware.bluewater.client.stored.library.items.StoredItem
+import com.lasthopesoftware.bluewater.client.stored.library.items.files.repository.StoredFileEntityCreator
+import com.lasthopesoftware.bluewater.client.stored.library.items.files.repository.StoredFileEntityUpdater
+import com.lasthopesoftware.resources.executors.CachedSingleThreadExecutor
+import com.namehillsoftware.artful.Artful
+import java.io.Closeable
+import java.util.concurrent.Executor
+import java.util.concurrent.locks.ReentrantReadWriteLock
+
+class RepositoryAccessHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION), Closeable {
+
+	companion object {
+		@JvmStatic
+		fun databaseExecutor(): Executor {
+			return databaseExecutor.value
+		}
+
+		private val databaseSynchronization = lazy { ReentrantReadWriteLock() }
+		private val databaseExecutor = lazy { CachedSingleThreadExecutor() }
+		private const val DATABASE_VERSION = 8
+		private const val DATABASE_NAME = "sessions_db"
+		private val entityCreators = lazy { arrayOf(Library(), StoredFileEntityCreator(), StoredItem(), CachedFile()) }
+		private val entityUpdaters = lazy { arrayOf(Library(), StoredFileEntityUpdater(), StoredItem(), CachedFile()) }
+	}
+
+	private val sqliteDb = lazy { this.writableDatabase }
+
+	fun mapSql(sqlQuery: String?): Artful = Artful(sqliteDb.value, sqlQuery)
+
+	override fun onCreate(db: SQLiteDatabase) {
+		for (entityCreator in entityCreators.value) entityCreator.onCreate(db)
+	}
+
+	override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+		for (entityUpdater in entityUpdaters.value) entityUpdater.onUpdate(db, oldVersion, newVersion)
+	}
+
+	fun beginTransaction(): CloseableTransaction = CloseableTransaction(sqliteDb.value, databaseSynchronization.value)
+
+	fun beginNonExclusiveTransaction(): CloseableNonExclusiveTransaction =
+		CloseableNonExclusiveTransaction(sqliteDb.value, databaseSynchronization.value)
+
+	override fun close() {
+		super.close()
+		if (sqliteDb.isInitialized()) sqliteDb.value.close()
+	}
+}
