6bb56c4c8bda2e02a8193f394f0278f0e0999682
David Vedvick <dvedvick@gmail.com>
2020-08-26 01:16:34 -0500

    Fix build to fail when tests fail

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-docker-compose build
-docker-compose run gradle test
-docker-compose run gradle :projectBlueWater:assembleRelease
+docker-compose build \
+&& docker-compose run gradle test \
+&& docker-compose run gradle :projectBlueWater:assembleRelease \
+&& docker-compose run gradle :projectBlue:clean
