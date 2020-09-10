2de5e3a1acb94b517394cd8d69019d121374ecc9
David Vedvick <dvedvick@gmail.com>
2020-09-09 22:09:25 -0500

    Resolve deprecation warning in `MediaSourceProvider`

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/file/exoplayer/preparation/mediasource/MediaSourceProvider.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/file/exoplayer/preparation/mediasource/MediaSourceProvider.kt
@@ -1,61 +1,38 @@
-package com.lasthopesoftware.bluewater.client.playback.file.exoplayer.preparation.mediasource;
+package com.lasthopesoftware.bluewater.client.playback.file.exoplayer.preparation.mediasource
 
-import android.net.Uri;
+import android.net.Uri
+import com.google.android.exoplayer2.extractor.mp3.Mp3Extractor
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultLoadErrorHandlingPolicy
+import com.google.android.exoplayer2.upstream.FileDataSource
+import com.google.android.exoplayer2.upstream.cache.Cache
+import com.google.android.exoplayer2.upstream.cache.CacheDataSourceFactory
+import com.lasthopesoftware.bluewater.client.browsing.library.repository.Library
+import com.lasthopesoftware.bluewater.shared.IoCommon
 
-import com.google.android.exoplayer2.extractor.ExtractorsFactory;
-import com.google.android.exoplayer2.extractor.mp3.Mp3Extractor;
-import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.ProgressiveMediaSource;
-import com.google.android.exoplayer2.upstream.DefaultLoadErrorHandlingPolicy;
-import com.google.android.exoplayer2.upstream.FileDataSourceFactory;
-import com.google.android.exoplayer2.upstream.HttpDataSource;
-import com.google.android.exoplayer2.upstream.cache.Cache;
-import com.google.android.exoplayer2.upstream.cache.CacheDataSourceFactory;
-import com.lasthopesoftware.bluewater.client.browsing.library.repository.Library;
-import com.lasthopesoftware.bluewater.shared.IoCommon;
-import com.namehillsoftware.lazyj.AbstractSynchronousLazy;
-import com.namehillsoftware.lazyj.CreateAndHold;
-import com.namehillsoftware.lazyj.Lazy;
+class MediaSourceProvider(library: Library, dataSourceFactoryProvider: ProvideHttpDataSourceFactory, cache: Cache) : SpawnMediaSources {
 
-public class MediaSourceProvider implements SpawnMediaSources {
-
-	private static final CreateAndHold<ExtractorsFactory> extractorsFactory = new Lazy<>(() -> Mp3Extractor.FACTORY);
-
-	private final CreateAndHold<ProgressiveMediaSource.Factory> lazyFileExtractorFactory = new AbstractSynchronousLazy<ProgressiveMediaSource.Factory>() {
-		@Override
-		protected ProgressiveMediaSource.Factory create() {
-			return new ProgressiveMediaSource.Factory(new FileDataSourceFactory(), extractorsFactory.getObject());
-		}
-	};
-
-	private final CreateAndHold<ProgressiveMediaSource.Factory> lazyRemoteExtractorFactory;
-
-	public MediaSourceProvider(Library library, ProvideHttpDataSourceFactory dataSourceFactoryProvider, Cache cache) {
-
-		lazyRemoteExtractorFactory = new AbstractSynchronousLazy<ProgressiveMediaSource.Factory>() {
-			@Override
-			protected ProgressiveMediaSource.Factory create() {
-				final HttpDataSource.Factory httpDataSourceFactory = dataSourceFactoryProvider.getHttpDataSourceFactory(library);
-
-				final CacheDataSourceFactory cacheDataSourceFactory = new CacheDataSourceFactory(
-					cache,
-					httpDataSourceFactory);
-
-				final ProgressiveMediaSource.Factory factory = new ProgressiveMediaSource.Factory(cacheDataSourceFactory, extractorsFactory.getObject());
-				factory.setLoadErrorHandlingPolicy(new DefaultLoadErrorHandlingPolicy(DefaultLoadErrorHandlingPolicy.DEFAULT_MIN_LOADABLE_RETRY_COUNT_PROGRESSIVE_LIVE));
-				return factory;
-			}
-		};
+	companion object {
+		private val extractorsFactory = lazy { Mp3Extractor.FACTORY }
 	}
 
-	@Override
-	public MediaSource getNewMediaSource(Uri uri) {
-		return getFactory(uri).createMediaSource(uri);
+	private val lazyFileExtractorFactory = lazy {
+		ProgressiveMediaSource.Factory(FileDataSource.Factory(), extractorsFactory.value)
 	}
 
-	private ProgressiveMediaSource.Factory getFactory(Uri uri) {
-		return IoCommon.FileUriScheme.equalsIgnoreCase(uri.getScheme())
-			? lazyFileExtractorFactory.getObject()
-			: lazyRemoteExtractorFactory.getObject();
+	private val lazyRemoteExtractorFactory = lazy {
+		val httpDataSourceFactory = dataSourceFactoryProvider.getHttpDataSourceFactory(library)
+		val cacheDataSourceFactory = CacheDataSourceFactory(
+			cache,
+			httpDataSourceFactory)
+		val factory = ProgressiveMediaSource.Factory(cacheDataSourceFactory, extractorsFactory.value)
+		factory.setLoadErrorHandlingPolicy(DefaultLoadErrorHandlingPolicy(DefaultLoadErrorHandlingPolicy.DEFAULT_MIN_LOADABLE_RETRY_COUNT_PROGRESSIVE_LIVE))
 	}
+
+	override fun getNewMediaSource(uri: Uri): MediaSource = getFactory(uri).createMediaSource(uri)
+
+	private fun getFactory(uri: Uri): ProgressiveMediaSource.Factory =
+		if (IoCommon.FileUriScheme.equals(uri.scheme, ignoreCase = true)) lazyFileExtractorFactory.value
+		else lazyRemoteExtractorFactory.value
 }
