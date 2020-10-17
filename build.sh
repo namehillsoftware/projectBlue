121bbf788f61063db112be7bc11a1199f04ebd5e
David Vedvick <dvedvick@gmail.com>
2020-10-16 22:38:24 -0500

    Improve build
    - Add `.dockerignore`
    - Execute build tasks in same docker image
    - Copy files into docker image

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-docker-compose build \
-&& docker-compose run gradle test \
-&& docker-compose run gradle :projectBlueWater:assembleRelease \
-&& docker-compose run gradle :projectBlue:clean
+docker-compose build && docker-compose run gradle \
+  :projectBlueWater:testReleaseUnitTest \
+  :projectBlueWater:assembleRelease
