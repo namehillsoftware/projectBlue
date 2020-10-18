3cf1a742c7f3819a5ead37d0fcfcf3a8d4fb72ec
David Vedvick <dvedvick@gmail.com>
2020-10-18 11:11:26 -0500

    Copy results out of container

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
-docker-compose build && docker-compose run gradle \
+# Generate a random ID for the build, to be used for finding the build in the docker host.
+# This magic is taken from this stack overflow answer - https://stackoverflow.com/a/34329799/1189542.
+BUILD_ID="$(od  -vN "8" -An -tx1  /dev/urandom | tr -d " \n")"
+
+echo "Build ID: ${BUILD_ID}"
+
+docker-compose build && docker-compose run --name ${BUILD_ID} gradle \
   :projectBlueWater:testReleaseUnitTest \
-  :projectBlueWater:assembleRelease
+  :projectBlueWater:bundleRelease
+EXIT_CODE=${PIPESTATUS[0]}
+
+BUILD_CONTAINER=${BUILD_ID}
+
+docker container cp "${BUILD_CONTAINER}":/src/projectBlueWater/release ./release
+docker container rm "${BUILD_CONTAINER}"
+
+exit "${EXIT_CODE}"
