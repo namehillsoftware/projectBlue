6d9bc9242c8210c39d93fbde6b61170ed5336861
David Vedvick <dvedvick@gmail.com>
2020-12-25 13:33:48 -0600

    Simplify arbitrators

--- projectBlueWater/src/main/java/com/lasthopesoftware/storage/read/permissions/FileReadPossibleArbitrator.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/storage/read/permissions/FileReadPossibleArbitrator.kt
@@ -4,8 +4,5 @@ import com.lasthopesoftware.storage.walkUpUntilFileExists
 import java.io.File
 
 class FileReadPossibleArbitrator : IFileReadPossibleArbitrator {
-	override fun isFileReadPossible(file: File): Boolean {
-		val firstFileThatExists = file.walkUpUntilFileExists()
-		return firstFileThatExists?.canRead() ?: false
-	}
+	override fun isFileReadPossible(file: File): Boolean = file.walkUpUntilFileExists()?.canRead() ?: false
 }
--- projectBlueWater/src/main/java/com/lasthopesoftware/storage/write/permissions/FileWritePossibleArbitrator.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/storage/write/permissions/FileWritePossibleArbitrator.kt
@@ -4,8 +4,5 @@ import com.lasthopesoftware.storage.walkUpUntilFileExists
 import java.io.File
 
 class FileWritePossibleArbitrator : IFileWritePossibleArbitrator {
-	override fun isFileWritePossible(file: File): Boolean {
-		val firstFileThatExists = file.walkUpUntilFileExists()
-		return firstFileThatExists?.canWrite() ?: false
-	}
+	override fun isFileWritePossible(file: File): Boolean = file.walkUpUntilFileExists()?.canWrite() ?: false
 }
