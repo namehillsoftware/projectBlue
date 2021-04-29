7536e6769ca787aa6b690e5b31c7154f2bffdf86
David Vedvick <dvedvick@gmail.com>
2021-04-28 22:05:28 -0500

    Do not reject buffering when media source error occurs

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/file/exoplayer/buffering/BufferingExoPlayer.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/file/exoplayer/buffering/BufferingExoPlayer.kt
@@ -1,70 +1,47 @@
-package com.lasthopesoftware.bluewater.client.playback.file.exoplayer.buffering;
+package com.lasthopesoftware.bluewater.client.playback.file.exoplayer.buffering
 
-import android.os.Handler;
+import android.os.Handler
+import com.google.android.exoplayer2.ExoPlaybackException
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.source.LoadEventInfo
+import com.google.android.exoplayer2.source.MediaLoadData
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.MediaSourceEventListener
+import com.lasthopesoftware.bluewater.client.playback.exoplayer.PromisingExoPlayer
+import com.lasthopesoftware.bluewater.client.playback.file.buffering.IBufferingPlaybackFile
+import com.namehillsoftware.handoff.promises.Promise
+import org.slf4j.LoggerFactory
+import java.io.IOException
 
-import androidx.annotation.Nullable;
+class BufferingExoPlayer(handler: Handler, private val mediaSource: MediaSource, private val exoPlayer: PromisingExoPlayer) : Promise<IBufferingPlaybackFile>(), IBufferingPlaybackFile, MediaSourceEventListener, Player.EventListener {
 
-import com.google.android.exoplayer2.source.LoadEventInfo;
-import com.google.android.exoplayer2.source.MediaLoadData;
-import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.source.MediaSourceEventListener;
-import com.lasthopesoftware.bluewater.client.playback.file.buffering.IBufferingPlaybackFile;
-import com.namehillsoftware.handoff.promises.Promise;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-
-public class BufferingExoPlayer
-extends Promise<IBufferingPlaybackFile>
-implements
-	IBufferingPlaybackFile,
-	MediaSourceEventListener
-{
-	private static final Logger logger = LoggerFactory.getLogger(BufferingExoPlayer.class);
-	private final MediaSource mediaSource;
-
-	@Override
-	public Promise<IBufferingPlaybackFile> promiseBufferedPlaybackFile() {
-		return this;
+	companion object {
+		private val logger = LoggerFactory.getLogger(BufferingExoPlayer::class.java)
 	}
 
-	public BufferingExoPlayer(Handler handler, MediaSource mediaSource) {
-		this.mediaSource = mediaSource;
-		mediaSource.addEventListener(handler, this);
+	init {
+		mediaSource.addEventListener(handler, this)
+		exoPlayer.addListener(this)
 	}
 
-	@Override
-	public void onLoadStarted(int windowIndex, @Nullable MediaSource.MediaPeriodId mediaPeriodId, LoadEventInfo loadEventInfo, MediaLoadData mediaLoadData) {
-
+	override fun promiseBufferedPlaybackFile(): Promise<IBufferingPlaybackFile> {
+		return this
 	}
 
-	@Override
-	public void onLoadCompleted(int windowIndex, @Nullable MediaSource.MediaPeriodId mediaPeriodId, LoadEventInfo loadEventInfo, MediaLoadData mediaLoadData) {
-		resolve(this);
-		mediaSource.removeEventListener(this);
+	override fun onLoadCompleted(windowIndex: Int, mediaPeriodId: MediaSource.MediaPeriodId?, loadEventInfo: LoadEventInfo, mediaLoadData: MediaLoadData) {
+		resolve(this)
+		mediaSource.removeEventListener(this)
+		exoPlayer.removeListener(this)
 	}
 
-	@Override
-	public void onLoadCanceled(int windowIndex, @Nullable MediaSource.MediaPeriodId mediaPeriodId, LoadEventInfo loadEventInfo, MediaLoadData mediaLoadData) {
-
+	override fun onLoadError(windowIndex: Int, mediaPeriodId: MediaSource.MediaPeriodId?, loadEventInfo: LoadEventInfo, mediaLoadData: MediaLoadData, error: IOException, wasCanceled: Boolean) {
+		logger.error("An error occurred during playback buffering", error)
 	}
 
-	@Override
-	public void onLoadError(int windowIndex, @Nullable MediaSource.MediaPeriodId mediaPeriodId, LoadEventInfo loadEventInfo, MediaLoadData mediaLoadData, IOException error, boolean wasCanceled) {
-		logger.error("An error occurred during playback buffering", error);
-		reject(error);
-		mediaSource.removeEventListener(this);
-	}
-
-	@Override
-	public void onUpstreamDiscarded(int windowIndex, MediaSource.MediaPeriodId mediaPeriodId, MediaLoadData mediaLoadData) {
-
-	}
-
-	@Override
-	public void onDownstreamFormatChanged(int windowIndex, @Nullable MediaSource.MediaPeriodId mediaPeriodId, MediaLoadData mediaLoadData) {
-
+	override fun onPlayerError(error: ExoPlaybackException) {
+		logger.error("A player error occurred during playback buffering", error)
+		reject(error)
+		mediaSource.removeEventListener(this)
+		exoPlayer.removeListener(this)
 	}
 }
