9c561f3197d5483e2dd0f899bce2cfd98ca6b5cf
David Vedvick <dvedvick@gmail.com>
2021-05-20 21:49:20 -0500

    Remove paused mode

--- projectBlueWater/src/test/java/com/lasthopesoftware/AndroidContext.java
+++ projectBlueWater/src/test/java/com/lasthopesoftware/AndroidContext.java
@@ -3,7 +3,6 @@ package com.lasthopesoftware;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidContextRunner.class)
-//@LooperMode(LooperMode.Mode.PAUSED)
 public abstract class AndroidContext {
 
 	public abstract void before() throws Exception;
