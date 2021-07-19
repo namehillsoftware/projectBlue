4f2dc7d0d09aa02c98a8800e229f3643d69cdca9
David Vedvick <dvedvick@gmail.com>
2021-07-18 22:33:17 -0500

    Make interfaces more explicit

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/connection/session/ManageConnectionSessions.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/connection/session/ManageConnectionSessions.kt
@@ -3,11 +3,12 @@ package com.lasthopesoftware.bluewater.client.connection.session
 import com.lasthopesoftware.bluewater.client.browsing.library.repository.LibraryId
 import com.lasthopesoftware.bluewater.client.connection.BuildingConnectionStatus
 import com.lasthopesoftware.bluewater.client.connection.IConnectionProvider
-import com.lasthopesoftware.bluewater.client.connection.libraries.ProvideLibraryConnections
 import com.lasthopesoftware.bluewater.shared.promises.extensions.ProgressingPromise
 
-interface ManageConnectionSessions : ProvideLibraryConnections {
+interface ManageConnectionSessions {
 	fun isConnectionActive(libraryId: LibraryId): Boolean
+
+	fun promiseLibraryConnection(libraryId: LibraryId): ProgressingPromise<BuildingConnectionStatus, IConnectionProvider?>
 
 	fun promiseTestedLibraryConnection(libraryId: LibraryId): ProgressingPromise<BuildingConnectionStatus, IConnectionProvider?>
 }
