a0f01f0b5d8307db7234e3965dc7fea6c2269b03
David Vedvick <dvedvick@gmail.com>
2021-08-09 11:11:14 -0500

    Use readonly check in file update promise

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/browsing/items/media/files/properties/storage/ScopedFilePropertiesStorage.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/browsing/items/media/files/properties/storage/ScopedFilePropertiesStorage.kt
@@ -4,6 +4,7 @@ import com.lasthopesoftware.bluewater.client.browsing.items.media.files.ServiceF
 import com.lasthopesoftware.bluewater.client.browsing.items.media.files.properties.repository.IFilePropertiesContainerRepository
 import com.lasthopesoftware.bluewater.client.browsing.library.revisions.CheckScopedRevisions
 import com.lasthopesoftware.bluewater.client.connection.IConnectionProvider
+import com.lasthopesoftware.bluewater.client.connection.authentication.CheckIfScopedConnectionIsReadOnly
 import com.lasthopesoftware.bluewater.shared.UrlKeyHolder
 import com.lasthopesoftware.bluewater.shared.promises.extensions.unitResponse
 import com.namehillsoftware.handoff.promises.Promise
@@ -11,50 +12,52 @@ import org.slf4j.LoggerFactory
 
 class ScopedFilePropertiesStorage(
 	private val scopedConnectionProvider: IConnectionProvider,
+	private val checkIfScopedConnectionIsReadOnly: CheckIfScopedConnectionIsReadOnly,
 	private val checkScopedRevisions: CheckScopedRevisions,
 	private val filePropertiesContainerRepository: IFilePropertiesContainerRepository
 ) : UpdateFileProperties {
 
-	override fun promiseFileUpdate(serviceFile: ServiceFile, property: String, value: String, isFormatted: Boolean): Promise<Unit> {
-		val urlProvider = scopedConnectionProvider.urlProvider
-		urlProvider.authCode ?: return Promise(SecurityException("Authentication is required to save properties"))
+	override fun promiseFileUpdate(serviceFile: ServiceFile, property: String, value: String, isFormatted: Boolean): Promise<Unit> =
+		checkIfScopedConnectionIsReadOnly.promiseIsReadOnly().eventually { isReadOnly ->
 
-		val promisedUpdate = scopedConnectionProvider.promiseResponse(
-			"File/SetInfo",
-			"File=${serviceFile.key}",
-			"Field=$property",
-			"Value=$value",
-			"formatted=" + if (isFormatted) "1" else "0"
-		).let { promisedServerResponse ->
-			if (!logger.isInfoEnabled) {
-				promisedServerResponse.unitResponse()
-			} else {
-				promisedServerResponse.then { response ->
-					response.use {
-						logger.info("api/v1/File/SetInfo responded with a response code of ${it.code}")
+			if (isReadOnly) return@eventually Promise(SecurityException("Authentication is required to save properties"))
+
+			val promisedUpdate = scopedConnectionProvider.promiseResponse(
+				"File/SetInfo",
+				"File=${serviceFile.key}",
+				"Field=$property",
+				"Value=$value",
+				"formatted=" + if (isFormatted) "1" else "0"
+			).let { promisedServerResponse ->
+				if (!logger.isInfoEnabled) {
+					promisedServerResponse.unitResponse()
+				} else {
+					promisedServerResponse.then { response ->
+						response.use {
+							logger.info("api/v1/File/SetInfo responded with a response code of ${it.code}")
+						}
 					}
 				}
 			}
-		}
 
-		urlProvider.baseUrl?.also { baseUrl ->
-			promisedUpdate.eventually { checkScopedRevisions.promiseRevision() }
-				.then { revision ->
-					val urlKeyHolder = UrlKeyHolder(baseUrl, serviceFile)
-					filePropertiesContainerRepository.getFilePropertiesContainer(urlKeyHolder)
-						?.takeIf { it.revision == revision }
-						?.updateProperty(property, value)
-				}
-				.excuse { e ->
-					logger.warn(
-						"${serviceFile.key}'s property cache item $property was not updated with the new value of $value",
-						e
-					)
-				}
-		}
+			scopedConnectionProvider.urlProvider.baseUrl?.also { baseUrl ->
+				promisedUpdate.eventually { checkScopedRevisions.promiseRevision() }
+					.then { revision ->
+						val urlKeyHolder = UrlKeyHolder(baseUrl, serviceFile)
+						filePropertiesContainerRepository.getFilePropertiesContainer(urlKeyHolder)
+							?.takeIf { it.revision == revision }
+							?.updateProperty(property, value)
+					}
+					.excuse { e ->
+						logger.warn(
+							"${serviceFile.key}'s property cache item $property was not updated with the new value of $value",
+							e
+						)
+					}
+			}
 
-		return promisedUpdate
-	}
+			promisedUpdate
+		}
 
 	companion object {
 		private val logger = LoggerFactory.getLogger(ScopedFilePropertiesStorage::class.java)
