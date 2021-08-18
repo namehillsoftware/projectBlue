fe9827c27ac79d8c998cd3801e7f67d357beb7ba
David Vedvick <dvedvick@gmail.com>
2021-08-17 21:36:12 -0500

    Clean-up path discovery code

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/stored/library/sync/SyncDirectoryLookup.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/stored/library/sync/SyncDirectoryLookup.kt
@@ -3,6 +3,7 @@ package com.lasthopesoftware.bluewater.client.stored.library.sync
 import com.lasthopesoftware.bluewater.client.browsing.library.access.ILibraryProvider
 import com.lasthopesoftware.bluewater.client.browsing.library.repository.Library.SyncedFileLocation
 import com.lasthopesoftware.bluewater.client.browsing.library.repository.LibraryId
+import com.lasthopesoftware.bluewater.shared.promises.extensions.toPromise
 import com.lasthopesoftware.storage.GetFreeSpace
 import com.lasthopesoftware.storage.directories.GetPrivateDirectories
 import com.lasthopesoftware.storage.directories.GetPublicDirectories
@@ -15,9 +16,13 @@ class SyncDirectoryLookup(
 	private val privateDrives: GetPrivateDirectories,
 	private val freeSpace: GetFreeSpace) : LookupSyncDirectory {
 
-	override fun promiseSyncDirectory(libraryId: LibraryId): Promise<File> =
+	companion object {
+		private val lazyEmptyFiles = lazy { Promise<Collection<File>>(emptyList()) }
+	}
+
+	override fun promiseSyncDirectory(libraryId: LibraryId): Promise<File?> =
 		getExternalFilesDirectoriesStream(libraryId)
-			.then { files -> files.maxByOrNull(freeSpace::getFreeSpace)!! }
+			.then { files -> files.maxByOrNull(freeSpace::getFreeSpace) }
 
 	private fun getExternalFilesDirectoriesStream(libraryId: LibraryId): Promise<Collection<File>> =
 		libraryProvider.getLibrary(libraryId)
@@ -25,12 +30,12 @@ class SyncDirectoryLookup(
 				when (library?.syncedFileLocation) {
 					SyncedFileLocation.EXTERNAL -> publicDrives.promisePublicDrives().promiseDirectoriesWithLibrary(libraryId)
 					SyncedFileLocation.INTERNAL -> privateDrives.promisePrivateDrives().promiseDirectoriesWithLibrary(libraryId)
-					SyncedFileLocation.CUSTOM -> {
-						val customSyncedFilePath = library.customSyncedFilesPath
-						if (customSyncedFilePath != null) Promise(listOf(File(customSyncedFilePath)))
-						else Promise(emptyList())
-					}
-					else -> Promise(emptyList())
+					SyncedFileLocation.CUSTOM ->
+						library.customSyncedFilesPath
+							?.let { p -> listOf(File(p)) }
+							?.toPromise()
+							?: lazyEmptyFiles.value
+					else -> lazyEmptyFiles.value
 				}
 			}
 
--- projectBlueWater/src/main/java/com/lasthopesoftware/storage/RecursiveFileAssertionTester.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/storage/RecursiveFileAssertionTester.kt
@@ -3,10 +3,10 @@ package com.lasthopesoftware.storage
 import java.io.File
 
 fun File.walkUpUntilFileExists(): File? {
-	var testFile = this
-	do {
-		if (testFile.exists()) return testFile
-	} while (testFile.parentFile?.also { testFile = it } != null)
+	var testFile: File? = this
+	while (testFile != null && !testFile.exists()) {
+		testFile = testFile.parentFile
+	}
 
-	return null
+	return testFile
 }
