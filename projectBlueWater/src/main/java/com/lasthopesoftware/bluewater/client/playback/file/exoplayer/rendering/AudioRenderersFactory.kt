9678b871b983d02f04e120f7ac06396d2c56554c
David Vedvick <dvedvick@gmail.com>
2021-08-17 20:50:19 -0500

    Revert unnecessary changes

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/file/exoplayer/rendering/AudioRenderersFactory.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/file/exoplayer/rendering/AudioRenderersFactory.kt
@@ -3,22 +3,17 @@ package com.lasthopesoftware.bluewater.client.playback.file.exoplayer.rendering
 import android.content.Context
 import android.os.Handler
 import com.google.android.exoplayer2.Renderer
-import com.google.android.exoplayer2.RenderersFactory
 import com.google.android.exoplayer2.audio.AudioCapabilities
-import com.google.android.exoplayer2.audio.AudioRendererEventListener
 import com.google.android.exoplayer2.audio.DefaultAudioSink
 import com.google.android.exoplayer2.audio.MediaCodecAudioRenderer
 import com.google.android.exoplayer2.mediacodec.MediaCodecSelector
-import com.google.android.exoplayer2.metadata.MetadataOutput
-import com.google.android.exoplayer2.text.TextOutput
-import com.google.android.exoplayer2.video.VideoRendererEventListener
 import com.lasthopesoftware.compilation.DebugFlag
 import com.namehillsoftware.handoff.promises.Promise
 
-class AudioRenderersFactory(private val context: Context, private val handler: Handler) : GetAudioRenderers, RenderersFactory {
+class AudioRenderersFactory(private val context: Context, private val handler: Handler) : GetAudioRenderers {
 
-	override fun newRenderers(): Promise<Array<Renderer>> {
-		return try {
+	override fun newRenderers(): Promise<Array<Renderer>> =
+		try {
 			Promise(arrayOf(
 				MediaCodecAudioRenderer(
 					context,
@@ -30,20 +25,4 @@ class AudioRenderersFactory(private val context: Context, private val handler: H
 		} catch (err: Throwable) {
 			Promise(err)
 		}
-	}
-
-	override fun createRenderers(
-		eventHandler: Handler,
-		videoRendererEventListener: VideoRendererEventListener,
-		audioRendererEventListener: AudioRendererEventListener,
-		textRendererOutput: TextOutput,
-		metadataRendererOutput: MetadataOutput): Array<Renderer> =
-		arrayOf(
-			MediaCodecAudioRenderer(
-				context,
-				MediaCodecSelector.DEFAULT,
-				false,
-				if (DebugFlag.isDebugCompilation) handler else null,
-				if (DebugFlag.isDebugCompilation) AudioRenderingEventListener() else null,
-				DefaultAudioSink(AudioCapabilities.getCapabilities(context), emptyArray())))
 }
