1dac79930c8efe82e72be28e1a3e5d1c02c7423c
David Vedvick <dvedvick@gmail.com>
2021-09-04 18:23:44 -0500

    Fix more tests

--- projectBlueWater/src/test/java/com/lasthopesoftware/bluewater/client/browsing/items/media/files/uri/GivenAFileThatIsAvailableRemotely/AndAvailableOnDisk/AndExistingFileUsageIsNotAllowed/WhenGettingTheUri.kt
+++ projectBlueWater/src/test/java/com/lasthopesoftware/bluewater/client/browsing/items/media/files/uri/GivenAFileThatIsAvailableRemotely/AndAvailableOnDisk/AndExistingFileUsageIsNotAllowed/WhenGettingTheUri.kt
@@ -1,84 +1,60 @@
-package com.lasthopesoftware.bluewater.client.browsing.items.media.files.uri.GivenAFileThatIsAvailableRemotely.AndAvailableOnDisk.AndExistingFileUsageIsNotAllowed;
+package com.lasthopesoftware.bluewater.client.browsing.items.media.files.uri.GivenAFileThatIsAvailableRemotely.AndAvailableOnDisk.AndExistingFileUsageIsNotAllowed
 
-import android.net.Uri;
+import android.net.Uri
+import com.lasthopesoftware.bluewater.client.browsing.items.media.audio.uri.CachedAudioFileUriProvider
+import com.lasthopesoftware.bluewater.client.browsing.items.media.files.ServiceFile
+import com.lasthopesoftware.bluewater.client.browsing.items.media.files.uri.BestMatchUriProvider
+import com.lasthopesoftware.bluewater.client.browsing.items.media.files.uri.RemoteFileUriProvider
+import com.lasthopesoftware.bluewater.client.browsing.library.repository.Library
+import com.lasthopesoftware.bluewater.client.stored.library.items.files.system.uri.MediaFileUriProvider
+import com.lasthopesoftware.bluewater.client.stored.library.items.files.uri.StoredFileUriProvider
+import com.lasthopesoftware.bluewater.shared.promises.extensions.toFuture
+import com.namehillsoftware.handoff.promises.Promise
+import io.mockk.every
+import io.mockk.mockk
+import org.assertj.core.api.AssertionsForClassTypes.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers
+import org.robolectric.RobolectricTestRunner
+import java.io.File
 
-import com.lasthopesoftware.bluewater.client.browsing.items.media.audio.uri.CachedAudioFileUriProvider;
-import com.lasthopesoftware.bluewater.client.browsing.items.media.files.ServiceFile;
-import com.lasthopesoftware.bluewater.client.browsing.items.media.files.uri.BestMatchUriProvider;
-import com.lasthopesoftware.bluewater.client.browsing.items.media.files.uri.RemoteFileUriProvider;
-import com.lasthopesoftware.bluewater.client.browsing.library.repository.Library;
-import com.lasthopesoftware.bluewater.client.stored.library.items.files.system.uri.MediaFileUriProvider;
-import com.lasthopesoftware.bluewater.client.stored.library.items.files.uri.StoredFileUriProvider;
-import com.namehillsoftware.handoff.promises.Promise;
+@RunWith(RobolectricTestRunner::class)
+class WhenGettingTheUri {
 
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
+	companion object {
 
-import java.io.File;
-import java.util.concurrent.CountDownLatch;
+		private val returnedFileUri by lazy {
+			val mockStoredFileUriProvider = mockk<StoredFileUriProvider>()
+			every { mockStoredFileUriProvider.promiseFileUri(ArgumentMatchers.any()) } returns Promise.empty()
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+			val cachedAudioFileUriProvider = mockk<CachedAudioFileUriProvider>()
+			every { cachedAudioFileUriProvider.promiseFileUri(ServiceFile(3)) } returns Promise.empty()
 
-@RunWith(RobolectricTestRunner.class)
-public class WhenGettingTheUri {
+			val mockMediaFileUriProvider = mockk<MediaFileUriProvider>()
+			every { mockMediaFileUriProvider.promiseFileUri(ArgumentMatchers.any()) } returns Promise(Uri.fromFile(File("/a_media_path/to_a_file.mp3")))
 
-	private static Throwable rejection;
-	private static Uri returnedFileUri;
+			val mockRemoteFileUriProvider = mockk<RemoteFileUriProvider>()
+			every { mockRemoteFileUriProvider.promiseFileUri(ServiceFile(3)) } returns Promise(Uri.parse("http://remote-url/to_a_file.mp3"))
 
-	@BeforeClass
-	public static void context() throws Throwable {
-
-		final StoredFileUriProvider mockStoredFileUriProvider = mock(StoredFileUriProvider.class);
-		when(mockStoredFileUriProvider.promiseFileUri(any()))
-			.thenReturn(Promise.empty());
-
-		final CachedAudioFileUriProvider cachedAudioFileUriProvider = mock(CachedAudioFileUriProvider.class);
-		when(cachedAudioFileUriProvider.promiseFileUri(new ServiceFile(3)))
-			.thenReturn(Promise.empty());
-
-		final MediaFileUriProvider mockMediaFileUriProvider = mock(MediaFileUriProvider.class);
-		when(mockMediaFileUriProvider.promiseFileUri(any()))
-			.thenReturn(new Promise<>(Uri.fromFile(new File("/a_media_path/to_a_file.mp3"))));
-
-		final RemoteFileUriProvider mockRemoteFileUriProvider = mock(RemoteFileUriProvider.class);
-		when(mockRemoteFileUriProvider.promiseFileUri(new ServiceFile(3)))
-			.thenReturn(new Promise<>(Uri.parse("http://remote-url/to_a_file.mp3")));
-
-		final BestMatchUriProvider bestMatchUriProvider =
-			new BestMatchUriProvider(
-				new Library(),
+			val bestMatchUriProvider = BestMatchUriProvider(
+				Library(),
 				mockStoredFileUriProvider,
 				cachedAudioFileUriProvider,
 				mockMediaFileUriProvider,
-				mockRemoteFileUriProvider);
+				mockRemoteFileUriProvider
+			)
 
-		final CountDownLatch countDownLatch = new CountDownLatch(1);
-		bestMatchUriProvider
-			.promiseFileUri(new ServiceFile(3))
-			.then(f -> {
-				returnedFileUri = f;
-				countDownLatch.countDown();
-				return null;
-			})
-			.excuse(e -> {
-				rejection = e;
-				countDownLatch.countDown();
-				return null;
-			});
-
-		countDownLatch.await();
-
-		if (rejection != null)
-			throw rejection;
+			bestMatchUriProvider
+				.promiseFileUri(ServiceFile(3))
+				.toFuture()
+				.get()
+		}
 	}
 
 	@Test
-	public void thenTheRemoteFileUriIsReturned() {
-		assertThat(returnedFileUri.toString()).isEqualTo("http://remote-url/to_a_file.mp3");
+	fun thenTheRemoteFileUriIsReturned() {
+		assertThat(returnedFileUri.toString())
+			.isEqualTo("http://remote-url/to_a_file.mp3")
 	}
 }
