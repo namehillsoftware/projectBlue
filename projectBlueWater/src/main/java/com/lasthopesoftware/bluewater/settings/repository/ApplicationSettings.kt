0cc8b2d52211326e75adb7441a9b2915e6d2cc8d
David Vedvick <dvedvick@gmail.com>
2021-09-06 11:51:26 -0500

    Add keep annotations to new database objects

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/settings/repository/ApplicationSettings.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/settings/repository/ApplicationSettings.kt
@@ -1,5 +1,8 @@
 package com.lasthopesoftware.bluewater.settings.repository
 
+import androidx.annotation.Keep
+
+@Keep
 data class ApplicationSettings(
 	var isSyncOnWifiOnly: Boolean = false,
 	var isSyncOnPowerOnly: Boolean = false,
