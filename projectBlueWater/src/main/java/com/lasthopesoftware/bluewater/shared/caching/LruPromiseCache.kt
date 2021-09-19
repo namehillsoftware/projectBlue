e1368301283f757e82dec6a5797335e56961bde3
David Vedvick <dvedvick@gmail.com>
2021-09-19 10:11:18 -0500

    Add quick access cache

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/caching/LruPromiseCache.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/caching/LruPromiseCache.kt
@@ -3,18 +3,30 @@ package com.lasthopesoftware.bluewater.shared.caching
 import androidx.collection.LruCache
 import com.lasthopesoftware.bluewater.shared.promises.extensions.toPromise
 import com.namehillsoftware.handoff.promises.Promise
+import java.util.concurrent.ConcurrentHashMap
 
 class LruPromiseCache<Input : Any, Output>(maxValues: Int) : CachePromiseFunctions<Input, Output> {
-	private val cachedPromises = LruCache<Input, Promise<Output>>(maxValues)
+	private val quickAccessCache = ConcurrentHashMap<Input, Promise<Output>>()
+	private val cachedPromises = object : LruCache<Input, Promise<Output>>(maxValues) {
+		override fun entryRemoved(evicted: Boolean, key: Input, oldValue: Promise<Output>, newValue: Promise<Output>?) {
+			if (evicted || newValue == null) quickAccessCache.remove(key)
+		}
+	}
 
 	override fun getOrAdd(input: Input, factory: (Input) -> Promise<Output>): Promise<Output> {
 		fun produceAndStoreValue(): Promise<Output> =
 			synchronized(cachedPromises) {
-				cachedPromises[input] ?: factory(input).also { cachedPromises.put(input, it) }
+				cachedPromises[input]
+					?: factory(input).also { p ->
+						cachedPromises.put(input, p)
+						p.then { quickAccessCache[input] = p }
+					}
 			}
 
-		return cachedPromises[input]
-			?.eventually({ o -> o.toPromise() }, { produceAndStoreValue() })
+		return quickAccessCache[input]
+			?: cachedPromises[input]?.eventually({ o -> o.toPromise() }, { produceAndStoreValue() })
 			?: produceAndStoreValue()
 	}
+
+	private data class Box<Output>(val output: Output)
 }
