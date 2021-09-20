c46a33017c17d3397d001797aed7b9e3798ce676
David Vedvick <dvedvick@gmail.com>
2021-09-19 22:07:16 -0500

    Simplify PermanentPromiseFunctionCache

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/caching/PermanentPromiseFunctionCache.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/caching/PermanentPromiseFunctionCache.kt
@@ -2,24 +2,38 @@ package com.lasthopesoftware.bluewater.shared.caching
 
 import com.lasthopesoftware.bluewater.shared.promises.extensions.toPromise
 import com.namehillsoftware.handoff.promises.Promise
+import com.namehillsoftware.handoff.promises.response.ImmediateResponse
 import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicBoolean
 
 class PermanentPromiseFunctionCache<Input : Any, Output> : CachePromiseFunctions<Input, Output> {
 	private val cachedSyncs = ConcurrentHashMap<Input, Any>()
-	private val cachedPromises = HashMap<Input, Promise<Output>>()
-	private val cachedValues = HashMap<Input, Output>()
+	private val cachedPromises = ConcurrentHashMap<Input, PromiseBox<Output>>()
 
 	override fun getOrAdd(input: Input, factory: (Input) -> Promise<Output>): Promise<Output> {
-		fun Promise<Output>?.promiseValue(): Promise<Output> {
-			fun produceAndStoreValue() = factory(input).then { o -> o.also { cachedValues[input] = o } }
+		fun produceAndStoreValue(): Promise<Output> =
+			synchronized(cachedSyncs.getOrPut(input, ::Any)) {
+				factory(input).also { p ->
+					cachedPromises[input] = PromiseBox(p)
+				}
+			}
 
-			return this?.eventually({ o -> o.toPromise() }, { produceAndStoreValue() })
-				?: produceAndStoreValue()
-		}
+		return cachedPromises[input]?.resolvedPromise
+			?: synchronized(cachedSyncs.getOrPut(input, ::Any)) {
+				cachedPromises[input]?.resolvedPromise
+					?: cachedPromises[input]?.promise?.eventually({ o -> o.toPromise() }, { produceAndStoreValue() })
+					?: produceAndStoreValue()
+			}
+	}
 
-		return cachedValues[input]?.toPromise() ?: synchronized(cachedSyncs.getOrPut(input, ::Any)) {
-			cachedValues[input]?.toPromise()
-				?: cachedPromises[input].promiseValue().also { cachedPromises[input] = it }
-		}
+	private class PromiseBox<Resolution>(val promise: Promise<Resolution>) : ImmediateResponse<Resolution, Unit> {
+		private val isResolved = AtomicBoolean()
+
+		init { promise.then(this) }
+
+		override fun respond(resolution: Resolution) { isResolved.set(true) }
+
+		val resolvedPromise: Promise<Resolution>?
+			get() = promise.takeIf { isResolved.get() }
 	}
 }
