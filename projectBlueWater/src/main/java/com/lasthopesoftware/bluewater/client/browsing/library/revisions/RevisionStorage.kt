d95c70c9a197214f67fcf1fddfe42471e784850a
David Vedvick <dvedvick@gmail.com>
2021-10-13 08:52:11 -0500

    Add `TimedExpirationCache`, use in `RevisionStorage`

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/browsing/library/revisions/RevisionStorage.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/browsing/library/revisions/RevisionStorage.kt
@@ -1,51 +1,39 @@
 package com.lasthopesoftware.bluewater.client.browsing.library.revisions
 
 import com.lasthopesoftware.bluewater.client.connection.IConnectionProvider
-import com.lasthopesoftware.bluewater.client.connection.url.IUrlProvider
 import com.lasthopesoftware.bluewater.shared.StandardRequest
-import com.lasthopesoftware.bluewater.shared.promises.extensions.toPromise
+import com.lasthopesoftware.bluewater.shared.policies.caching.TimedExpirationPromiseCache
 import com.namehillsoftware.handoff.promises.Promise
 import org.joda.time.Duration
-import java.util.*
 
 internal object RevisionStorage {
 	private const val badRevision = -1
-	private val checkedExpirationTime = Duration.standardSeconds(30).millis
-	private val cachedRevisions: MutableMap<String, Int> = HashMap()
-	private val lastRevisions: MutableMap<String, Long> = HashMap()
-
-	private fun getCachedRevision(urlProvider: IUrlProvider): Int {
-		val serverUrl = urlProvider.baseUrl?.toString() ?: return badRevision
-		if (!cachedRevisions.containsKey(serverUrl)) cachedRevisions[serverUrl] =
-			badRevision
-		return cachedRevisions[serverUrl] ?: badRevision
-	}
+	private val badRevisionPromise = Promise(badRevision)
+	private val checkedExpirationTime = Duration.standardSeconds(30)
+	private val expiringRevisionCache = TimedExpirationPromiseCache<String, Int>(checkedExpirationTime)
 
 	internal fun promiseRevision(connectionProvider: IConnectionProvider?): Promise<Int> {
-		connectionProvider ?: return badRevision.toPromise()
+		connectionProvider ?: return badRevisionPromise
 
 		val urlProvider = connectionProvider.urlProvider
 		return urlProvider.baseUrl?.toString()
 			?.let { baseServerUrl ->
-				lastRevisions[baseServerUrl]
-					?.takeIf { System.currentTimeMillis() - checkedExpirationTime < it }
-					?.let { getCachedRevision(urlProvider) }
-					?.takeIf { it != badRevision }
-					?.let { Promise(it) }
-					?: connectionProvider
+				expiringRevisionCache.getOrAdd(baseServerUrl) {
+					connectionProvider
 						.promiseResponse("Library/GetRevision")
-						.then({ response ->
+						.then { response ->
 							response.body
 								?.use { body -> body.byteStream().use(StandardRequest::fromInputStream) }
 								?.let { standardRequest -> standardRequest.items["Sync"] }
-								?.takeIf { revisionValue -> revisionValue.isNotEmpty() }
-								?.let(Integer::valueOf)
-								?.also { revisionValue ->
-									cachedRevisions[baseServerUrl] = revisionValue
-									lastRevisions[baseServerUrl] = System.currentTimeMillis()
-								}
-								?: getCachedRevision(urlProvider)
-						}, { getCachedRevision(urlProvider) })
-			} ?: badRevision.toPromise()
+								?.takeIf { revisionValue -> revisionValue.isNotEmpty() }!!
+								.let(Integer::valueOf)
+						}
+				}
+			}
+			?.then(
+				{ i -> i },
+				{ badRevision }
+			)
+			?: badRevisionPromise
 	}
 }
