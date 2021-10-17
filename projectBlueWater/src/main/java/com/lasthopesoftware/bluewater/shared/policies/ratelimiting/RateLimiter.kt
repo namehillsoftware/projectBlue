6b1ac725d830bd40711a05de64aaf2f2500ff7ef
David Vedvick <dvedvick@gmail.com>
2021-10-17 09:38:53 -0500

    Swallow rate-limited rejections before propagating

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/policies/ratelimiting/RateLimiter.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/policies/ratelimiting/RateLimiter.kt
@@ -2,17 +2,18 @@ package com.lasthopesoftware.bluewater.shared.policies.ratelimiting
 
 import com.namehillsoftware.handoff.promises.Promise
 import com.namehillsoftware.handoff.promises.propagation.PromiseProxy
-import com.namehillsoftware.handoff.promises.response.ImmediateAction
 import com.namehillsoftware.handoff.promises.response.ImmediateResponse
 import java.util.concurrent.ConcurrentLinkedQueue
 import java.util.concurrent.Executor
 import java.util.concurrent.Semaphore
 import java.util.concurrent.atomic.AtomicReference
 
-class RateLimiter<T>(private val executor: Executor, rate: Int): RateLimitPromises<T>, Runnable, ImmediateAction, ImmediateResponse<Throwable, Unit> {
+class RateLimiter<T>(private val executor: Executor, rate: Int): RateLimitPromises<T>, Runnable {
 	private val queueProcessorReference = AtomicReference<RateLimiter<T>>()
 	private val semaphore = Semaphore(rate)
 	private val queuedPromises = ConcurrentLinkedQueue<() -> Promise<T>>()
+	private val resolutionHandler = ImmediateResponse<T, Unit> { semaphore.release() }
+	private val rejectionHandler = ImmediateResponse<Throwable, Unit> { semaphore.release() }
 
 	override fun limit(factory: () -> Promise<T>): Promise<T> {
 		return Promise<T> { m ->
@@ -28,18 +29,11 @@ class RateLimiter<T>(private val executor: Executor, rate: Int): RateLimitPromis
 			var promiseFactory: (() -> Promise<T>)?
 			while (queuedPromises.poll().also { promiseFactory = it } != null) {
 				semaphore.acquire()
-				promiseFactory?.invoke()?.must(this)?.excuse(this)
+				// Use resolve/rejection handler over `must` so that errors don't propagate as unhandled
+				promiseFactory?.invoke()?.then(resolutionHandler, rejectionHandler)
 			}
 		} finally {
 			queueProcessorReference.set(null)
 		}
-	}
-
-	override fun act() {
-		semaphore.release()
-	}
-
-	override fun respond(resolution: Throwable?) {
-		// swallow the exception so it doesn't appear unhandled
 	}
 }
