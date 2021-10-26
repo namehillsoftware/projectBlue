0efa1b4fa0758a0495909b55cac5fcf4428a5575
David Vedvick <dvedvick@gmail.com>
2021-10-25 20:45:44 -0500

    Return early if key event is null

---
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/service/receivers/RemoteControlReceiver.kt
@@ -0,0 +1,25 @@
+package com.lasthopesoftware.bluewater.client.playback.service.receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.view.KeyEvent
+import com.lasthopesoftware.bluewater.client.playback.service.PlaybackService.Companion.next
+import com.lasthopesoftware.bluewater.client.playback.service.PlaybackService.Companion.pause
+import com.lasthopesoftware.bluewater.client.playback.service.PlaybackService.Companion.play
+import com.lasthopesoftware.bluewater.client.playback.service.PlaybackService.Companion.previous
+import com.lasthopesoftware.bluewater.client.playback.service.PlaybackService.Companion.togglePlayPause
+
+class RemoteControlReceiver : BroadcastReceiver() {
+	override fun onReceive(context: Context, intent: Intent) {
+		val event = intent.getParcelableExtra<KeyEvent?>(Intent.EXTRA_KEY_EVENT)
+		if (event?.action != KeyEvent.ACTION_UP) return
+		when (event.keyCode) {
+			KeyEvent.KEYCODE_MEDIA_PLAY -> play(context)
+			KeyEvent.KEYCODE_MEDIA_STOP, KeyEvent.KEYCODE_MEDIA_PAUSE -> pause(context)
+			KeyEvent.KEYCODE_HEADSETHOOK, KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE -> togglePlayPause(context)
+			KeyEvent.KEYCODE_MEDIA_NEXT -> next(context)
+			KeyEvent.KEYCODE_MEDIA_PREVIOUS -> previous(context)
+		}
+	}
+}
