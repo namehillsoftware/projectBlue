e6c70ec97ed0426143d3ad04346ccfd4a2a1f7af
David Vedvick <dvedvick@gmail.com>
2021-11-22 18:52:25 -0600

    Use atomics in rate limiter

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/policies/ratelimiting/PromisingRateLimiter.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/policies/ratelimiting/PromisingRateLimiter.kt
@@ -2,41 +2,41 @@
 package com.lasthopesoftware.bluewater.shared.policies.ratelimiting
 
 import com.lasthopesoftware.bluewater.shared.promises.extensions.toPromise
+import com.lasthopesoftware.bluewater.shared.promises.extensions.unitResponse
 import com.namehillsoftware.handoff.promises.Promise
 import com.namehillsoftware.handoff.promises.propagation.PromiseProxy
-import com.namehillsoftware.handoff.promises.response.PromisedResponse
 import java.util.concurrent.ConcurrentLinkedQueue
-import kotlin.math.max
+import java.util.concurrent.atomic.AtomicReference
 
-class PromisingRateLimiter<T>(private val rate: Int): RateLimitPromises<T> {
-	private val activePromiseSync = Any()
-	@Volatile
-	private var activePromises = 0
+class PromisingRateLimiter<T>(rate: Int): RateLimitPromises<T> {
+	private val activePromises = List(rate) { AtomicReference<() -> Promise<Unit>>() }
 	private val queuedPromises = ConcurrentLinkedQueue<() -> Promise<Unit>>()
-	private val resolutionHandler = PromisedResponse<T, Unit> { finishCurrentDoNext() }
-	private val rejectionHandler = PromisedResponse<Throwable, Unit> { finishCurrentDoNext() }
 
 	override fun limit(factory: () -> Promise<T>): Promise<T> =
 		Promise<T> { m ->
 			val promiseProxy = PromiseProxy(m)
 			// Use resolve/rejection handler over `must` so that errors don't propagate as unhandled
-			queuedPromises.offer { factory().also(promiseProxy::proxy).eventually(resolutionHandler, rejectionHandler) }
+			queuedPromises.offer { factory().also(promiseProxy::proxy).unitResponse() }
 			doNext()
 		}
 
-	private fun doNext(): Promise<Unit> = synchronized(activePromiseSync) {
-		queuedPromises
-			.takeIf { ++activePromises <= rate }
-			?.poll()
-			?.invoke()
-			?: run {
-				--activePromises
-				Unit.toPromise()
-			}
-	}
+	private fun doNext(): Promise<Unit> {
+		val p = queuedPromises.poll() ?: return Unit.toPromise()
+		val reference = activePromises.firstOrNull { it.compareAndSet(null, p) }
+		if (reference == null) {
+			queuedPromises.offer(p)
+			return Unit.toPromise()
+		}
 
-	private fun finishCurrentDoNext(): Promise<Unit> = synchronized(activePromiseSync) {
-		activePromises = max(activePromises - 1, 0)
-		doNext()
+		return p().eventually(
+			{
+				reference.compareAndSet(p, null)
+				doNext()
+			},
+			{
+				reference.compareAndSet(p, null)
+				doNext()
+			}
+		)
 	}
 }
