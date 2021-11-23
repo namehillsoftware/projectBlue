468c0c8fb99d09e91a86e666b91fa8ce8352c548
David Vedvick <dvedvick@gmail.com>
2021-11-22 20:35:43 -0600

    Remove unnecessary unit response

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/policies/ratelimiting/PromisingRateLimiter.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/policies/ratelimiting/PromisingRateLimiter.kt
@@ -2,21 +2,20 @@
 package com.lasthopesoftware.bluewater.shared.policies.ratelimiting
 
 import com.lasthopesoftware.bluewater.shared.promises.NoopResponse.Companion.ignore
-import com.lasthopesoftware.bluewater.shared.promises.extensions.unitResponse
 import com.namehillsoftware.handoff.promises.Promise
 import com.namehillsoftware.handoff.promises.propagation.PromiseProxy
 import java.util.concurrent.ConcurrentLinkedDeque
 import java.util.concurrent.atomic.AtomicReference
 
 class PromisingRateLimiter<T>(rate: Int): RateLimitPromises<T> {
-	private val activePromises = List(rate) { AtomicReference<() -> Promise<Unit>>() }
-	private val queuedPromises = ConcurrentLinkedDeque<() -> Promise<Unit>>()
+	private val activePromises = List(rate) { AtomicReference<() -> Promise<T>>() }
+	private val queuedPromises = ConcurrentLinkedDeque<() -> Promise<T>>()
 
 	override fun limit(factory: () -> Promise<T>): Promise<T> =
 		Promise<T> { m ->
 			val promiseProxy = PromiseProxy(m)
 			// Use resolve/rejection handler over `must` so that errors don't propagate as unhandled
-			queuedPromises.offer { factory().also(promiseProxy::proxy).unitResponse() }
+			queuedPromises.offer { factory().also(promiseProxy::proxy) }
 			doNext()
 		}
 
