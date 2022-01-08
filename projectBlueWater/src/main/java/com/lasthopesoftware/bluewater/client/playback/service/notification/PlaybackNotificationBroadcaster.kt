2dd6e82e80451f0c4e02528d755ba67ee98df81a
David Vedvick <dvedvick@gmail.com>
2022-01-07 22:55:27 -0600

    Use `notifyEither` method

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/service/notification/PlaybackNotificationBroadcaster.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/service/notification/PlaybackNotificationBroadcaster.kt
@@ -1,111 +1,101 @@
-package com.lasthopesoftware.bluewater.client.playback.service.notification;
+package com.lasthopesoftware.bluewater.client.playback.service.notification
 
-import android.app.Notification;
+import com.lasthopesoftware.bluewater.client.browsing.items.media.files.ServiceFile
+import com.lasthopesoftware.bluewater.client.playback.service.notification.building.BuildNowPlayingNotificationContent
+import com.lasthopesoftware.bluewater.client.playback.service.notification.building.BuildPlaybackStartingNotification
+import com.lasthopesoftware.bluewater.shared.android.notifications.control.ControlNotifications
 
-import com.lasthopesoftware.bluewater.client.browsing.items.media.files.ServiceFile;
-import com.lasthopesoftware.bluewater.client.playback.service.notification.building.BuildNowPlayingNotificationContent;
-import com.lasthopesoftware.bluewater.client.playback.service.notification.building.BuildPlaybackStartingNotification;
-import com.lasthopesoftware.bluewater.shared.android.notifications.control.ControlNotifications;
-import com.namehillsoftware.handoff.promises.response.VoidResponse;
+class PlaybackNotificationBroadcaster(
+	private val notificationsController: ControlNotifications,
+	notificationsConfiguration: NotificationsConfiguration,
+	private val nowPlayingNotificationContentBuilder: BuildNowPlayingNotificationContent,
+	private val playbackStartingNotification: BuildPlaybackStartingNotification
+) : NotifyOfPlaybackEvents {
+	private val notificationId: Int = notificationsConfiguration.notificationId
+	private val notificationSync = Any()
+	private var isPlaying = false
+	private var isNotificationStarted = false
+	private var serviceFile: ServiceFile? = null
 
+	override fun notifyPlaying() {
+		isPlaying = true
 
-public class PlaybackNotificationBroadcaster implements NotifyOfPlaybackEvents {
-
-	private final ControlNotifications notificationsController;
-	private final BuildNowPlayingNotificationContent nowPlayingNotificationContentBuilder;
-	private final int notificationId;
-	private final BuildPlaybackStartingNotification playbackStartingNotification;
-
-	private final Object notificationSync = new Object();
-	private boolean isPlaying;
-	private boolean isNotificationStarted;
-	private ServiceFile serviceFile;
-
-	public PlaybackNotificationBroadcaster(ControlNotifications notificationsController, NotificationsConfiguration notificationsConfiguration, BuildNowPlayingNotificationContent nowPlayingNotificationContentBuilder, BuildPlaybackStartingNotification playbackStartingNotification) {
-		this.notificationsController = notificationsController;
-		this.notificationId = notificationsConfiguration.getNotificationId();
-		this.nowPlayingNotificationContentBuilder = nowPlayingNotificationContentBuilder;
-		this.playbackStartingNotification = playbackStartingNotification;
-	}
-
-	@Override
-	public void notifyPlaying() {
-		isPlaying = true;
-
-		if (serviceFile != null) {
-			updateNowPlaying(serviceFile);
-			return;
+		serviceFile?.also {
+			updateNowPlaying(it)
+			return
 		}
 
 		playbackStartingNotification.promisePreparedPlaybackStartingNotification()
-			.then(new VoidResponse<>(builder -> {
-				synchronized (notificationSync) {
-					if (isNotificationStarted) return;
-
-					isNotificationStarted = true;
-					notificationsController.notifyForeground(builder.build(), notificationId);
+			.then { builder ->
+				synchronized(notificationSync) {
+					if (isNotificationStarted) return@then
+					isNotificationStarted = true
+					notificationsController.notifyForeground(builder.build(), notificationId)
 				}
-			}));
+			}
 	}
 
-	@Override
-	public void notifyPaused() {
+	override fun notifyPaused() {
 		if (serviceFile == null) {
-			notificationsController.stopForegroundNotification(notificationId);
-			return;
+			notificationsController.stopForegroundNotification(notificationId)
+			return
 		}
 
-		nowPlayingNotificationContentBuilder.promiseNowPlayingNotification(serviceFile, isPlaying = false)
-			.then(builder -> {
-				notificationsController.notifyBackground(builder.build(), notificationId);
-				return null;
-			});
+		nowPlayingNotificationContentBuilder.promiseNowPlayingNotification(
+			serviceFile,
+			false.also { isPlaying = it })
+			.then { builder ->
+				notificationsController.notifyEither(builder.build(), notificationId)
+			}
 	}
 
-	@Override
-	public void notifyStopped() {
-		synchronized (notificationSync) {
-			isPlaying = false;
-			isNotificationStarted = false;
-			notificationsController.removeNotification(notificationId);
+	override fun notifyStopped() {
+		synchronized(notificationSync) {
+			isPlaying = false
+			isNotificationStarted = false
+			notificationsController.removeNotification(notificationId)
 		}
 	}
 
-	@Override
-	public void notifyPlayingFileChanged(ServiceFile serviceFile) {
-		updateNowPlaying(serviceFile);
+	override fun notifyPlayingFileChanged(serviceFile: ServiceFile) {
+		updateNowPlaying(serviceFile)
 	}
 
-	private void updateNowPlaying(ServiceFile serviceFile) {
-		synchronized (notificationSync) {
-			this.serviceFile = serviceFile;
+	private fun updateNowPlaying(serviceFile: ServiceFile) {
+		synchronized(notificationSync) {
+			this.serviceFile = serviceFile
 
-			if (!isNotificationStarted && !isPlaying) return;
+			if (!isNotificationStarted && !isPlaying) return
 
-			final Notification loadingBuilderNotification = nowPlayingNotificationContentBuilder.getLoadingNotification(isPlaying).build();
+			val loadingBuilderNotification =
+				nowPlayingNotificationContentBuilder.getLoadingNotification(isPlaying).build()
+
 			if (isPlaying) {
-				notificationsController.notifyForeground(loadingBuilderNotification, notificationId);
-				isNotificationStarted = true;
+				notificationsController.notifyForeground(loadingBuilderNotification, notificationId)
+				isNotificationStarted = true
 			}
 
 			if (!isPlaying && isNotificationStarted) {
-				notificationsController.notifyBackground(loadingBuilderNotification, notificationId);
+				notificationsController.notifyBackground(loadingBuilderNotification, notificationId)
 			}
 
 			nowPlayingNotificationContentBuilder.promiseNowPlayingNotification(serviceFile, isPlaying)
-				.then(new VoidResponse<>(builder -> {
-					synchronized (notificationSync) {
+				.then { builder ->
+					synchronized(notificationSync) {
 						if (!isPlaying) {
-							if (!isNotificationStarted) return;
-
-							notificationsController.notifyBackground(builder.build(), notificationId);
-							return;
+							if (!isNotificationStarted) return@then
+							notificationsController.notifyBackground(
+								builder.build(),
+								notificationId
+							)
+							return@then
 						}
 
-						isNotificationStarted = true;
-						notificationsController.notifyForeground(builder.build(), notificationId);
+						isNotificationStarted = true
+						notificationsController.notifyForeground(builder.build(), notificationId)
 					}
-				}));
+				}
 		}
 	}
+
 }
