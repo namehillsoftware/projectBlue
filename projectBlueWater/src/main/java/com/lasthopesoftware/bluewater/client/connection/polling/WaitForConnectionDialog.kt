39c997bc06bbd396d23ad9345a9e04fa54405821
David Vedvick <dvedvick@gmail.com>
2022-02-08 07:34:47 -0600

    Update `WaitForConnectionDialog`

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/connection/polling/WaitForConnectionDialog.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/connection/polling/WaitForConnectionDialog.kt
@@ -1,53 +1,48 @@
-package com.lasthopesoftware.bluewater.client.connection.polling;
+package com.lasthopesoftware.bluewater.client.connection.polling
 
-import android.app.AlertDialog;
-import android.content.Context;
+import android.app.AlertDialog
+import android.content.Context
+import com.lasthopesoftware.bluewater.R
+import com.lasthopesoftware.bluewater.client.connection.polling.PollConnectionService.Companion.pollSessionConnection
+import com.lasthopesoftware.bluewater.shared.promises.extensions.LoopedInPromise
 
-import com.lasthopesoftware.bluewater.R;
-import com.lasthopesoftware.bluewater.client.connection.IConnectionProvider;
-import com.lasthopesoftware.bluewater.shared.promises.extensions.LoopedInPromise;
-import com.namehillsoftware.handoff.promises.Promise;
+object WaitForConnectionDialog {
+    fun show(context: Context) {
+        AlertHolder(context)
+    }
 
-public class WaitForConnectionDialog {
+    private class AlertHolder(context: Context) {
+        private val alertDialog: AlertDialog
+        private var isDismissed = false
 
-	public static void show(final Context context) {
-		new AlertHolder(context);
-	}
-
-	private static class AlertHolder {
-
-		private final AlertDialog alertDialog;
-		private boolean isDismissed;
-
-		AlertHolder(Context context) {
-
-			final String message = String.format(context.getString(R.string.lbl_attempting_to_reconnect), context.getString(R.string.app_name));
-			final AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.DialogTheme);
-			builder.setCancelable(false);
-			builder.setTitle(context.getText(R.string.lbl_connection_lost_title)).setMessage(message).setCancelable(true);
-
-			Promise<IConnectionProvider> pollingSessionConnection = PollConnectionService.pollSessionConnection(context);
-
-			builder.setNegativeButton(context.getText(R.string.btn_cancel), (dialog, which) -> {
-				pollingSessionConnection.cancel();
-				dialog.dismiss();
-			});
-
-			builder.setOnDismissListener(dialogInterface -> isDismissed = true);
-
-			builder.setOnCancelListener(dialogInterface -> isDismissed = true);
-
-			alertDialog = builder.create();
-
-			alertDialog.setInverseBackgroundForced(true);
-
-			alertDialog.setOnShowListener(dialog -> pollingSessionConnection
-				.inevitably(() -> new LoopedInPromise<Void>(() -> {
-					if (!isDismissed && alertDialog.isShowing()) alertDialog.dismiss();
-					return null;
-				}, context)));
-
-			alertDialog.show();
-		}
-	}
+        init {
+            val message = context.getString(R.string.lbl_attempting_to_reconnect)
+				.format(context.getString(R.string.app_name))
+            val builder = AlertDialog.Builder(context, R.style.DialogTheme)
+            builder
+				.setTitle(context.getText(R.string.lbl_connection_lost_title))
+				.setMessage(message)
+				.setCancelable(true)
+            val pollingSessionConnection = pollSessionConnection(context)
+            builder.setNegativeButton(context.getText(R.string.btn_cancel)) { dialog, _ ->
+                pollingSessionConnection.cancel()
+                dialog.dismiss()
+            }
+            builder.setOnDismissListener { isDismissed = true }
+            builder.setOnCancelListener { isDismissed = true }
+            alertDialog = builder.create()
+				.apply {
+					setInverseBackgroundForced(true)
+					setOnShowListener {
+						pollingSessionConnection
+							.must {
+								LoopedInPromise({
+									if (!isDismissed && isShowing) dismiss()
+								}, context)
+							}
+					}
+					show()
+				}
+        }
+    }
 }
