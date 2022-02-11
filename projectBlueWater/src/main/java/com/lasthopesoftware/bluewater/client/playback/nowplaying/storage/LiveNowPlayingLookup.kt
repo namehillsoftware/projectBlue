a1b404ba62b864fa9e91a0e102b1d868da8c51c1
David Vedvick <dvedvick@gmail.com>
2022-02-11 08:23:54 -0600

    Simplify live now playing lookup initialization

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/nowplaying/storage/LiveNowPlayingLookup.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/nowplaying/storage/LiveNowPlayingLookup.kt
@@ -3,25 +3,80 @@ package com.lasthopesoftware.bluewater.client.playback.nowplaying.storage
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.lasthopesoftware.bluewater.client.browsing.library.access.ILibraryProvider
+import com.lasthopesoftware.bluewater.client.browsing.library.access.ILibraryStorage
+import com.lasthopesoftware.bluewater.client.browsing.library.access.LibraryRepository
+import com.lasthopesoftware.bluewater.client.browsing.library.access.SpecificLibraryProvider
+import com.lasthopesoftware.bluewater.client.browsing.library.access.session.BrowserLibrarySelection
+import com.lasthopesoftware.bluewater.client.browsing.library.access.session.ProvideSelectedLibraryId
+import com.lasthopesoftware.bluewater.client.browsing.library.access.session.SelectedBrowserLibraryIdentifierProvider
+import com.lasthopesoftware.bluewater.client.browsing.library.repository.LibraryId
 import com.lasthopesoftware.bluewater.client.playback.service.broadcasters.PlaylistEvents
 import com.lasthopesoftware.bluewater.client.playback.service.broadcasters.TrackPositionBroadcaster
+import com.lasthopesoftware.bluewater.settings.repository.access.CachingApplicationSettingsRepository.Companion.getApplicationSettingsRepository
+import com.lasthopesoftware.bluewater.shared.promises.extensions.keepPromise
 import com.namehillsoftware.handoff.promises.Promise
 
-class LiveNowPlayingLookup(private val inner: GetNowPlayingState) : BroadcastReceiver(), GetNowPlayingState {
+class LiveNowPlayingLookup private constructor(
+	selectedLibraryIdentifierProvider: ProvideSelectedLibraryId,
+	private val libraryProvider: ILibraryProvider,
+	private val libraryStorage: ILibraryStorage
+) : BroadcastReceiver(), GetNowPlayingState {
+
+	companion object {
+		private lateinit var instance: LiveNowPlayingLookup
+
+		@Synchronized
+		fun initializeInstance(context: Context): LiveNowPlayingLookup {
+			if (::instance.isInitialized) throw IllegalStateException("Instance is already initialized")
+
+			val libraryRepository = LibraryRepository(context)
+			return LiveNowPlayingLookup(
+				SelectedBrowserLibraryIdentifierProvider(context.getApplicationSettingsRepository()),
+				libraryRepository,
+				libraryRepository
+			).also { instance = it }
+		}
+
+		fun getInstance(): LiveNowPlayingLookup {
+			if (!::instance.isInitialized) throw java.lang.IllegalStateException("Instance should be initialized in application root")
+
+			return instance
+		}
+	}
+
+	private var inner: GetNowPlayingState? = null
 	private var trackedPosition: Long? = null
+
+	init {
+		selectedLibraryIdentifierProvider.selectedLibraryId.then {
+			it?.also(::updateInner)
+		}
+	}
 
 	override fun promiseNowPlaying(): Promise<NowPlaying?> =
 		inner
-			.promiseNowPlaying()
-			.then { np -> np?.apply { filePosition = trackedPosition ?: filePosition } }
+			?.promiseNowPlaying()
+			?.then { np -> np?.apply { filePosition = trackedPosition ?: filePosition } }
+			.keepPromise()
 
 	override fun onReceive(context: Context?, intent: Intent?) {
-		trackedPosition = when (intent?.action) {
-			TrackPositionBroadcaster.trackPositionUpdate -> intent
+		when (intent?.action) {
+			BrowserLibrarySelection.libraryChosenEvent -> intent
+				.getIntExtra(BrowserLibrarySelection.chosenLibraryId, -1)
+				.takeIf { it > -1 }
+				?.also { libraryId -> updateInner(LibraryId(libraryId)) }
+			TrackPositionBroadcaster.trackPositionUpdate -> trackedPosition = intent
 				.getLongExtra(TrackPositionBroadcaster.TrackPositionChangedParameters.filePosition, -1)
 				.takeIf { it > -1 }
-			PlaylistEvents.onPlaylistTrackChange -> null
-			else -> trackedPosition
+			PlaylistEvents.onPlaylistTrackChange -> trackedPosition =null
 		}
+	}
+
+	private fun updateInner(libraryId: LibraryId) {
+		inner = NowPlayingRepository(
+			SpecificLibraryProvider(libraryId, libraryProvider),
+			libraryStorage
+		)
 	}
 }
