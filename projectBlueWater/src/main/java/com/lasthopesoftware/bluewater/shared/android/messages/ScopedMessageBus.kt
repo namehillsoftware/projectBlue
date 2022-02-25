e3de47c0fec2726a338137c34510e45f667c8e45
David Vedvick <dvedvick@gmail.com>
2022-02-24 19:54:41 -0600

    Make ScopedMessageBus closable

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/android/messages/ScopedMessageBus.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/android/messages/ScopedMessageBus.kt
@@ -6,36 +6,41 @@ import android.content.IntentFilter
 import android.os.Handler
 import java.util.concurrent.ConcurrentHashMap
 
-class ScopedMessageBus(private val contentResolver: ContentResolver, private val handler: Handler): SendMessages, RegisterForMessages {
-
+class ScopedMessageBus(private val contentResolver: ContentResolver, private val handler: Handler) :
+	SendMessages,
+	RegisterForMessages,
+	AutoCloseable
+{
 	private val subscriptions = ConcurrentHashMap<ReceiveBroadcastEvents, IntentFilter>()
 
-	override fun sendBroadcast(intent: Intent) {
-		if (Thread.currentThread() == handler.looper.thread) {
-			with (intent) {
-				val type = resolveTypeIfNeeded(contentResolver)
-				subscriptions
-					.filter { it.value.match(action, type, scheme, data, categories, "ScopedMessageBus") >= 0 }
-					.forEach { it.key.onReceive(this) }
-			}
-			return
-		}
+	private var isClosed = false
 
-		handler.post {
-			with (intent) {
-				val type = resolveTypeIfNeeded(contentResolver)
-				subscriptions
-					.filter { it.value.match(action, type, scheme, data, categories, "ScopedMessageBus") >= 0 }
-					.forEach { it.key.onReceive(this) }
-			}
-		}
+	override fun sendBroadcast(intent: Intent) {
+		if (isClosed) return
+
+		if (Thread.currentThread() == handler.looper.thread) intent.sendToSubscribers()
+		else handler.post { intent.sendToSubscribers() }
 	}
 
 	override fun registerReceiver(receiver: ReceiveBroadcastEvents, filter: IntentFilter) {
-		subscriptions.putIfAbsent(receiver, filter)
+		if (!isClosed) subscriptions.putIfAbsent(receiver, filter)
 	}
 
 	override fun unregisterReceiver(receiver: ReceiveBroadcastEvents) {
 		subscriptions.remove(receiver)
+	}
+
+	override fun close() {
+		if (isClosed) return
+
+		isClosed = true
+		subscriptions.clear()
+	}
+
+	private fun Intent.sendToSubscribers() {
+		val type = resolveTypeIfNeeded(contentResolver)
+		subscriptions
+			.filter { it.value.match(action, type, scheme, data, categories, "ScopedMessageBus") >= 0 }
+			.forEach { it.key.onReceive(this) }
 	}
 }
