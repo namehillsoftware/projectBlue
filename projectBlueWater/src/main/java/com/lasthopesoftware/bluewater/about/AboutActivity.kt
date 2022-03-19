280e644ce68a86fc14fe6a811ca4a160d72b5b42
David Vedvick <dvedvick@gmail.com>
2022-03-18 19:27:36 -0500

    Use maxWidth instead of maxSize

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/about/AboutActivity.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/about/AboutActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -64,7 +65,7 @@ fun AboutView() {
 							},
 							onClick = {}
 						)
-						.fillMaxSize()
+						.fillMaxWidth()
 				)
 
 				Text(
