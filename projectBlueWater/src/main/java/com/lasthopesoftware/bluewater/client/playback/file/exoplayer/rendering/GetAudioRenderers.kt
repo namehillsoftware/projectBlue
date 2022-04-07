a2686e14b3fd81e2a8b796e3f7f54648679e38fb
David Vedvick <dvedvick@gmail.com>
2022-04-07 11:29:02 -0500

    Remove unused interface

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/file/exoplayer/rendering/GetAudioRenderers.kt
+++
@@ -1,8 +0,0 @@
-package com.lasthopesoftware.bluewater.client.playback.file.exoplayer.rendering
-
-import com.google.android.exoplayer2.Renderer
-import com.namehillsoftware.handoff.promises.Promise
-
-fun interface GetAudioRenderers {
-	fun newRenderers(): Promise<Array<Renderer>>
-}
