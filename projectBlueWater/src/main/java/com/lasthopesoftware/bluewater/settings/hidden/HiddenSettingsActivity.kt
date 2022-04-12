2a022dfa7eb41c5923fe3225037157443a174040
David Vedvick <dvedvick@gmail.com>
2022-04-12 06:45:55 -0500

    Add view model for hidden settings

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/settings/hidden/HiddenSettingsActivity.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/settings/hidden/HiddenSettingsActivity.kt
@@ -8,28 +8,54 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.lasthopesoftware.bluewater.settings.repository.access.CachingApplicationSettingsRepository.Companion.getApplicationSettingsRepository
+import com.lasthopesoftware.bluewater.settings.repository.access.HoldApplicationSettings
 
 class HiddenSettingsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-		setContent { HiddenSettings() }
+		setContent { HiddenSettings(HiddenSettingsViewModel(getApplicationSettingsRepository())) }
     }
 }
 
-@Preview
+//@Preview
 @Composable
-fun HiddenSettings() {
+fun HiddenSettings(hiddenSettingsViewModel: HiddenSettingsViewModel) {
 	Column {
 		Row {
 			Column {
-				Checkbox(checked = false, onCheckedChange = {})
+				Checkbox(checked = hiddenSettingsViewModel.isUsingCustomCaching, onCheckedChange = { hiddenSettingsViewModel.saveSettings() })
 			}
 
 			Column {
 				Text(text = "Use custom caching implementation")
 			}
 		}
+	}
+}
+
+class HiddenSettingsViewModel(private val applicationSettingsRepository: HoldApplicationSettings) : ViewModel() {
+	var isUsingCustomCaching by mutableStateOf(false)
+
+	init {
+	    applicationSettingsRepository
+			.promiseApplicationSettings()
+			.then { settings ->
+				isUsingCustomCaching = settings.isUsingCustomCaching
+			}
+	}
+
+	fun saveSettings() {
+		applicationSettingsRepository
+			.promiseApplicationSettings()
+			.eventually { settings ->
+				settings.isUsingCustomCaching = isUsingCustomCaching
+				applicationSettingsRepository.promiseUpdatedSettings(settings)
+			}
 	}
 }
