cc1de5f094ed59853ba922ffaad145f6a621e86c
David Vedvick <dvedvick@gmail.com>
2022-05-28 12:08:24 -0500

    Fix landscape rendering of about view

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/about/AboutActivity.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/about/AboutActivity.kt
@@ -7,10 +7,7 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -24,6 +21,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.lasthopesoftware.bluewater.BuildConfig
@@ -46,22 +44,33 @@ class AboutActivity : ComponentActivity() {
 }
 
 @OptIn(ExperimentalFoundationApi::class)
+@Preview
 @Composable
 fun AboutView() {
-	Box(
+	BoxWithConstraints(modifier = Modifier
+		.fillMaxSize()
+		.padding(Dp(10f))) {
+		if (maxWidth < 500.dp) AboutViewVertical()
+		else AboutViewHorizontal()
+	}
+}
+
+@Composable
+@OptIn(ExperimentalFoundationApi::class)
+fun AboutViewVertical() {
+	Column(
 		Modifier
-			.fillMaxSize()
-			.padding(Dp(10f))) {
+			.fillMaxSize()) {
 		with (LocalContext.current) {
 			val hapticFeedback = LocalHapticFeedback.current
 			val hiddenSettingsActivityIntentBuilder = HiddenSettingsActivityIntentBuilder(IntentFactory(this))
 			Image(
 				painter = painterResource(id = R.drawable.project_blue_logo),
 				contentDescription = "Project Blue logo",
-				contentScale = ContentScale.Fit,
+				contentScale = ContentScale.Inside,
 				alignment = Alignment.TopCenter,
 				modifier = Modifier
-					.shadow(elevation = 20.dp, shape = CircleShape)
+					.shadow(elevation = 20.dp, shape = CircleShape, clip = true)
 					.combinedClickable(
 						interactionSource = remember { MutableInteractionSource() },
 						indication = null,
@@ -86,7 +95,56 @@ fun AboutView() {
 					getString(R.string.copyright_year)
 				),
 				textAlign = TextAlign.Center,
-				modifier = Modifier.align(Alignment.BottomCenter)
+				modifier = Modifier
+					.fillMaxSize()
+					.align(Alignment.CenterHorizontally)
+					.padding(top = 50.dp)
+			)
+		}
+	}
+}
+
+@Composable
+@OptIn(ExperimentalFoundationApi::class)
+fun AboutViewHorizontal() {
+	Row(
+		Modifier
+			.fillMaxSize()) {
+		with (LocalContext.current) {
+			val hapticFeedback = LocalHapticFeedback.current
+			val hiddenSettingsActivityIntentBuilder = HiddenSettingsActivityIntentBuilder(IntentFactory(this))
+			Image(
+				painter = painterResource(id = R.drawable.project_blue_logo),
+				contentDescription = "Project Blue logo",
+				contentScale = ContentScale.Inside,
+				alignment = Alignment.TopCenter,
+				modifier = Modifier
+					.shadow(elevation = 20.dp, shape = CircleShape, clip = true)
+					.combinedClickable(
+						interactionSource = remember { MutableInteractionSource() },
+						indication = null,
+						enabled = true,
+						onLongClick = {
+							hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
+							startActivity(
+								hiddenSettingsActivityIntentBuilder.buildHiddenSettingsIntent()
+							)
+						},
+						onClick = {}
+					)
+					.fillMaxHeight(),
+			)
+
+			Text(
+				getString(R.string.aboutAppText).format(
+					getString(R.string.app_name),
+					BuildConfig.VERSION_NAME,
+					BuildConfig.VERSION_CODE,
+					getString(R.string.company_name),
+					getString(R.string.copyright_year)
+				),
+				textAlign = TextAlign.Center,
+				modifier = Modifier.fillMaxSize().align(Alignment.CenterVertically)
 			)
 		}
 	}
