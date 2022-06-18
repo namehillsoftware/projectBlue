c67715837800fa0bb529d28baae74e2b0b172546
David Vedvick <dvedvick@gmail.com>
2022-06-18 18:01:16 -0500

    Return immediately if table is created

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/settings/repository/ApplicationSettingsUpdater.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/settings/repository/ApplicationSettingsUpdater.kt
@@ -7,7 +7,10 @@ import com.lasthopesoftware.bluewater.settings.repository.ApplicationSettingsEnt
 
 class ApplicationSettingsUpdater(private val applicationSettingsMigrator: ApplicationSettingsMigrator) : IEntityUpdater {
 	override fun onUpdate(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-		if (oldVersion < 9) applicationSettingsMigrator.migrateSettings(db)
+		if (oldVersion < 9) {
+			applicationSettingsMigrator.migrateSettings(db)
+			return
+		}
 
 		if (oldVersion < 10)
 			db.execSQL("ALTER TABLE `$tableName` add column `$isUsingCustomCachingColumn` SMALLINT;")
