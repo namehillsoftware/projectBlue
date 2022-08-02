2954f09d61543f752eedc9cc5502273d0ce87112
David Vedvick <dvedvick@gmail.com>
2022-08-01 20:46:37 -0500

    Add privacy policy

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/android/services/ServiceExtensions.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/android/services/ServiceExtensions.kt
@@ -6,19 +6,26 @@ import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
 import android.os.IBinder
+import com.lasthopesoftware.bluewater.shared.cls
 import com.namehillsoftware.handoff.promises.Promise
 
 inline fun <reified TService : Service> Context.promiseBoundService(): Promise<ConnectedServiceBinding<TService>> {
-	val context = this
+	val c = cls<TService>()
+	val intent = Intent(this, c)
 	return object : Promise<ConnectedServiceBinding<TService>>() {
 		init {
 			try {
-				val c = TService::class.java
-				context.bindService(Intent(context, c), object : ServiceConnection {
+				bindService(intent, object : ServiceConnection {
 					override fun onServiceConnected(name: ComponentName?, service: IBinder) {
 						val boundService = (service as? GenericBinder<*>)?.service as? TService
-						if (boundService == null) reject(InvalidBindingException(c))
-						else resolve(ConnectedServiceBinding(boundService, this))
+						if (boundService != null) {
+							resolve(ConnectedServiceBinding(boundService, this))
+							return
+						}
+
+						unbindService(this)
+						reject(InvalidBindingException(c))
+						return
 					}
 
 					override fun onServiceDisconnected(name: ComponentName?) {}
