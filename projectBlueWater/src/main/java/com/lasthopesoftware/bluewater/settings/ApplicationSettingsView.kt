272ab8817f2ac1f7a9fe02b46dbb9c142e5fb69d
david <dvedvick@gmail.com>
2023-03-19 22:54:02 -0500

    Add Initial ViewModel and View for Application Settings
    Initial binding of ApplicationSettingsViewModel.kt

---
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/settings/ApplicationSettingsView.kt
@@ -0,0 +1,13 @@
+package com.lasthopesoftware.bluewater.settings
+
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ActivitySettingsView(
+	applicationSettingsViewModel: ApplicationSettingsViewModel,
+) {
+	Surface {
+
+	}
+}
---
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/settings/ApplicationSettingsViewModel.kt
@@ -0,0 +1,52 @@
+package com.lasthopesoftware.bluewater.settings
+
+import androidx.lifecycle.ViewModel
+import com.lasthopesoftware.bluewater.client.browsing.TrackLoadedViewState
+import com.lasthopesoftware.bluewater.client.browsing.library.repository.Library
+import com.lasthopesoftware.bluewater.client.browsing.library.repository.LibraryId
+import com.lasthopesoftware.bluewater.client.playback.engine.selection.LookupSelectedPlaybackEngineType
+import com.lasthopesoftware.bluewater.client.playback.engine.selection.PlaybackEngineType
+import com.lasthopesoftware.bluewater.settings.repository.access.HoldApplicationSettings
+import com.namehillsoftware.handoff.promises.Promise
+import com.namehillsoftware.handoff.promises.response.ImmediateAction
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class ApplicationSettingsViewModel(
+	private val applicationSettingsRepository: HoldApplicationSettings,
+	private val selectedPlaybackEngineTypeAccess: LookupSelectedPlaybackEngineType
+) : ViewModel(), TrackLoadedViewState, ImmediateAction
+{
+	private val mutableLibraries = MutableStateFlow(emptyList<Library>())
+	private val mutableIsLoading = MutableStateFlow(false)
+
+	val isSyncOnWifiOnly = MutableStateFlow(false)
+	val isSyncOnPowerOnly = MutableStateFlow(false)
+	val isVolumeLevelingEnabled = MutableStateFlow(false)
+	val playbackEngineType = MutableStateFlow(PlaybackEngineType.ExoPlayer)
+	val chosenLibraryId = MutableStateFlow(LibraryId(-1))
+	val libraries = mutableLibraries.asStateFlow()
+	override val isLoading = mutableIsLoading.asStateFlow()
+
+	fun loadSettings(): Promise<*> {
+		mutableIsLoading.value = true
+
+		val promisedSimpleValuesUpdate = applicationSettingsRepository.promiseApplicationSettings()
+			.then {
+				isSyncOnWifiOnly.value = it.isSyncOnWifiOnly
+				isSyncOnPowerOnly.value = it.isSyncOnPowerOnly
+				isVolumeLevelingEnabled.value = it.isVolumeLevelingEnabled
+				chosenLibraryId.value = LibraryId(it.chosenLibraryId)
+			}
+
+		val promisedEngineTypeUpdate = selectedPlaybackEngineTypeAccess
+			.promiseSelectedPlaybackEngineType()
+			.then { playbackEngineType.value = it }
+
+		return Promise.whenAll(promisedSimpleValuesUpdate, promisedEngineTypeUpdate).must(this)
+	}
+
+	override fun act() {
+		mutableIsLoading.value = false
+	}
+}
