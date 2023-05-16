408a9cbbc06043e3d8521841b7b1976f3fa3f575
David Vedvick <dvedvick@gmail.com>
2023-05-15 20:03:59 -0500

    Keep promise when the connection is null

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/browsing/files/access/stringlist/LibraryFileStringListProvider.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/browsing/files/access/stringlist/LibraryFileStringListProvider.kt
@@ -3,25 +3,28 @@ package com.lasthopesoftware.bluewater.client.browsing.files.access.stringlist
 import com.lasthopesoftware.bluewater.client.browsing.files.access.parameters.FileListParameters
 import com.lasthopesoftware.bluewater.client.browsing.library.repository.LibraryId
 import com.lasthopesoftware.bluewater.client.connection.session.ManageConnectionSessions
+import com.lasthopesoftware.bluewater.shared.promises.extensions.keepPromise
 import com.namehillsoftware.handoff.promises.Promise
 import com.namehillsoftware.handoff.promises.response.ImmediateResponse
 import okhttp3.Response
 
 class LibraryFileStringListProvider(private val libraryConnections: ManageConnectionSessions) :
 	ProvideFileStringListsForParameters,
-	ImmediateResponse<Response, String>
+	ImmediateResponse<Response?, String>
 {
 
 	override fun promiseFileStringList(libraryId: LibraryId, option: FileListParameters.Options, vararg params: String): Promise<String> {
 		return libraryConnections
 			.promiseLibraryConnection(libraryId)
 			.eventually { connection ->
-				connection?.promiseResponse(
-					*FileListParameters.Helpers.processParams(
-						option,
-						*params
+				connection
+					?.promiseResponse(
+						*FileListParameters.Helpers.processParams(
+							option,
+							*params
+						)
 					)
-				)
+					.keepPromise()
 			}
 			.then(this)
 	}
