0e81f482a53f66b9f06f3279dabf4691de32e4b6
david <dvedvick@gmail.com>
2023-05-19 22:33:56 -0500

    Treat `SocketException` as Lost Connection condition

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/connection/ConnectionLostExceptionFilter.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/connection/ConnectionLostExceptionFilter.kt
@@ -3,21 +3,21 @@ package com.lasthopesoftware.bluewater.client.connection
 import com.lasthopesoftware.bluewater.client.connection.libraries.ConnectionUnavailableException
 import java.io.EOFException
 import java.io.IOException
-import java.net.ConnectException
+import java.net.SocketException
 import java.net.SocketTimeoutException
 import java.net.UnknownHostException
 import javax.net.ssl.SSLException
 
 object ConnectionLostExceptionFilter {
-    fun isConnectionLostException(error: Throwable?): Boolean =
+	fun isConnectionLostException(error: Throwable?): Boolean =
 		error is IOException && isConnectionLostException(error)
 
-    private fun isConnectionLostException(ioException: IOException): Boolean {
-        return (ioException is SocketTimeoutException
-                || ioException is EOFException
-                || ioException is ConnectException
-                || ioException is UnknownHostException
-                || ioException is SSLException
-				|| ioException is ConnectionUnavailableException)
-    }
+	private fun isConnectionLostException(ioException: IOException): Boolean {
+		return (ioException is SocketTimeoutException
+			|| ioException is SocketException
+			|| ioException is EOFException
+			|| ioException is UnknownHostException
+			|| ioException is SSLException
+			|| ioException is ConnectionUnavailableException)
+	}
 }
