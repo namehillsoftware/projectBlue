a1ed55e8a8e2cccc92297f19a5433b50fc73d0eb
David Vedvick <dvedvick@gmail.com>
2023-07-05 12:39:49 -0500

    Base scroller state off of `totalDistanceTraveled`

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/android/ui/components/ScrollConnectedScaler.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/android/ui/components/ScrollConnectedScaler.kt
@@ -18,22 +18,19 @@ private const val logTag = "ScrollConnectedScaler"
  * https://developer.android.com/reference/kotlin/androidx/compose/ui/input/nestedscroll/package-summary#extension-functions
  */
 @Composable
-fun memorableScrollConnectedScaler(max: Float, min: Float) = rememberSaveable(saver = ScrollConnectedScalerSaver(max, min)) {
+fun memorableScrollConnectedScaler(max: Float, min: Float) = rememberSaveable(saver = ScrollConnectedScaler(max, min)) {
 	ScrollConnectedScaler(max, min)
 }
 
-class ScrollConnectedScalerSaver(private val max: Float, private val min: Float) : Saver<ScrollConnectedScaler, Float> {
-	override fun restore(value: Float): ScrollConnectedScaler =
-		ScrollConnectedScaler(max, min, value)
+class ScrollConnectedScaler private constructor(private val max: Float, private val min: Float, initialDistanceTraveled: Float) : NestedScrollConnection, Saver<ScrollConnectedScaler, Float> {
 
-	override fun SaverScope.save(value: ScrollConnectedScaler): Float = value.currentProgress()
-}
-
-class ScrollConnectedScaler(private val max: Float, private val min: Float, initialProgress: Float = 0f) : NestedScrollConnection {
+	constructor(max: Float, min: Float): this(max, min, 0f)
 
 	private val fullDistance = max - min
 
-	private val initialValue = max - (fullDistance * initialProgress)
+	private var totalDistanceTraveled = initialDistanceTraveled
+
+	private val initialValue = (max + totalDistanceTraveled).coerceIn(min, max)
 
 	private val valueState = BehaviorSubject.createDefault(initialValue)
 
@@ -42,25 +39,22 @@ class ScrollConnectedScaler(private val max: Float, private val min: Float, init
 		.replay(1)
 		.refCount()
 
-	private var totalDistanceTraveled = 0f
-
 	override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
 		// try to consume before LazyColumn to collapse toolbar if needed, hence pre-scroll
 		val delta = available.y
-		totalDistanceTraveled += delta
-		val newValue = totalDistanceTraveled.coerceIn(min, max)
+		totalDistanceTraveled = totalDistanceTraveled.plus(delta).coerceAtMost(0f)
+		val newValue = (max + totalDistanceTraveled).coerceIn(min, max)
 
 		val originalValue = currentValue()
 		if (newValue != originalValue)
 			valueState.onNext(newValue)
 
+		Log.d(logTag, "totalDistanceTraveled: $totalDistanceTraveled")
 		Log.d(logTag, "newValue: $newValue")
 
 		val remainingDelta = newValue - originalValue
 		return Offset.Zero.copy(y = remainingDelta)
 	}
-
-	fun currentProgress(): Float = calculateProgress(currentValue())
 
 	@Composable
 	fun rememberProgress() = progress.subscribeAsState(initial = calculateProgress(currentValue()))
@@ -79,4 +73,9 @@ class ScrollConnectedScaler(private val max: Float, private val min: Float, init
 	private fun currentValue(): Float = valueState.value ?: initialValue
 
 	private fun calculateProgress(value: Float) = (max - value) / fullDistance
+
+	override fun restore(value: Float): ScrollConnectedScaler =
+		ScrollConnectedScaler(max, min, value)
+
+	override fun SaverScope.save(value: ScrollConnectedScaler): Float = value.totalDistanceTraveled
 }
