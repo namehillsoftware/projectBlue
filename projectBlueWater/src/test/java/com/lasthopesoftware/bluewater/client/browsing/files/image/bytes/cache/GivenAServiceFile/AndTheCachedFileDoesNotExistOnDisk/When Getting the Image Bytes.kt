d503fbf5ea0185eaa069413968b9d70ecf692d69
David Vedvick <dvedvick@gmail.com>
2023-10-29 12:22:12 -0500

    Only attempt to read cached image if it exists

---
+++ projectBlueWater/src/test/java/com/lasthopesoftware/bluewater/client/browsing/files/image/bytes/cache/GivenAServiceFile/AndTheCachedFileDoesNotExistOnDisk/When Getting the Image Bytes.kt
@@ -0,0 +1,51 @@
+package com.lasthopesoftware.bluewater.client.browsing.files.image.bytes.cache.GivenAServiceFile.AndTheCachedFileDoesNotExistOnDisk
+
+import com.lasthopesoftware.bluewater.client.browsing.files.ServiceFile
+import com.lasthopesoftware.bluewater.client.browsing.files.image.bytes.cache.DiskCacheImageAccess
+import com.lasthopesoftware.bluewater.client.browsing.library.repository.LibraryId
+import com.lasthopesoftware.bluewater.shared.promises.extensions.toExpiringFuture
+import com.namehillsoftware.handoff.promises.Promise
+import io.mockk.every
+import io.mockk.mockk
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import java.io.File
+
+private const val libraryId = 292
+private const val serviceFileId = 113
+
+class `When Getting the Image Bytes` {
+
+	private val expectedBytes = byteArrayOf(
+		(892 % 128).toByte(),
+		(283 % 128).toByte(),
+		(492 % 128).toByte(),
+	)
+
+	private val mut by lazy {
+		DiskCacheImageAccess(
+			mockk {
+				every { promiseImageBytes(LibraryId(libraryId), ServiceFile(serviceFileId)) } returns Promise(expectedBytes)
+			},
+			mockk {
+				every { promiseImageCacheKey(LibraryId(libraryId), ServiceFile(serviceFileId)) } returns Promise("pvamAttX")
+			},
+			mockk {
+				every { promiseCachedFile(LibraryId(libraryId), "pvamAttX") } returns Promise(File("y1GUF"))
+			}
+		)
+	}
+
+	private var returnedBytes: ByteArray? = null
+
+	@BeforeAll
+	fun act() {
+		returnedBytes = mut.promiseImageBytes(LibraryId(libraryId), ServiceFile(serviceFileId)).toExpiringFuture().get()
+	}
+
+	@Test
+	fun `then the result is correct`() {
+		assertThat(returnedBytes).isEqualTo(expectedBytes)
+	}
+}
