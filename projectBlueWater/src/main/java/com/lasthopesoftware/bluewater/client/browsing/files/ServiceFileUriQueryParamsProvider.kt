9615f7acba1e2e076194b8232a23b7600adbd781
David <dvedvick@gmail.com>
2024-02-05 11:26:39 -0600

    Update music query string

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/browsing/files/ServiceFileUriQueryParamsProvider.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/browsing/files/ServiceFileUriQueryParamsProvider.kt
@@ -1,11 +1,14 @@
 package com.lasthopesoftware.bluewater.client.browsing.files
 
+import android.os.Build
+
 object ServiceFileUriQueryParamsProvider : IServiceFileUriQueryParamsProvider {
 	override fun getServiceFileUriQueryParams(serviceFile: ServiceFile): Array<String> = arrayOf(
 		"File/GetFile",
-		"File=" + serviceFile.key,
-		"Quality=medium",
+		"File=${serviceFile.key}",
+		"Quality=Medium",
 		"Conversion=Android",
-		"Playback=0"
+		"Playback=0",
+		"AndroidVersion=${Build.VERSION.RELEASE}"
 	)
 }
