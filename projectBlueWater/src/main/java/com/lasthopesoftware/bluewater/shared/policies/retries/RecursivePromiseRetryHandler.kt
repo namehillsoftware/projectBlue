b2f1cde6e71bbbc9bc6a2025144b519564f0867c
David <dvedvick@gmail.com>
2024-03-14 21:47:43 -0500

    Properly propagate retry cancellation to factory

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/policies/retries/RecursivePromiseRetryHandler.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/policies/retries/RecursivePromiseRetryHandler.kt
@@ -1,19 +1,28 @@
 package com.lasthopesoftware.bluewater.shared.policies.retries
 
-import com.lasthopesoftware.promises.extensions.toPromise
+import com.lasthopesoftware.promises.ForwardedResponse.Companion.promisedForward
 import com.namehillsoftware.handoff.promises.Promise
+import com.namehillsoftware.handoff.promises.response.PromisedResponse
 
 object RecursivePromiseRetryHandler : RetryPromises {
-	override fun <T> retryOnException(promiseFactory: (Throwable?) -> Promise<T>): Promise<T> =
-		promiseFactory(null)
-			.eventually(
-				{
-					it.toPromise()
-				},
-				{ e ->
-					Promise.Proxy {
-						if (it.isCancelled) Promise(e)
-						else promiseFactory(e)
-					}
-				})
+	override fun <T> retryOnException(promiseFactory: (Throwable?) -> Promise<T>): Promise<T> {
+		return AttemptPromise(promiseFactory)
+	}
+
+	private class AttemptPromise<T>(
+		private val promiseFactory: (Throwable?) -> Promise<T>,
+		error: Throwable? = null
+	) : Promise.Proxy<T>(), PromisedResponse<Throwable, T> {
+		init {
+			proxy(
+				promiseFactory(error)
+					.also(cancellationProxy::doCancel)
+					.eventually(promisedForward(), this)
+			)
+		}
+
+		override fun promiseResponse(error: Throwable?): Promise<T> =
+			if (cancellationProxy.isCancelled) Promise(error)
+			else AttemptPromise(promiseFactory, error)
+	}
 }
