294e51bf05f416ae06e016064a626d71f1ce84bd
David <dvedvick@gmail.com>
2024-08-19 07:08:27 -0500

    Read stream into string before parsing

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/XmlParsingHelpers.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/XmlParsingHelpers.kt
@@ -2,6 +2,6 @@ package com.lasthopesoftware.bluewater.shared
 
 object XmlParsingHelpers {
     fun handleBadXml(currentSb: StringBuilder?, ch: CharArray?, start: Int, length: Int) {
-        currentSb?.append(ch, start, length)
+        currentSb?.appendRange(ch!!, start, start + length)
     }
 }
