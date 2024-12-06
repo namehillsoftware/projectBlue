eca3fdea99b6f345b96a02b2299a2b379128e45f
David <dvedvick@gmail.com>
2024-12-06 16:53:06 -0600

    Logged uncaught exceptions to reported thread

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/ProjectBlueApplication.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/ProjectBlueApplication.kt
@@ -40,7 +40,7 @@ import com.lasthopesoftware.bluewater.client.stored.sync.receivers.SyncItemState
 import com.lasthopesoftware.bluewater.settings.ApplicationSettingsUpdated
 import com.lasthopesoftware.bluewater.settings.repository.ApplicationSettings
 import com.lasthopesoftware.bluewater.settings.repository.access.CachingApplicationSettingsRepository.Companion.getApplicationSettingsRepository
-import com.lasthopesoftware.bluewater.shared.exceptions.LoggerUncaughtExceptionHandler
+import com.lasthopesoftware.bluewater.shared.exceptions.UncaughtExceptionHandlerLogger
 import com.lasthopesoftware.bluewater.shared.lazyLogger
 import com.lasthopesoftware.bluewater.shared.messages.application.ApplicationMessageBus.Companion.getApplicationMessageBus
 import com.lasthopesoftware.bluewater.shared.messages.registerReceiver
@@ -118,8 +118,8 @@ open class ProjectBlueApplication : Application() {
 
 		initializeLogging()
 
-		Thread.setDefaultUncaughtExceptionHandler(LoggerUncaughtExceptionHandler)
-		Promise.Rejections.setUnhandledRejectionsReceiver(LoggerUncaughtExceptionHandler)
+		Thread.setDefaultUncaughtExceptionHandler(UncaughtExceptionHandlerLogger)
+		Promise.Rejections.setUnhandledRejectionsReceiver(UncaughtExceptionHandlerLogger)
 
 		applicationSettings
 			.promiseApplicationSettings()
