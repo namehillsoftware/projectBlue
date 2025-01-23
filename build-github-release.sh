204aef4599719f27f1b6aa0e7f69c739f4834e6f
David <dvedvick@gmail.com>
2025-01-22 18:44:05 -0600

    Added nullglob option

--- build-github-release.sh
+++ build-github-release.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 shopt -s globstar
+shopt -s nullglob
 
 # Do not set user (`-u "$(id -u)":"$(id -g)"`) because it fails during Github actions. This means that
 # running this on a local machine will leave artifacts that have root ownership.
