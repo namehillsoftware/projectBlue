6f78912bacc2bbaf5246c076a8b589f7bd49eb88
David <dvedvick@gmail.com>
2025-01-25 08:35:46 -0600

    Ignored exceptions when closing lazy promises

--- projectBlueWater/src/main/java/com/lasthopesoftware/policies/retries/RetryOnRejectionLazyPromise.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/policies/retries/RetryOnRejectionLazyPromise.kt
@@ -1,5 +1,6 @@
 package com.lasthopesoftware.policies.retries
 
+import com.lasthopesoftware.promises.NoopResponse
 import com.lasthopesoftware.promises.ResolvedPromiseBox
 import com.lasthopesoftware.promises.extensions.toPromise
 import com.lasthopesoftware.resources.closables.PromisingCloseable
@@ -7,9 +8,13 @@ import com.namehillsoftware.handoff.promises.Promise
 import com.namehillsoftware.handoff.promises.response.ImmediateResponse
 import com.namehillsoftware.lazyj.AbstractSynchronousLazy
 import java.util.concurrent.atomic.AtomicReference
+import kotlin.coroutines.cancellation.CancellationException
 
 open class RetryOnRejectionLazyPromise<T>(private val factory: () -> Promise<T>) : Lazy<Promise<T>>, AutoCloseable {
 	private val resolvedPromiseBox = AtomicReference(RecurseOnRejectionLazyPromise())
+
+	@Volatile
+	private var isClosed = false
 
 	override val value: Promise<T>
 		get() = resolvedPromiseBox.get().`object`.originalPromise
@@ -21,6 +26,7 @@ open class RetryOnRejectionLazyPromise<T>(private val factory: () -> Promise<T>)
 	}
 
 	override fun close() {
+		isClosed = true
 		if (isInitializing())
 			value.cancel()
 	}
@@ -28,6 +34,8 @@ open class RetryOnRejectionLazyPromise<T>(private val factory: () -> Promise<T>)
 	private inner class RecurseOnRejectionLazyPromise
 		: AbstractSynchronousLazy<ResolvedPromiseBox<T, Promise<T>>>(), ImmediateResponse<Throwable, Boolean> {
 		override fun create(): ResolvedPromiseBox<T, Promise<T>> {
+			if (isClosed) return ResolvedPromiseBox(Promise(CancellationException("${RetryOnRejectionLazyPromise::class.simpleName} is cancelled.")))
+
 			val promise = factory()
 			promise.excuse(this)
 			return ResolvedPromiseBox(promise)
@@ -42,7 +50,7 @@ open class RetryOnRejectionLazyPromise<T>(private val factory: () -> Promise<T>)
 class CloseableRetryOnRejectionLazyPromise<T : AutoCloseable>(factory: () -> Promise<T>) : RetryOnRejectionLazyPromise<T>(factory), PromisingCloseable, ImmediateResponse<T, Unit> {
 	override fun promiseClose(): Promise<Unit> {
 		close()
-		return if (isInitializing()) value.then(this)
+		return if (isInitializing()) value.then(this, NoopResponse.noOpResponse())
 		else Unit.toPromise()
 	}
 
