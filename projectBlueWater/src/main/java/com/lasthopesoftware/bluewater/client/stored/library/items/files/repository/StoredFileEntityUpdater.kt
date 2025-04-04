9fb94e7eb2a5db03736b858284fa4c9ea38e6f8c
David <dvedvick@gmail.com>
2025-04-03 19:29:58 -0500

    Updated serviceId columns to strings

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/stored/library/items/files/repository/StoredFileEntityUpdater.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/stored/library/items/files/repository/StoredFileEntityUpdater.kt
@@ -4,7 +4,9 @@ import android.content.ContentUris
 import android.database.SQLException
 import android.database.sqlite.SQLiteDatabase
 import androidx.annotation.Keep
+import com.lasthopesoftware.bluewater.client.stored.library.items.files.repository.StoredFileEntityInformation.tableName
 import com.lasthopesoftware.bluewater.repository.IEntityUpdater
+import com.lasthopesoftware.bluewater.repository.InsertBuilder
 import com.lasthopesoftware.bluewater.repository.fetch
 import com.lasthopesoftware.bluewater.shared.lazyLogger
 import com.lasthopesoftware.resources.uri.MediaCollections
@@ -17,10 +19,42 @@ object StoredFileEntityUpdater : IEntityUpdater {
 
     override fun onUpdate(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         if (oldVersion < 13) recreateTableSchema(db)
+
+		if (oldVersion < 20) {
+			val tempTableName = tableName + "Temp"
+			db.execSQL("DROP TABLE IF EXISTS `$tempTableName`")
+
+			val createTempTableSql = StoredFileEntityInformation.createTableSql
+				.replaceFirst(
+					tableName,
+					tempTableName
+				)
+
+			db.execSQL(createTempTableSql)
+
+			val storedFiles = SqLiteCommand(db, "SELECT * FROM $tableName").fetch<Version13StoredFile>()
+
+			val storedFilesInsertStatement = storedFilesInsertStatement(tempTableName)
+			for (storedFile in storedFiles) {
+				val newStoredFile = storedFile.toStoredFile()
+
+				SqLiteCommand(db, storedFilesInsertStatement)
+					.addParameter("id", newStoredFile.id)
+					.addParameter(StoredFileEntityInformation.libraryIdColumnName, newStoredFile.libraryId)
+					.addParameter(StoredFileEntityInformation.serviceIdColumnName, newStoredFile.serviceId)
+					.addParameter(StoredFileEntityInformation.uriColumnName, newStoredFile.uri)
+					.addParameter(StoredFileEntityInformation.isDownloadCompleteColumnName, newStoredFile.isDownloadComplete)
+					.addParameter(StoredFileEntityInformation.isOwnerColumnName, newStoredFile.isOwner)
+					.execute()
+			}
+
+			db.execSQL("DROP TABLE `$tableName`")
+			db.execSQL("ALTER TABLE `$tempTableName` RENAME TO `$tableName`")
+		}
     }
 
 	private const val checkIfStoredFilesExists =
-		"SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='${StoredFileEntityInformation.tableName}'"
+		"SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='$tableName'"
 
 	private fun recreateTableSchema(db: SQLiteDatabase) {
 		val artful = SqLiteCommand(db, checkIfStoredFilesExists)
@@ -29,7 +63,7 @@ object StoredFileEntityUpdater : IEntityUpdater {
 			db.execSQL(StoredFileEntityInformation.createTableSql)
 			return
 		}
-		val storedFilesTempTableName = StoredFileEntityInformation.tableName + "Temp"
+		val storedFilesTempTableName = tableName + "Temp"
 		try {
 			db.execSQL("DROP TABLE `$storedFilesTempTableName`")
 		} catch (se: SQLException) {
@@ -37,13 +71,13 @@ object StoredFileEntityUpdater : IEntityUpdater {
 		}
 		val createTempTableSql = StoredFileEntityInformation.createTableSql
 			.replaceFirst(
-				StoredFileEntityInformation.tableName,
+				tableName,
 				storedFilesTempTableName
 			)
 		logger.warn("Creating temp table with SQL: $createTempTableSql")
 		db.execSQL(createTempTableSql)
 		try {
-			val oldStoredFiles = SqLiteCommand(db, "SELECT * FROM " + StoredFileEntityInformation.tableName).fetch<Version12StoredFile>()
+			val oldStoredFiles = SqLiteCommand(db, "SELECT * FROM " + tableName).fetch<Version12StoredFile>()
 
 			for (oldStoredFile in oldStoredFiles) {
 				val newUriString = oldStoredFile
@@ -69,8 +103,8 @@ object StoredFileEntityUpdater : IEntityUpdater {
 			logger.error("There was an error moving the data!", sqlException)
 			throw sqlException
 		}
-		db.execSQL("DROP TABLE `${StoredFileEntityInformation.tableName}`")
-		db.execSQL("ALTER TABLE `$storedFilesTempTableName` RENAME TO `${StoredFileEntityInformation.tableName}`")
+		db.execSQL("DROP TABLE `$tableName`")
+		db.execSQL("ALTER TABLE `$storedFilesTempTableName` RENAME TO `$tableName`")
 	}
 
 	@Keep
@@ -82,4 +116,32 @@ object StoredFileEntityUpdater : IEntityUpdater {
 		var path: String? = null
 		var isOwner = false
 	}
+
+	@Keep
+	class Version13StoredFile {
+		var id = 0
+		var libraryId = 0
+		var serviceId = 0
+		var isDownloadComplete = false
+		var uri: String? = null
+		var isOwner = false
+
+		fun toStoredFile(): StoredFile = StoredFile()
+			.setId(id)
+			.setLibraryId(libraryId)
+			.setServiceId(serviceId.toString())
+			.setIsDownloadComplete(isDownloadComplete)
+			.setIsOwner(isOwner)
+			.setUri(uri)
+	}
+
+	private fun storedFilesInsertStatement(tableName: String) = InsertBuilder
+		.fromTable(tableName)
+		.addColumn("id")
+		.addColumn(StoredFileEntityInformation.libraryIdColumnName)
+		.addColumn(StoredFileEntityInformation.serviceIdColumnName)
+		.addColumn(StoredFileEntityInformation.uriColumnName)
+		.addColumn(StoredFileEntityInformation.isDownloadCompleteColumnName)
+		.addColumn(StoredFileEntityInformation.isOwnerColumnName)
+		.build()
 }
