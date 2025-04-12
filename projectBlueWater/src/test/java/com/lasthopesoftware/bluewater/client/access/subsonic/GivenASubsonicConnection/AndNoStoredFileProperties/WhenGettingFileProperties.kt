73ecbcdae32f4be051227b6d3294c886a61ea545
David <dvedvick@gmail.com>
2025-04-12 12:56:51 -0500

    Implemented getting file properties

--- projectBlueWater/src/test/java/com/lasthopesoftware/bluewater/client/access/subsonic/GivenASubsonicConnection/AndNoStoredFileProperties/WhenGettingFileProperties.kt
+++ projectBlueWater/src/test/java/com/lasthopesoftware/bluewater/client/access/subsonic/GivenASubsonicConnection/AndNoStoredFileProperties/WhenGettingFileProperties.kt
@@ -5,34 +5,32 @@ import com.lasthopesoftware.bluewater.client.browsing.files.ServiceFile
 import com.lasthopesoftware.bluewater.client.browsing.files.properties.KnownFileProperties
 import com.lasthopesoftware.bluewater.client.connection.SubsonicConnectionDetails
 import com.lasthopesoftware.bluewater.client.connection.live.LiveSubsonicConnection
-import com.lasthopesoftware.bluewater.client.connection.requests.FakeHttpConnection
 import com.lasthopesoftware.bluewater.client.connection.requests.FakeHttpConnectionProvider
 import com.lasthopesoftware.bluewater.client.connection.url.UrlBuilder.addParams
+import com.lasthopesoftware.bluewater.client.connection.url.UrlBuilder.addPath
 import com.lasthopesoftware.bluewater.client.connection.url.UrlBuilder.withSubsonicApi
 import com.lasthopesoftware.bluewater.shared.promises.extensions.toExpiringFuture
+import com.lasthopesoftware.promises.extensions.toPromise
 import com.lasthopesoftware.resources.PassThroughHttpResponse
 import com.lasthopesoftware.resources.strings.JsonEncoderDecoder
+import io.mockk.every
 import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class WhenGettingFileProperties {
 	private val fileProperties by lazy {
-		val httpConnection = FakeHttpConnection().apply {
-			mapResponse(
-				TestUrl.withSubsonicApi().addParams("getSong").addParams("id=c6c3585f823f4c5093fada347ccd0239")
-			) {
-				PassThroughHttpResponse(
-					200,
-					"Alright",
-					"""{"subsonic-response":{"status":"ok","version":"1.16.1","type":"navidrome","serverVersion":"0.53.3 (13af8ed4)","openSubsonic":true,"song":{"id":"3c00a4d5a48b0790d8e2288faf6fc93c","parent":"9022600b4f57592edd8e8bfc9048fe38","isDir":false,"title":"How Will You Meet Your End","album":"American Hearts","artist":"A.A. Bondy","track":1,"year":2007,"genre":"Americana","coverArt":"mf-3c00a4d5a48b0790d8e2288faf6fc93c_6728cffb","size":25331533,"contentType":"audio/flac","suffix":"flac","duration":242,"bitRate":827,"path":"A.A. Bondy/American Hearts/01 - How Will You Meet Your End.flac","discNumber":1,"created":"2025-02-10T04:25:55.814140973Z","albumId":"9022600b4f57592edd8e8bfc9048fe38","artistId":"563727e73731392260da4a787f534387","type":"music","isVideo":false,"bpm":128,"comment":"Visit http://aabondy.bandcamp.com","sortName":"","mediaType":"song","musicBrainzId":"4fefe475-a23e-44e5-a46a-ce2d4167e108","genres":[{"name":"Americana"}],"replayGain":{"trackGain":-2.34,"trackPeak":0.933,"albumPeak":1},"channelCount":2,"samplingRate":44100}}}""".toByteArray().inputStream()
-				)
-			}
-		}
-
 		val connection = LiveSubsonicConnection(
 			SubsonicConnectionDetails(TestUrl, "2QbKsOo", "rIuUXpsY"),
-			FakeHttpConnectionProvider(httpConnection),
+			FakeHttpConnectionProvider(
+				mockk {
+					every { promiseResponse(TestUrl.withSubsonicApi().addPath("getSong").addParams("id=c6c3585f823f4c5093fada347ccd0239")) } returns PassThroughHttpResponse(
+						200,
+						"Alright",
+						"""{"subsonic-response":{"status":"ok","version":"1.16.1","type":"navidrome","serverVersion":"0.53.3 (13af8ed4)","openSubsonic":true,"song":{"id":"3c00a4d5a48b0790d8e2288faf6fc93c","parent":"9022600b4f57592edd8e8bfc9048fe38","isDir":false,"title":"How Will You Meet Your End","album":"American Hearts","artist":"A.A. Bondy","track":1,"year":2007,"genre":"Americana","coverArt":"mf-3c00a4d5a48b0790d8e2288faf6fc93c_6728cffb","size":25331533,"contentType":"audio/flac","suffix":"flac","duration":242,"bitRate":827,"path":"A.A. Bondy/American Hearts/01 - How Will You Meet Your End.flac","discNumber":1,"created":"2025-02-10T04:25:55.814140973Z","albumId":"9022600b4f57592edd8e8bfc9048fe38","artistId":"563727e73731392260da4a787f534387","type":"music","isVideo":false,"bpm":128,"comment":"Visit http://aabondy.bandcamp.com","sortName":"","mediaType":"song","musicBrainzId":"4fefe475-a23e-44e5-a46a-ce2d4167e108","genres":[{"name":"Americana"}],"replayGain":{"trackGain":-2.34,"trackPeak":0.933,"albumPeak":1},"channelCount":2,"samplingRate":44100}}}""".toByteArray().inputStream()
+					).toPromise()
+				}
+			),
 			mockk(),
 			JsonEncoderDecoder,
 		)
@@ -45,7 +43,7 @@ class WhenGettingFileProperties {
 
     @Test
     fun `then files property key is retrieved`() {
-        assertThat(fileProperties!![KnownFileProperties.Key]).isEqualTo("45")
+        assertThat(fileProperties!![KnownFileProperties.Key]).isEqualTo("3c00a4d5a48b0790d8e2288faf6fc93c")
     }
 
 	@Test
