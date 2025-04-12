760506b6fb7ddd2c6825886d8dde4bc4a26fe5f6
David <dvedvick@gmail.com>
2025-04-12 09:46:55 -0500

    Moved server version based file property updates to connection provider

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/browsing/files/properties/playstats/fileproperties/UpdatePlayStatsWithFileProperties.kt
+++
@@ -1,5 +0,0 @@
-package com.lasthopesoftware.bluewater.client.browsing.files.properties.playstats.fileproperties
-
-import com.lasthopesoftware.bluewater.client.browsing.files.properties.playstats.UpdatePlaystats
-
-interface UpdatePlayStatsWithFileProperties : UpdatePlaystats
