47658e22bcba1e848ce370d0e178f66b6e47f9be
David <dvedvick@gmail.com>
2025-04-18 06:58:31 -0500

    Moved library revision caching

--- projectBlueWater/src/main/java/com/lasthopesoftware/policies/ExecutionPolicies.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/policies/ExecutionPolicies.kt
@@ -2,7 +2,7 @@ package com.lasthopesoftware.policies
 
 import com.namehillsoftware.handoff.promises.Promise
 
-interface ApplyExecutionPolicies {
+interface ExecutionPolicies {
 	fun<Input: Any?, Output> applyPolicy(function: (Input) -> Promise<Output>): (Input) -> Promise<Output>
 	fun<In1: Any?, In2: Any?, Output> applyPolicy(function: (In1, In2) -> Promise<Output>): (In1, In2) -> Promise<Output>
 
