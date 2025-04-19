a54dc581aff8effa25db67c21b4552a0ca56bf54
David <dvedvick@gmail.com>
2025-04-18 19:16:09 -0500

    Implemented isReadOnly check using getUser API

--- projectBlueWater/src/test/java/com/lasthopesoftware/bluewater/client/access/subsonic/GivenASubsonicConnection/AndAReadWriteLibraryServer/When Checking Authentication.kt
+++ projectBlueWater/src/test/java/com/lasthopesoftware/bluewater/client/access/subsonic/GivenASubsonicConnection/AndAReadWriteLibraryServer/When Checking Authentication.kt
@@ -1,10 +1,11 @@
-package com.lasthopesoftware.bluewater.client.access.subsonic.GivenASubsonicConnection.AndAnOlderLibraryServer
+package com.lasthopesoftware.bluewater.client.access.subsonic.GivenASubsonicConnection.AndAReadWriteLibraryServer
 
 import com.lasthopesoftware.TestUrl
 import com.lasthopesoftware.bluewater.client.connection.SubsonicConnectionDetails
 import com.lasthopesoftware.bluewater.client.connection.live.LiveSubsonicConnection
 import com.lasthopesoftware.bluewater.client.connection.requests.FakeHttpConnection
 import com.lasthopesoftware.bluewater.client.connection.requests.FakeHttpConnectionProvider
+import com.lasthopesoftware.bluewater.client.connection.url.UrlBuilder.addParams
 import com.lasthopesoftware.bluewater.client.connection.url.UrlBuilder.addPath
 import com.lasthopesoftware.bluewater.client.connection.url.UrlBuilder.withSubsonicApi
 import com.lasthopesoftware.bluewater.shared.promises.extensions.toExpiringFuture
@@ -14,21 +15,46 @@ import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-class WhenCheckingAuthentication {
+class `When Checking Authentication` {
 
 	private val isReadOnly by lazy {
 		val httpConnection = FakeHttpConnection().apply {
-			mapResponse(TestUrl.withSubsonicApi().addPath("Authenticate")) {
+			mapResponse(TestUrl.withSubsonicApi().addPath("getUser").addParams("username=mh6qdj2kJH")) {
 				PassThroughHttpResponse(
 					200,
 					"OK",
-					"""{"subsonic-response":{"status":"ok","version":"1.16.1","type":"navidrome","serverVersion":"0.53.3 (13af8ed4)","openSubsonic":true,"license":{"valid":true}}}""".toByteArray().inputStream()
+					"""{
+  "subsonic-response": {
+    "status": "ok",
+    "version": "1.16.1",
+    "user": {
+      "folder": [
+          1,
+          3
+      ],
+      "username": "HanRaniJi",
+      "email": "RamonPang@Nullaclass.8a",
+      "scrobblingEnabled": "true",
+      "adminRole": "false",
+      "settingsRole": "true",
+      "downloadRole": "true",
+      "uploadRole": "false",
+      "playlistRole": "true",
+      "coverArtRole": "true",
+      "commentRole": "true",
+      "podcastRole": "true",
+      "streamRole": "true",
+      "jukeboxRole": "true",
+      "shareRole": "false"
+    }
+  }
+}""".trimIndent().toByteArray().inputStream()
 				)
 			}
 		}
 
 		val access = LiveSubsonicConnection(
-			SubsonicConnectionDetails(TestUrl, "zSaHtBXlict", "KtzJVjBK"),
+			SubsonicConnectionDetails(TestUrl, "mh6qdj2kJH", "bNI1AuryTD"),
 			FakeHttpConnectionProvider(httpConnection),
 			mockk(),
 			JsonEncoderDecoder,
