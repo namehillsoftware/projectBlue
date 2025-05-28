e3e3ce26841886da7e4cf3ba86603285dc854241
David <dvedvick@gmail.com>
2025-05-27 22:36:59 -0500

    Replaced overflow menu with refresh button in ItemListView

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/browsing/items/list/menus/ItemListMenuButtons.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/browsing/items/list/menus/ItemListMenuButtons.kt
@@ -1,20 +1,7 @@
 package com.lasthopesoftware.bluewater.client.browsing.items.list.menus
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.res.painterResource
@@ -22,12 +9,9 @@ import androidx.compose.ui.res.stringResource
 import com.lasthopesoftware.bluewater.NavigateApplication
 import com.lasthopesoftware.bluewater.R
 import com.lasthopesoftware.bluewater.client.browsing.files.list.FileListViewModel
-import com.lasthopesoftware.bluewater.client.browsing.files.list.UnlabelledSyncButton
 import com.lasthopesoftware.bluewater.client.browsing.items.list.ItemListViewModel
 import com.lasthopesoftware.bluewater.shared.android.ui.components.ColumnMenuIcon
-import com.lasthopesoftware.bluewater.shared.android.ui.components.LabelledRefreshButton
-import com.lasthopesoftware.bluewater.shared.android.ui.navigable
-import com.lasthopesoftware.bluewater.shared.android.ui.theme.LocalControlColor
+import com.lasthopesoftware.bluewater.shared.android.ui.components.UnlabelledRefreshButton
 
 @Composable
 fun RowScope.LabelledActiveDownloadsButton(
@@ -69,9 +53,10 @@ fun RowScope.LabelledSearchButton(
 }
 
 @Composable
-fun RowScope.UnlabelledSettingsButton(
+fun RowScope.LabelledSettingsButton(
 	itemListViewModel: ItemListViewModel,
 	applicationNavigation: NavigateApplication,
+	modifier: Modifier = Modifier,
 ) {
 	val settingsButtonLabel = stringResource(id = R.string.settings)
 	ColumnMenuIcon(
@@ -80,18 +65,20 @@ fun RowScope.UnlabelledSettingsButton(
 		},
 		iconPainter = painterResource(id = R.drawable.ic_action_settings),
 		contentDescription = settingsButtonLabel,
+		label = settingsButtonLabel,
+		labelModifier = modifier,
+		labelMaxLines = 1,
 	)
 }
 
-
 @Composable
-fun RowScope.LabelledRefreshButton(
+fun UnlabelledRefreshButton(
 	itemListViewModel: ItemListViewModel,
 	fileListViewModel: FileListViewModel,
 	modifier: Modifier = Modifier,
 	focusRequester: FocusRequester? = null,
 ) {
-	LabelledRefreshButton(
+	UnlabelledRefreshButton(
 		onClick = {
 			itemListViewModel.promiseRefresh()
 			fileListViewModel.promiseRefresh()
@@ -99,72 +86,4 @@ fun RowScope.LabelledRefreshButton(
 		modifier = modifier,
 		focusRequester = focusRequester,
 	)
-}
-
-@Composable
-@OptIn(ExperimentalComposeUiApi::class)
-fun MoreFileOptionsMenu(fileListViewModel: FileListViewModel, modifier: Modifier = Modifier, focusRequester: FocusRequester? = null) {
-	Box(modifier = Modifier
-		.wrapContentSize(Alignment.TopEnd)
-		.then(modifier)
-	) {
-		var isExpanded by remember { mutableStateOf(false) }
-		Icon(
-			painter = painterResource(R.drawable.more_vertical_24),
-			contentDescription = stringResource(R.string.view_more_options),
-			modifier = Modifier.navigable(onClick = { isExpanded = !isExpanded }, focusRequester = focusRequester),
-			tint = LocalControlColor.current,
-		)
-
-		DropdownMenu(
-			expanded = isExpanded,
-			onDismissRequest = { isExpanded = false }
-		) {
-			DropdownMenuItem(onClick = { fileListViewModel.toggleSync() }) {
-				val isSynced by fileListViewModel.isSynced.collectAsState()
-				val syncButtonLabel =
-					if (!isSynced) stringResource(id = R.string.btn_sync_item)
-					else stringResource(id = R.string.files_synced)
-				Text(syncButtonLabel)
-				UnlabelledSyncButton(fileListViewModel = fileListViewModel)
-			}
-		}
-	}
-}
-
-@Composable
-@OptIn(ExperimentalComposeUiApi::class)
-fun MoreItemsOnlyOptionsMenu(
-	itemListViewModel: ItemListViewModel,
-	applicationNavigation: NavigateApplication,
-	modifier: Modifier = Modifier,
-	focusRequester: FocusRequester? = null,
-) {
-	Box(modifier = Modifier
-		.wrapContentSize(Alignment.TopEnd)
-		.then(modifier)
-	) {
-		var isExpanded by remember { mutableStateOf(false) }
-		Icon(
-			painter = painterResource(R.drawable.more_vertical_24),
-			contentDescription = stringResource(R.string.view_more_options),
-			modifier = Modifier
-				.navigable(onClick = { isExpanded = !isExpanded }, focusRequester = focusRequester),
-			tint = LocalControlColor.current,
-		)
-
-		DropdownMenu(
-			expanded = isExpanded,
-			onDismissRequest = { isExpanded = false }
-		) {
-			DropdownMenuItem(onClick = { itemListViewModel.loadedLibraryId?.also(applicationNavigation::viewServerSettings) }) {
-				val settingsButtonLabel = stringResource(id = R.string.settings)
-				Text(settingsButtonLabel)
-				UnlabelledSettingsButton(
-					itemListViewModel = itemListViewModel,
-					applicationNavigation = applicationNavigation,
-				)
-			}
-		}
-	}
 }
