72d75353c9ee8ab7b15397410d5284d5dd964c0d
David <dvedvick@gmail.com>
2025-10-01 22:31:37 -0500

    Tweaked theme colors
    Calculated by https://mui.com/material-ui/customization/color/#playground

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/android/ui/theme/Theme.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/android/ui/theme/Theme.kt
@@ -8,10 +8,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-	primary = Dark.ProjectBlue,
+	primary = Dark.ProjectBlueDark,
 	onPrimary = Color.White,
-	primaryVariant = Dark.ProjectBlueLight,
-	secondary = Dark.ProjectBlueDark,
+	primaryVariant = Dark.ProjectBlue,
+	secondary = Dark.ProjectBlueLight,
 	onSecondary = Color.White,
 	onError = Color.White,
 )
