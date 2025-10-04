7a25f817e7be559fb9c597f31d41405a2d72afea
David <dvedvick@gmail.com>
2025-10-04 09:06:47 -0500

    Reduced observable subscription overhead

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/observables/StateObservableExtensions.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/shared/observables/StateObservableExtensions.kt
@@ -1,6 +1,7 @@
 package com.lasthopesoftware.bluewater.shared.observables
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
@@ -8,21 +9,18 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshotFlow
 import kotlinx.coroutines.flow.drop
-import kotlinx.coroutines.rx3.asFlow
 import kotlinx.coroutines.withContext
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
 @Composable
-fun <T, S : InteractionState<T>> S.subscribeAsState(
-	context: CoroutineContext = EmptyCoroutineContext
-): State<T> = updatingState(context)
+fun <T, S : InteractionState<T>> S.subscribeAsState(): State<T> = updatingState()
 
 @Composable
 fun <T, S : MutableInteractionState<T>> S.subscribeAsMutableState(
 	context: CoroutineContext = EmptyCoroutineContext
 ): MutableState<T> {
-	val state = updatingState(context)
+	val state = updatingState()
 
 	LaunchedEffect(key1 = this, context) {
 		if (context == EmptyCoroutineContext) {
@@ -39,17 +37,15 @@ fun <T, S : MutableInteractionState<T>> S.subscribeAsMutableState(
 }
 
 @Composable
-private fun <T, S : InteractionState<T>> S.updatingState(context: CoroutineContext): MutableState<T> {
+private fun <T, S : InteractionState<T>> S.updatingState(): MutableState<T> {
 	val state = remember { mutableStateOf(value) }
-	LaunchedEffect(this, context) {
-		if (context == EmptyCoroutineContext) {
-			asFlow().collect {
-				state.value = it.value
-			}
-		} else withContext(context) {
-			asFlow().collect {
-				state.value = it.value
-			}
+	DisposableEffect(this) {
+		val subscription = subscribe {
+			state.value = it.value
+		}
+
+		onDispose {
+			subscription.dispose()
 		}
 	}
 	return state
