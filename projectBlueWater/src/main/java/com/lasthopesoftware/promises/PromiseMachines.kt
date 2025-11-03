5ca9ed4adfbcb161ced067f76ef30ba9490c67bb
David <dvedvick@gmail.com>
2025-11-02 19:28:56 -0600

    Optimized loop proxies

--- projectBlueWater/src/main/java/com/lasthopesoftware/promises/PromiseMachines.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/promises/PromiseMachines.kt
@@ -1,9 +1,8 @@
 package com.lasthopesoftware.promises
 
-import com.lasthopesoftware.promises.extensions.toPromise
 import com.namehillsoftware.handoff.cancellation.Cancellable
 import com.namehillsoftware.handoff.promises.Promise
-import com.namehillsoftware.handoff.promises.response.PromisedResponse
+import com.namehillsoftware.handoff.promises.response.ImmediateResponse
 
 object PromiseMachines {
 	fun <Resolution> repeat(function: () -> Promise<Resolution>, repetitions: Int) : Promise<Resolution> {
@@ -20,21 +19,27 @@ object PromiseMachines {
 	}
 
 	private class LoopMachine<Resolution>(private val function: (Resolution?, Cancellable) -> Promise<Resolution>) :
-		ContinuableMachine<Resolution>(), PromisedResponse<Resolution, Resolution> {
+		ContinuableMachine<Resolution>(), ImmediateResponse<Resolution, Unit> {
 
 		@Volatile
 		private var currentValue: Resolution? = null
 
 		init {
-			start()
+			next()
 		}
 
-		override fun next(): Promise<Resolution> = function(currentValue, this).eventually(this)
+		override fun next(): Promise<Resolution> =
+			function(currentValue, this)
+				.also {
+					doCancel(it)
+					it.then(this)
+					proxyRejection(it)
+				}
 
-		override fun promiseResponse(resolution: Resolution): Promise<Resolution> {
+		override fun respond(resolution: Resolution) {
 			currentValue = resolution
-			return if (!isCancelled) next()
-			else resolution.toPromise()
+			if (!isCancelled) next()
+			else resolve(resolution)
 		}
 	}
 
