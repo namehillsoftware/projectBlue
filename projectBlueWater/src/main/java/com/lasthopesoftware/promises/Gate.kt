719d0502d9e586c57f4d76c41eb0337476bdfed4
David <dvedvick@gmail.com>
2025-12-27 22:43:25 -0600

    Fixed uses of gates

--- projectBlueWater/src/main/java/com/lasthopesoftware/promises/Gate.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/promises/Gate.kt
@@ -1,5 +1,8 @@
 package com.lasthopesoftware.promises
 
+import com.namehillsoftware.handoff.promises.Promise
+
 interface Gate {
-	fun open(): AutoCloseable
+	fun open(): Gate
+	fun reset(): Promise<Boolean>
 }
