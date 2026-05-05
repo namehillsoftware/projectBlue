9210dba99a5370318a69f4cf11c75daf3fe0a6d0
David <dvedvick@gmail.com>
2026-05-05 09:04:39 -0500

    Finish playlist edit when edit view is collapsed

--- projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/nowplaying/view/NowPlayingPlaylist.kt
+++ projectBlueWater/src/main/java/com/lasthopesoftware/bluewater/client/playback/nowplaying/view/NowPlayingPlaylist.kt
@@ -1,8 +1,13 @@
 package com.lasthopesoftware.bluewater.client.playback.nowplaying.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
@@ -12,13 +17,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.input.InputMode
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.platform.LocalInputModeManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import com.lasthopesoftware.bluewater.NavigateApplication
+import com.lasthopesoftware.bluewater.R
 import com.lasthopesoftware.bluewater.android.ui.components.dragging.DragDropItemScope
 import com.lasthopesoftware.bluewater.android.ui.components.dragging.DragDropLazyColumn
 import com.lasthopesoftware.bluewater.android.ui.components.dragging.rememberDragDropListState
+import com.lasthopesoftware.bluewater.android.ui.navigable
 import com.lasthopesoftware.bluewater.client.browsing.files.list.ViewPlaylistFileItem
 import com.lasthopesoftware.bluewater.client.browsing.items.list.menus.changes.handlers.ItemListMenuBackPressedHandler
 import com.lasthopesoftware.bluewater.client.browsing.library.repository.LibraryId
@@ -100,6 +112,7 @@ fun DragDropItemScope.NowPlayingFileView(
 		}
 	)
 }
+
 @Composable
 fun NowPlayingFileView(
 	activeLibraryId: LibraryId?,
@@ -180,6 +193,100 @@ fun NowPlayingFileView(
 			}
 		},
 	)
+}
+
+@Composable
+fun PlaylistControls(
+	modifier: Modifier = Modifier,
+	playlistViewModel: NowPlayingPlaylistViewModel,
+	viewModelMessageBus: ViewModelMessageBus<NowPlayingMessage>,
+	undoBackStack: UndoStack,
+) {
+	Row(
+		modifier = modifier,
+		horizontalArrangement = Arrangement.SpaceAround,
+		verticalAlignment = Alignment.CenterVertically,
+	) {
+		val isEditingPlaylist by playlistViewModel.isEditingPlaylist.subscribeAsState()
+		if (isEditingPlaylist) {
+			Image(
+				painter = painterResource(id = R.drawable.cancel_36dp),
+				contentDescription = stringResource(id = R.string.finish_edit_now_playing_list),
+				modifier = Modifier.navigable(onClick = playlistViewModel::finishPlaylistEdit),
+				alpha = playlistControlAlpha,
+			)
+		} else {
+			Image(
+				painter = painterResource(id = R.drawable.pencil),
+				contentDescription = stringResource(id = R.string.edit_now_playing_list),
+				modifier = Modifier
+					.navigable(onClick = {
+						playlistViewModel.editPlaylist()
+						undoBackStack.addAction { playlistViewModel.finishPlaylistEdit().toPromise() }
+					}),
+				alpha = playlistControlAlpha,
+			)
+		}
+
+		if (isEditingPlaylist) {
+			Image(
+				painter = painterResource(id = R.drawable.clear_all_white_36dp),
+				contentDescription = stringResource(R.string.empty_playlist),
+				modifier = Modifier.navigable(onClick = playlistViewModel::requestPlaylistClearingPermission),
+				alpha = playlistControlAlpha,
+			)
+		} else {
+			val isRepeating by playlistViewModel.isRepeating.subscribeAsState()
+			if (isRepeating) {
+				Image(
+					painter = painterResource(id = R.drawable.av_repeat_white),
+					contentDescription = stringResource(id = R.string.btn_complete_playlist),
+					modifier = Modifier.navigable(onClick = playlistViewModel::toggleRepeating),
+					alpha = playlistControlAlpha,
+				)
+			} else {
+				Image(
+					painter = painterResource(id = R.drawable.av_no_repeat_white),
+					contentDescription = stringResource(id = R.string.btn_repeat_playlist),
+					modifier = Modifier.navigable(onClick = playlistViewModel::toggleRepeating),
+					alpha = playlistControlAlpha,
+				)
+			}
+		}
+
+		if (isEditingPlaylist) {
+			Image(
+				painter = painterResource(id = R.drawable.upload_36dp),
+				contentDescription = stringResource(id = R.string.save_playlist),
+				modifier = Modifier.navigable(onClick = playlistViewModel::enableSavingPlaylist),
+				alpha = playlistControlAlpha,
+			)
+		} else {
+			val hapticFeedback = LocalHapticFeedback.current
+			val isAutoScrollEnabled by playlistViewModel.isAutoScrolling.subscribeAsState()
+			Image(
+				painter = painterResource(id = R.drawable.baseline_playlist_play_36),
+				contentDescription = stringResource(R.string.scroll_to_now_playing_item),
+				modifier = Modifier.navigable(
+					interactionSource = remember { MutableInteractionSource() },
+					indication = ripple(),
+					onClick = {
+						viewModelMessageBus.sendMessage(NowPlayingMessage.ScrollToNowPlaying)
+					},
+					onClickLabel = stringResource(R.string.scroll_to_now_playing_item),
+					onLongClick = {
+						if (isAutoScrollEnabled)
+							playlistViewModel.disableUserAutoScrolling()
+						else
+							playlistViewModel.enableUserAutoScrolling()
+						hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
+					},
+					onLongClickLabel = stringResource(R.string.auto_scroll_to_now_playing_item)
+				),
+				alpha = if (isAutoScrollEnabled) 1f else disabledAlpha,
+			)
+		}
+	}
 }
 
 @Composable
